d971ab194a40b89036c52c7375ae734d3ba3f1be
Xu Shaohua <xushaohua2016@outlook.com>
2019-05-18 21:51:52 +0800

    Add setuid() and setsid()

--- src/platform/linux-x86_64/c.rs
+++ src/platform/linux-x86_64/c.rs
@@ -14,6 +14,7 @@ fn c_str(s: &str) -> [u8; 128]{
     return buf;
 }
 
+/// Close a file descriptor.
 pub fn close(fd: isize) -> Result<(), Errno> {
     unsafe {
         let fd = fd as usize;
@@ -27,12 +28,14 @@ pub fn close(fd: isize) -> Result<(), Errno> {
     }
 }
 
+/// Terminate current process.
 pub fn exit(status: u8) {
     unsafe {
         syscall1(SYS_EXIT, status as usize);
     }
 }
 
+/// Create a child process.
 pub fn fork() -> Result<pid_t, Errno> {
     unsafe {
         let ret = syscall0(SYS_FORK);
@@ -45,42 +48,56 @@ pub fn fork() -> Result<pid_t, Errno> {
     }
 }
 
+/// Get the effective group ID of the calling process.
 pub fn getegid() -> gid_t {
     unsafe {
         return syscall0(SYS_GETEGID) as gid_t;
     }
 }
 
+/// Get the effective user ID of the calling process.
 pub fn geteuid() -> uid_t {
     unsafe {
         return syscall0(SYS_GETEUID) as uid_t;
     }
 }
 
+/// Get the real group ID of the calling process.
 pub fn getgid() -> gid_t {
     unsafe {
         return syscall0(SYS_GETGID) as gid_t;
     }
 }
 
+/// Get process group.
 pub fn getpgrp() -> pid_t {
     unsafe {
         return syscall0(SYS_GETPGRP) as pid_t;
     }
 }
 
+/// Get the process ID (PID) of the calling process.
 pub fn getpid() -> pid_t {
     unsafe {
         return syscall0(SYS_GETPID) as pid_t;
     }
 }
 
+/// Get the process group ID of the calling process.
+pub fn getpgrp() -> pid_t {
+    unsafe {
+        return syscall0(SYS_GETPGRP) as pid_t;
+    }
+}
+
+/// Get the process ID of the parent of the calling process.
 pub fn getppid() -> pid_t {
     unsafe {
         return syscall0(SYS_GETPPID) as pid_t;
     }
 }
 
+/// Get the real user ID of the calling process.
 pub fn getuid() -> uid_t {
     unsafe {
         return syscall0(SYS_GETUID) as uid_t;
@@ -116,7 +133,7 @@ pub fn rename(oldpath: &str, newpath: &str) -> Result<(), Errno> {
     }
 }
 
-/// Set group identity.
+/// Set the group ID of the calling process to `gid`.
 pub fn setgid(gid: gid_t) -> Result<(), Errno> {
     unsafe {
         let gid = gid as usize;
@@ -130,11 +147,40 @@ pub fn setgid(gid: gid_t) -> Result<(), Errno> {
     }
 }
 
+/// Set the process group ID (PGID) of the process specified by `pid` to `pgid`.
 pub fn setpgid(pid: pid_t, pgid: pid_t) -> Result<(), Errno> {
     unsafe {
         let pid = pid as usize;
         let pgid = pgid as usize;
         let ret = syscall2(SYS_SETPGID, pid, pgid);
+        let reti = ret as isize;
+        if reti < 0 && reti >= -256 {
+            return Err(-reti);
+        } else {
+            return Ok(());
+        }
+    }
+}
+
+/// Create a new session if the calling process is not a process group leader.
+pub fn setsid() -> Result<pid_t, Errno> {
+    unsafe {
+        let ret = syscall0(SYS_SETSID);
+        let reti = ret as isize;
+        if reti < 0 && reti >= -256 {
+            return Err(-reti);
+        } else {
+            let ret = ret as pid_t;
+            return Ok(ret);
+        }
+    }
+}
+
+/// Set the effective user ID of the calling process to `uid`.
+pub fn setuid(uid: uid_t) -> Result<(), Errno> {
+    unsafe {
+        let uid = uid as usize;
+        let ret = syscall1(SYS_SETUID, uid);
         let reti = ret as isize;
         if reti < 0 && reti >= -256 {
             return Err(-reti);

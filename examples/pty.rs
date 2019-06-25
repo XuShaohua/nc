40433b760c6629ba4810a814fe62557cd49e3f4e
Xu Shaohua <xushaohua2016@outlook.com>
2019-06-26 01:14:00 +0800

    Handle pty error

--- examples/pty.rs
+++ examples/pty.rs
@@ -1,32 +1,37 @@
 
 extern crate nc;
 
-fn ptsname(pts_fd: i32) -> String {
-    let mut n: i32;
-    let n_ptr = n as *mut i32 as usize;
-    let _err = nc::ioctl(pts_fd, nc::TIOCGPTN, n_ptr);
-    return format!("/dev/pts/{}", n);
+fn ptsname(pty_fd: i32) -> Result<String, nc::Errno> {
+    println!("ptsname()");
+    #[allow(unused_mut)]
+    let mut n: i32 = 0;
+    let n_ptr = (&mut n) as *mut i32 as usize;
+    nc::ioctl(pty_fd, nc::TIOCGPTN, n_ptr)?;
+    return Ok(format!("/dev/pts/{}", n));
 }
 
-fn unlockpt(pts_fd: i32) {
-    let mut u: i32 = 0;
-    let u_ptr = u as *mut i32 as usize;
-    let _err = nc::ioctl(pts_fd, nc::TIOCSPTLCK, u_ptr);
+fn unlockpt(pty_fd: i32) -> Result<(), nc::Errno> {
+    println!("unlockpt()");
+    let u: i32 = 0;
+    let u_ptr = &u as *const i32 as usize;
+    nc::ioctl(pty_fd, nc::TIOCSPTLCK, u_ptr)
 }
 
-fn open_pty() {
-    let pts_fd = nc::open("/dev/ptmx", nc::O_RDWR, 0)
-        .expect("Failed to open file!");
+fn open_pty() -> Result<(i32, i32), nc::Errno> {
+    let pty_fd = nc::open("/dev/ptmx", nc::O_RDWR, 0)?;
+    println!("pty_fd: {}", pty_fd);
 
-    let sname = ptsname(pts_fd);
+    let sname = ptsname(pty_fd)?;
     println!("sname: {}", sname);
-    unlockpt(pts_fd);
+    unlockpt(pty_fd)?;
 
-    let t_fd = nc::open(&sname, nc::O_RDWR | nc::O_NOCTTY, 0)
-        .expect("Failed to open pty slave");
-    println!("t_fd: {}", t_fd);
+    let tty_fd = nc::open(&sname, nc::O_RDWR | nc::O_NOCTTY, 0)?;
+    println!("tty_fd: {}", tty_fd);
+
+    return Ok((pty_fd, tty_fd));
 }
 
 fn main() {
-    open_pty();
+    let ret = open_pty();
+    println!("ret: {:?}", ret);
 }

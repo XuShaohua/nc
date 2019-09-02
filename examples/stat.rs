17a79db9c047bdaece5bf908641b254123203cf7
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-02 14:01:33 +0800

    stat: Call `newstatfs` on x86_64

--- examples/stat.rs
+++ examples/stat.rs
@@ -2,6 +2,18 @@ extern crate nc;
 
 fn main() {
     let mut statbuf = nc::stat_t::default();
+
+    #[cfg(target_arch = "x86_64")]
+    match nc::newfstatat(nc::AT_FDCWD, "/etc/passwd", &mut statbuf, 0) {
+        Ok(_) => {
+            println!("s: {:?}", statbuf);
+        }
+        Err(errno) => {
+            eprintln!("Failed to get file status, got errno: {}", errno);
+        }
+    }
+
+    #[cfg(not(target_arch = "x86_64"))]
     match nc::fstatat(nc::AT_FDCWD, "/etc/passwd", &mut statbuf) {
         Ok(_) => {
             println!("s: {:?}", statbuf);

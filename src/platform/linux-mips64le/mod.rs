11dc3adab91b6d752b84a2dbacabc1823ad782ba
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-02 17:37:25 +0800

    linux-mips64le: Update syscall instructions

--- src/platform/linux-mips64le/mod.rs
+++ src/platform/linux-mips64le/mod.rs
@@ -24,51 +24,52 @@ pub fn check_errno(ret: usize) -> Result<usize, Errno> {
 #[inline(always)]
 pub unsafe fn syscall0(n: Sysno) -> Result<usize, Errno> {
     let ret: usize;
-    asm!("svc 0"
-         : "={x0}"(ret)
-         : "{x8}"(n)
-         : "memory" "cc"
+    let mut n = n;
+    asm!("syscall"
+         : "+&{r2}"(n), "={r7}"(ret)
+         :
+         : "$1", "$3", "$10", "$11", "$12", "$13", "$14", "$15", "$24", "$25", "hi", "lo", "memory"
          : "volatile");
-    check_errno(ret)
+    check_errno(n)
 }
 
 #[inline(always)]
 pub unsafe fn syscall1(n: Sysno, a1: usize) -> Result<usize, Errno> {
     let ret: usize;
-    asm!("svc 0"
-         : "={x0}"(ret)
-         : "{x8}"(n),
-           "{x0}"(a1)
-         : "memory" "cc"
+    let mut n = n;
+    asm!("syscall"
+         : "+&{r2}"(n), "={r7}"(ret)
+         : "{r4}"(a1)
+         : "$1", "$3", "$10", "$11", "$12", "$13", "$14", "$15", "$24", "$25", "hi", "lo", "memory"
          : "volatile");
-    check_errno(ret)
+    check_errno(n)
 }
 
 #[inline(always)]
 pub unsafe fn syscall2(n: Sysno, a1: usize, a2: usize) -> Result<usize, Errno> {
     let ret: usize;
-    asm!("svc 0"
-         : "={x0}"(ret)
-         : "{x8}"(n),
-           "{x0}"(a1),
-           "{x1}"(a2)
-         : "memory" "cc"
+    let mut n = n;
+    asm!("syscall"
+         : "+&{r2}"(n), "={r7}"(ret)
+         : "{r4}"(a1),
+           "{r5}"(a2)
+         : "$1", "$3", "$10", "$11", "$12", "$13", "$14", "$15", "$24", "$25", "hi", "lo", "memory"
          : "volatile");
-    check_errno(ret)
+    check_errno(n)
 }
 
 #[inline(always)]
 pub unsafe fn syscall3(n: Sysno, a1: usize, a2: usize, a3: usize) -> Result<usize, Errno> {
     let ret: usize;
-    asm!("svc 0"
-         : "={x0}"(ret)
-         : "{x8}"(n),
-           "{x0}"(a1),
-           "{x1}"(a2),
-           "{x2}"(a3)
-         : "memory" "cc"
+    let mut n = n;
+    asm!("syscall"
+         : "+&{r2}"(n), "={r7}"(ret)
+         : "{r4}"(a1),
+           "{r5}"(a2),
+           "{r6}"(a3)
+         : "$1", "$3", "$10", "$11", "$12", "$13", "$14", "$15", "$24", "$25", "hi", "lo", "memory"
          : "volatile");
-    check_errno(ret)
+    check_errno(n)
 }
 
 #[inline(always)]
@@ -79,17 +80,16 @@ pub unsafe fn syscall4(
     a3: usize,
     a4: usize,
 ) -> Result<usize, Errno> {
-    let ret: usize;
-    asm!("svc 0"
-         : "={x0}"(ret)
-         : "{x8}"(n),
-           "{x0}"(a1),
-           "{x1}"(a2),
-           "{x2}"(a3),
-           "{x3}"(a4)
-         : "memory" "cc"
+    let mut n = n;
+    let mut a4 = a4;
+    asm!("syscall"
+         : "+&{r2}"(n), "+{r7}"(a4)
+         : "{r4}"(a1),
+           "{r5}"(a2),
+           "{r6}"(a3)
+         : "$1", "$3", "$10", "$11", "$12", "$13", "$14", "$15", "$24", "$25", "hi", "lo", "memory"
          : "volatile");
-    check_errno(ret)
+    check_errno(n)
 }
 
 #[inline(always)]
@@ -101,18 +101,17 @@ pub unsafe fn syscall5(
     a4: usize,
     a5: usize,
 ) -> Result<usize, Errno> {
-    let ret: usize;
-    asm!("svc 0"
-         : "={x0}"(ret)
-         : "{x8}"(n),
-           "{x0}"(a1),
-           "{x1}"(a2),
-           "{x2}"(a3),
-           "{x3}"(a4),
-           "{x4}"(a5)
-         : "memory" "cc"
+    let mut n = n;
+    let mut a4 = a4;
+    asm!("syscall"
+         : "+&{r2}"(n), "+{r7}"(a4)
+         : "{r4}"(a1),
+           "{r5}"(a2),
+           "{r6}"(a3),
+           "{r8}"(a5)
+         : "$1", "$3", "$10", "$11", "$12", "$13", "$14", "$15", "$24", "$25", "hi", "lo", "memory"
          : "volatile");
-    check_errno(ret)
+    check_errno(n)
 }
 
 #[inline(always)]
@@ -125,17 +124,16 @@ pub unsafe fn syscall6(
     a5: usize,
     a6: usize,
 ) -> Result<usize, Errno> {
-    let ret: usize;
-    asm!("svc 0"
-         : "={x0}"(ret)
-         : "{x8}"(n),
-           "{x0}"(a1),
-           "{x1}"(a2),
-           "{x2}"(a3),
-           "{x3}"(a4),
-           "{x4}"(a5),
-           "{x5}"(a6)
-         : "memory" "cc"
+    let mut n = n;
+    let mut a4 = a4;
+    asm!("syscall"
+         : "+&{r2}"(n), "+{r7}"(a4)
+         : "{r4}"(a1),
+           "{r5}"(a2),
+           "{r6}"(a3),
+           "{r8}"(a5),
+           "{r9}"(a6)
+         : "$1", "$3", "$10", "$11", "$12", "$13", "$14", "$15", "$24", "$25", "hi", "lo", "memory"
          : "volatile");
-    check_errno(ret)
+    check_errno(n)
 }

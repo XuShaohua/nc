f23f299a41e286c07e85c035297e5e9daba6317e
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-02 11:59:33 +0800

    examples: Replace with existing syscalls

--- examples/rename.rs
+++ examples/rename.rs
@@ -1,12 +1,11 @@
-
 extern crate nc;
 
 fn main() {
-    let ret = nc::rename("/tmp/hello.rs", "/tmp/hello2.rs");
+    let ret = nc::renameat(nc::AT_FDCWD, "hello.rs", nc::AT_FDCWD, "world.rs");
     match ret {
         Ok(_) => {
             println!("Rename file success!");
-        },
+        }
         Err(errno) => {
             eprintln!("Failed to rename file, errno is: {}", errno);
         }
--- examples/stat.rs
+++ examples/stat.rs
@@ -1,12 +1,11 @@
-
 extern crate nc;
 
 fn main() {
     let mut statbuf = nc::stat_t::default();
-    match nc::stat("/etc/passwd", &mut statbuf) {
+    match nc::fstatat(nc::AT_FDCWD, "/etc/passwd", &mut statbuf) {
         Ok(_) => {
             println!("s: {:?}", statbuf);
-        },
+        }
         Err(errno) => {
             eprintln!("Failed to get file status, got errno: {}", errno);
         }

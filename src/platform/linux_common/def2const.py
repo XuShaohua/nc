d17260b3ee0f60959645f8054f52b9ed6f54a0a2
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-03 19:26:10 +0800

    Add fcntl binding

--- src/platform/linux_common/def2const.py
+++ src/platform/linux_common/def2const.py
@@ -10,12 +10,12 @@ def main():
         print("Usage: %s input-file" % sys.argv[0])
         sys.exit(1)
 
-    macro_pattern = re.compile("#define\s+(\w+)\s+(\w+)\n")
+    macro_pattern = re.compile("#define\s+(\w+)\s+(\w+)(.*)\n")
     with open(sys.argv[1]) as fh:
         for line in fh:
             m = macro_pattern.match(line)
             if m:
-                print("pub const {}: i32 = {};".format(m.group(1), m.group(2)))
+                print("pub const {}: i32 = {};{}".format(m.group(1), m.group(2), m.group(3)))
             else:
                 print(line, end="")
 

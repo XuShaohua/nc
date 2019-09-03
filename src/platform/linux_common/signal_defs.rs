25d0ef4c263aedd2c20a9afd2915ac5f101da2de
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-03 22:11:11 +0800

    Add signal-defs.rs

---
+++ src/platform/linux_common/signal_defs.rs
@@ -0,0 +1,20 @@
+/// for blocking signals
+pub const SIG_BLOCK: i32 = 0;
+/// for unblocking signals
+pub const SIG_UNBLOCK: i32 = 1;
+/// for setting the signal mask
+pub const SIG_SETMASK: i32 = 2;
+
+// TODO(Shaohua):
+//pub type signalfn_t = Fn<i32> -> !;
+//typedef void __signalfn_t(int);
+//typedef __signalfn_t __user *__sighandler_t;
+//typedef void __restorefn_t(void);
+//typedef __restorefn_t __user *__sigrestore_t;
+
+/// default signal handling
+pub const SIG_DFL: sighandler_t = 0;
+/// ignore signal
+pub const SIG_IGN: sighandler_t = 1;
+/// error return from signal
+pub const SIG_ERR: sighandler_t = -1;

16f4b4bad10fa8caac68bf1a295ba0b48753f7f1
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-12 07:40:03 +0800

    linux-types: Add linux_quota.rs

---
+++ src/platform/linux-types/linux_quota.rs
@@ -0,0 +1,19 @@
+/// element used for user quotas
+pub const USRQUOTA: i32 = 0;
+/// element used for group quotas
+pub const GRPQUOTA: i32 = 1;
+/// element used for project quotas
+pub const PRJQUOTA: i32 = 2;
+
+/// Masks for quota types when used as a bitmask
+pub const QTYPE_MASK_USR: i32 = (1 << USRQUOTA);
+pub const QTYPE_MASK_GRP: i32 = (1 << GRPQUOTA);
+pub const QTYPE_MASK_PRJ: i32 = (1 << PRJQUOTA);
+
+/// Type in which we store ids in memory
+pub type qid_t = i32;
+
+/// Type in which we store sizes
+pub type qsize_t = i64;
+
+// Other types are ignored!

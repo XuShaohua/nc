e416d3ae7c2c624d2d349059d16c642e6537063d
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-13 21:38:27 +0800

    linux-call: Implement `mbind()`

--- src/platform/linux-arm/call.rs
+++ src/platform/linux-arm/call.rs
@@ -1425,9 +1425,21 @@ pub fn madvise(addr: usize, len: size_t, advice: i32) -> Result<(), Errno> {
     }
 }
 
-pub fn mbind() {
-    core::unimplemented!();
-    // syscall0(SYS_MBIND);
+/// Set memory policy for a memory range.
+pub fn mbind(
+    start: usize,
+    len: usize,
+    mode: i32,
+    nmask: *const usize,
+    maxnode: usize,
+    flags: i32,
+) -> Result<(), Errno> {
+    unsafe {
+        let mode = mode as usize;
+        let nmask = nmask as usize;
+        let flags = flags as usize;
+        syscall6(SYS_MBIND, start, len, mode, nmask, maxnode, flags).map(|_ret| ())
+    }
 }
 
 pub fn membarrier() {

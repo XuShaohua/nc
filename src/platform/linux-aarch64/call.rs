656af2c55fc5153996bdd09443ebe4c0e8f0981d
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-15 02:30:26 +0800

    linux-call: Implement `kcmp()`

--- src/platform/linux-aarch64/call.rs
+++ src/platform/linux-aarch64/call.rs
@@ -1106,9 +1106,14 @@ pub fn io_uring_setup() {
     // syscall0(SYS_IO_URING_SETUP);
 }
 
-pub fn kcmp() {
-    core::unimplemented!();
-    // syscall0(SYS_KCMP);
+/// Compare two processes to determine if they share a kernel resource.
+pub fn kcmp(pid1: pid_t, pid2: pid_t, type_: i32, idx1: usize, idx2: usize) -> Result<i32, Errno> {
+    unsafe {
+        let pid1 = pid1 as usize;
+        let pid2 = pid2 as usize;
+        let type_ = type_ as usize;
+        syscall5(SYS_KCMP, pid1, pid2, type_, idx1, idx2).map(|ret| ret as i32)
+    }
 }
 
 pub fn kexec_file_load() {

24523eab9c5ea2eac0200d7ab6d17f3185270598
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-17 17:31:42 +0800

    linux-call: Implement io_pgetevents()

--- src/README.md
+++ src/README.md
@@ -46,3 +46,4 @@ mirrors do not support non-x86 architectures
 - [ ] simplify syscalls
 - [ ] fix siginfo_t
 - [ ] u64 to usize error
+- [ ] struct *timespec_t timeout is NULL or not
--- src/platform/linux-x86/call.rs
+++ src/platform/linux-x86/call.rs
@@ -1433,9 +1433,33 @@ pub fn io_getevents(
     }
 }
 
-pub fn io_pgetevents() {
-    core::unimplemented!();
-    // syscall0(SYS_IO_PGETEVENTS);
+/// read asynchronous I/O events from the completion queue
+pub fn io_pgetevents(
+    ctx_id: aio_context_t,
+    min_nr: isize,
+    nr: isize,
+    events: &mut io_event_t,
+    timeout: &mut timespec_t,
+    usig: &aio_sigset_t,
+) -> Result<i32, Errno> {
+    unsafe {
+        let ctx_id = ctx_id as usize;
+        let min_nr = min_nr as usize;
+        let nr = nr as usize;
+        let events_ptr = events as *mut io_event_t as usize;
+        let timeout_ptr = timeout as *mut timespec_t as usize;
+        let usig_ptr = usig as *const aio_sigset_t as usize;
+        syscall6(
+            SYS_IO_PGETEVENTS,
+            ctx_id,
+            min_nr,
+            nr,
+            events_ptr,
+            timeout_ptr,
+            usig_ptr,
+        )
+        .map(|ret| ret as i32)
+    }
 }
 
 pub fn io_pgetevents_time64() {

9d387650307aef05704edfe04fe8bc1df13b62ae
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-17 11:48:12 +0800

    linux-call: Implement `rt_sigqueueinfo()`

--- src/platform/linux-mips/call.rs
+++ src/platform/linux-mips/call.rs
@@ -2754,9 +2754,14 @@ pub fn rt_sigprocmask(how: i32, set: &sigset_t, oldset: &mut sigset_t) -> Result
     }
 }
 
-pub fn rt_sigqueueinfo() {
-    core::unimplemented!();
-    // syscall0(SYS_RT_SIGQUEUEINFO);
+/// Queue a signal and data.
+pub fn rt_sigqueueinfo(pid: pid_t, sig: i32, uinfo: &mut siginfo_t) -> Result<(), Errno> {
+    unsafe {
+        let pid = pid as usize;
+        let sig = sig as usize;
+        let uinfo_ptr = uinfo as *mut siginfo_t as usize;
+        syscall3(SYS_RT_SIGQUEUEINFO, pid, sig, uinfo_ptr).map(|_ret| ())
+    }
 }
 
 pub fn rt_sigreturn() {

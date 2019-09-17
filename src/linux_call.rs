b31af776282d68f1be3841dae428a60a8fd72e57
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-17 17:24:25 +0800

    linux-call: Implements io_getevents()

--- src/linux_call.rs
+++ src/linux_call.rs
@@ -3077,9 +3077,39 @@ pub fn io_destroy(ctx_id: aio_context_t) -> Result<(), Errno> {
     }
 }
 
-pub fn io_getevents() {
-    core::unimplemented!();
-    // syscall0(SYS_IO_GETEVENTS);
+/// Attempts to read at least min_nr events and up to nr events from
+/// the completion queue for the aio_context specified by ctx_id. If
+/// it succeeds, the number of read events is returned. May fail with
+/// -EINVAL if ctx_id is invalid, if min_nr is out of range, if nr is
+/// out of range, if timeout is out of range.  May fail with -EFAULT
+/// if any of the memory specified is invalid.  May return 0 or
+/// < min_nr if the timeout specified by timeout has elapsed
+/// before sufficient events are available, where timeout == NULL
+/// specifies an infinite timeout. Note that the timeout pointed to by
+/// timeout is relative.  Will fail with -ENOSYS if not implemented.
+pub fn io_getevents(
+    ctx_id: aio_context_t,
+    min_nr: isize,
+    nr: isize,
+    events: &mut io_event_t,
+    timeout: &mut timespec_t,
+) -> Result<i32, Errno> {
+    unsafe {
+        let ctx_id = ctx_id as usize;
+        let min_nr = min_nr as usize;
+        let nr = nr as usize;
+        let events_ptr = events as *mut io_event_t as usize;
+        let timeout_ptr = timeout as *mut timespec_t as usize;
+        syscall5(
+            SYS_IO_GETEVENTS,
+            ctx_id,
+            min_nr,
+            nr,
+            events_ptr,
+            timeout_ptr,
+        )
+        .map(|ret| ret as i32)
+    }
 }
 
 pub fn io_pgetevents() {

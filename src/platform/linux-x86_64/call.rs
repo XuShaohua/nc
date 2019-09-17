a8c9c2519f440e6de779986853ee20fd39ae32f2
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-17 16:56:31 +0800

    linux-call: Implement ppoll()

--- src/platform/linux-x86_64/call.rs
+++ src/platform/linux-x86_64/call.rs
@@ -2111,9 +2111,30 @@ pub fn poll(fds: &mut [pollfd_t], timeout: i32) -> Result<(), Errno> {
     }
 }
 
-pub fn ppoll() {
-    core::unimplemented!();
-    // syscall0(SYS_PPOLL);
+/// Wait for some event on a file descriptor.
+pub fn ppoll(
+    fds: &mut pollfd_t,
+    nfds: nfds_t,
+    timeout: &timespec_t,
+    sigmask: &sigset_t,
+    sigsetsize: size_t,
+) -> Result<i32, Errno> {
+    unsafe {
+        let fds_ptr = fds as *mut pollfd_t as usize;
+        let nfds = nfds as usize;
+        let timeout_ptr = timeout as *const timespec_t as usize;
+        let sigmask_ptr = sigmask as *const sigset_t as usize;
+        let sigsetsize = sigsetsize as usize;
+        syscall5(
+            SYS_PPOLL,
+            ndfs_ptr,
+            nfds,
+            timeout_ptr,
+            sigmask_ptr,
+            sigsetsize,
+        )
+        .map(|ret| ret as i32)
+    }
 }
 
 /// Operations on a process.

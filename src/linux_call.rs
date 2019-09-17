d99b32cdfb286a7e199a5ddf4b039585c5f3aa3b
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-17 11:54:16 +0800

    linux-call: Implement `sigprocmask()`

--- src/linux_call.rs
+++ src/linux_call.rs
@@ -4087,9 +4087,14 @@ pub fn sigpending(set: &mut sigset_t) -> Result<(), Errno> {
     }
 }
 
-pub fn sigprocmask() {
-    core::unimplemented!();
-    // syscall0(SYS_SIGPROCMASK);
+/// Examine and change blocked signals.
+pub fn sigprocmask(how: i32, newset: &mut sigset_t, oldset: &mut sigset_t) -> Result<(), Errno> {
+    unsafe {
+        let how = how as usize;
+        let newset_ptr = newset as *mut sigset_t as usize;
+        let oldset_ptr = oldset as *mut sigset_t as usize;
+        syscall3(SYS_SIGPROCMASK, how, newset_ptr, oldset_ptr).map(|_ret| ())
+    }
 }
 
 pub fn sigreturn() {

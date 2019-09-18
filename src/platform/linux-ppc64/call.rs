37a399419d14c4c749f52dacef3e7805cb928edd
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-18 11:48:27 +0800

    linux-call: Implement io_uring_enter()

--- src/platform/linux-ppc64/call.rs
+++ src/platform/linux-ppc64/call.rs
@@ -1382,9 +1382,32 @@ pub fn io_submit(ctx_id: aio_context_t, nr: isize, iocb: &mut iocb_t) -> Result<
     }
 }
 
-pub fn io_uring_enter() {
-    core::unimplemented!();
-    // syscall0(SYS_IO_URING_ENTER);
+pub fn io_uring_enter(
+    fd: i32,
+    to_submit: u32,
+    min_complete: u32,
+    flags: u32,
+    sig: &sigset_t,
+    sigsetsize: size_t,
+) -> Result<i32, Errno> {
+    unsafe {
+        let fd = fd as usize;
+        let to_submit = to_submit as usize;
+        let min_complete = min_complete as usize;
+        let flags = flags as usize;
+        let sig_ptr = sig as *const sigset_t as usize;
+        let sigsetsize = sigsetsize as usize;
+        syscall6(
+            SYS_IO_URING_ENTER,
+            fd,
+            to_submit,
+            min_complete,
+            flags,
+            sig_ptr,
+            sigsetsize,
+        )
+        .map(|ret| ret as i32)
+    }
 }
 
 pub fn io_uring_register() {

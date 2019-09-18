c2e2e5e2fef233f40dbfc349dfc48cb4d5bf85f8
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-18 10:42:31 +0800

    linux-call: Implement recv()

--- src/linux_call.rs
+++ src/linux_call.rs
@@ -4575,9 +4575,15 @@ pub fn pciconfig_write(
     unsafe { syscall5(SYS_PCICONFIG_WRITE, bus, dfn, off, len, buf).map(|_ret| ()) }
 }
 
-pub fn recv() {
-    core::unimplemented!();
-    // syscall0(SYS_RECV);
+/// Receive a datagram from a socket.
+pub fn recv(sockfd: i32, buf: &mut [u8], flags: i32) -> Result<ssize_t, Errno> {
+    unsafe {
+        let sockfd = sockfd as usize;
+        let buf_ptr = buf.as_mut_ptr() as usize;
+        let buflen = buf.len();
+        let flags = flags as usize;
+        syscall4(SYS_RECV, sockfd, buf_ptr, buflen, flags).map(|ret| ret as ssize_t)
+    }
 }
 
 pub fn rtas() {
--- src/platform/linux-ppc64/call.rs
+++ src/platform/linux-ppc64/call.rs
@@ -2665,9 +2665,15 @@ pub fn reboot(magic: i32, magci2: i32, cmd: u32, arg: usize) -> Result<(), Errno
     }
 }
 
-pub fn recv() {
-    core::unimplemented!();
-    // syscall0(SYS_RECV);
+/// Receive a datagram from a socket.
+pub fn recv(sockfd: i32, buf: &mut [u8], flags: i32) -> Result<ssize_t, Errno> {
+    unsafe {
+        let sockfd = sockfd as usize;
+        let buf_ptr = buf.as_mut_ptr() as usize;
+        let buflen = buf.len();
+        let flags = flags as usize;
+        syscall4(SYS_RECV, sockfd, buf_ptr, buflen, flags).map(|ret| ret as ssize_t)
+    }
 }
 
 /// Receive a message from a socket.

aaef64e2adfef750db65522460cc2fa27980ff72
Xu Shaohua <xushaohua2016@outlook.com>
2019-09-18 12:45:42 +0800

    linux-call: Implement move_mount()

--- src/platform/linux-arm/call.rs
+++ src/platform/linux-arm/call.rs
@@ -1921,9 +1921,37 @@ pub fn mount(
     }
 }
 
-pub fn move_mount() {
-    core::unimplemented!();
-    // syscall0(SYS_MOVE_MOUNT);
+/// Move a mount from one place to another. In combination with
+/// fsopen()/fsmount() this is used to install a new mount and in combination
+/// with open_tree(OPEN_TREE_CLONE [| AT_RECURSIVE]) it can be used to copy
+/// a mount subtree.
+///
+/// Note the flags value is a combination of MOVE_MOUNT_* flags.
+pub fn move_mount(
+    from_dfd: i32,
+    from_pathname: &str,
+    to_dfd: i32,
+    to_pathname: &str,
+    flags: u32,
+) -> Result<i32, Errno> {
+    unsafe {
+        let from_dfd = from_dfd as usize;
+        let from_pathname = CString::new(from_pathname);
+        let from_pathname_ptr = from_pathname.as_ptr() as usize;
+        let to_dfd = to_dfd as usize;
+        let to_pathname = CString::new(to_pathname);
+        let to_pathname_ptr = to_pathname.as_ptr() as usize;
+        let flags = flags as usize;
+        syscall5(
+            SYS_MOVE_MOUNT,
+            from_dfd,
+            from_pathname_ptr,
+            to_dfd,
+            to_pathname_ptr,
+            flags,
+        )
+        .map(|ret| ret as i32)
+    }
 }
 
 /// Move individual pages of a process to another node

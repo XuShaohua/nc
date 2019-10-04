8ff9e12c86f882a4f92865fbe9a6bcc152c7c21a
Xu Shaohua <xushaohua2016@outlook.com>
2019-10-04 10:43:51 +0800

    build.rs: Fix process error

--- build.rs
+++ build.rs
@@ -5,8 +5,8 @@ use std::path::Path;
 use std::process::Command;
 
 fn build_stable() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let out_dir = env::var("OUT_DIR").expect("OUT_DIR undefined in env");
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").expect("TARGET_ARCH undefined in env");
     let syscall_file = format!("src/syscalls/syscall_{}.c", target_arch);
     let obj_file = format!("{}/syscall.o", out_dir);
     println!("syscall file: {}", syscall_file);
@@ -17,20 +17,20 @@ fn build_stable() {
         .args(&[&syscall_file, "-c", "-fPIC", "-o"])
         .arg(&obj_file)
         .status()
-        .unwrap();
+        .expect("gcc returns error");
 
     Command::new("ar")
         .args(&["crs", "libsyscall.a", "syscall.o"])
         .current_dir(&Path::new(&out_dir))
         .status()
-        .unwrap();
+        .expect("ar returns error");
 
     println!("cargo:rustc-link-search=native={}", out_dir);
     println!("cargo:rustc-link-lib=static=syscall");
 }
 
 fn main() {
-    let rustc_toolchain = env::var("RUSTUP_TOOLCHAIN").unwrap();
+    let rustc_toolchain = env::var("RUSTUP_TOOLCHAIN").expect("TOOLCHAIN undefined in env");
     if rustc_toolchain.starts_with("nightly") {
         println!("cargo:rustc-cfg=nightly");
     } else {
--- ci/README.md
+++ ci/README.md
@@ -3,3 +3,17 @@ About
 =====
 
 This ci config is used to test non-x86 architectures, based on debian rootfs.
+
+
+## debian
+If cargo package is installed with apt command like:
+```bash
+sudo apt install cargo
+```
+
+environment named `RUSTUP_TOOLCHAIN` is undefined.
+
+We can pass this environment to cargo by hand:
+```bash
+RUSTUP_TOOLCHAIN=stable-x86_64-unknown-linux-gnu cargo build
+```

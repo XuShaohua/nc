f170af185ca7c78f1616ca83636492224933f506
Xu Shaohua <shaohua@biofan.org>
2020-05-27 23:19:02 +0800

    example: Add nproc

---
+++ examples/nproc.rs
@@ -0,0 +1,26 @@
+// Copyright (c) 2020 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+pub fn get_num_cpu() -> Result<u16, nc::Errno> {
+    let mut set: [u8; 128] = [0; 128];
+    set[0] = 1;
+    let set_ptr = set.as_ptr() as usize;
+
+    nc::sched_getaffinity(0, set.len() as u32, set_ptr)?;
+
+    let mut count = 0;
+    for item in set.iter() {
+        let mut item = *item;
+        while item > 0 {
+            item &= item - 1;
+            count += 1;
+        }
+    }
+
+    return Ok(count);
+}
+
+fn main() {
+    println!("num of cpu: {}", get_num_cpu().unwrap());
+}

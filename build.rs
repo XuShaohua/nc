1579baef300832d11b5f718c6eadf22683e891fc
Xu Shaohua <shaohua@biofan.org>
2020-09-30 21:21:39 +0800

    build: Compile syscall c file with `cc` crate (works for aarch64)

--- build.rs
+++ build.rs
@@ -3,35 +3,17 @@
 // in the LICENSE file.
 
 use std::env;
-use std::path::Path;
-use std::process::Command;
 
 fn build_syscalls() {
-    let out_dir = env::var("OUT_DIR").unwrap();
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     let syscall_file = format!("src/syscalls/syscall_{}.c", target_arch);
-    let obj_file = format!("{}/syscall.o", out_dir);
     println!("syscall file: {}", syscall_file);
-    println!("obj file: {}", obj_file);
 
-    Command::new("cc")
-        .args(&[&syscall_file, "-c", "-fPIC", "-o"])
-        .arg(&obj_file)
-        .status()
-        .unwrap();
-
-    Command::new("ar")
-        .args(&["crs", "libsyscall.a", "syscall.o"])
-        .current_dir(&Path::new(&out_dir))
-        .status()
-        .unwrap();
-
-    println!("cargo:rustc-link-search=native={}", out_dir);
-    println!("cargo:rustc-link-lib=static=syscall");
+    cc::Build::new().file(syscall_file).compile("syscall");
 }
 
 fn main() {
-    let rustc_toolchain = env::var("RUSTUP_TOOLCHAIN").expect("TOOLCHAIN undefined in env");
+    let rustc_toolchain = env::var("RUSTUP_TOOLCHAIN").unwrap_or("stable".to_string());
     if rustc_toolchain.starts_with("nightly") {
         println!("cargo:rustc-cfg=nightly");
     } else {

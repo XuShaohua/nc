3a14aa4396bb2c5901cc93905c8d902085e44b97
Xu Shaohua <shaohua@biofan.org>
2021-01-06 08:21:00 +0800

    linux: Add getresuid() unittest

--- src/platform/linux-arm/call.rs
+++ src/platform/linux-arm/call.rs
@@ -1078,6 +1078,16 @@ pub fn getresgid32() {
 }
 
 /// Get real, effect and saved user ID.
+/// ```
+/// let mut ruid = 0;
+/// let mut euid = 0;
+/// let mut suid = 0;
+/// let ret = nc::getresuid(&mut ruid, &mut euid, &mut suid);
+/// assert!(ret.is_ok());
+/// assert!(ruid > 0);
+/// assert!(euid > 0);
+/// assert!(suid > 0);
+/// ```
 pub fn getresuid(ruid: &mut uid_t, euid: &mut uid_t, suid: &mut uid_t) -> Result<(), Errno> {
     let ruid_ptr = ruid as *mut uid_t as usize;
     let euid_ptr = euid as *mut uid_t as usize;

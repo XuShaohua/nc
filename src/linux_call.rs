0f35032561ab1dcb2916ed4be78426d6776842ec
Xu Shaohua <shaohua@biofan.org>
2021-01-09 10:35:02 +0800

    linux: Add fstat64() unittest

--- src/linux_call.rs
+++ src/linux_call.rs
@@ -3743,6 +3743,19 @@ pub fn fcntl64(fd: i32, cmd: i32, arg: usize) -> Result<i32, Errno> {
 }
 
 /// Get file status.
+/// ```
+/// let path = "/tmp";
+/// // Open folder directly.
+/// let fd = nc::open(path, nc::O_PATH, 0);
+/// assert!(fd.is_ok());
+/// let fd = fd.unwrap();
+/// let mut stat = nc::stat_t::default();
+/// let ret = nc::fstat63(fd, &mut stat);
+/// assert!(ret.is_ok());
+/// // Check fd is a directory.
+/// assert_eq!((stat.st_mode & nc::S_IFMT), nc::S_IFDIR);
+/// assert!(nc::close(fd).is_ok());
+/// ```
 pub fn fstat64(fd: i32, statbuf: &mut stat64_t) -> Result<(), Errno> {
     let fd = fd as usize;
     let statbuf_ptr = statbuf as *mut stat64_t as usize;
--- src/platform/linux-x86/call.rs
+++ src/platform/linux-x86/call.rs
@@ -919,6 +919,19 @@ pub fn fstat(fd: i32, statbuf: &mut stat_t) -> Result<(), Errno> {
 }
 
 /// Get file status.
+/// ```
+/// let path = "/tmp";
+/// // Open folder directly.
+/// let fd = nc::open(path, nc::O_PATH, 0);
+/// assert!(fd.is_ok());
+/// let fd = fd.unwrap();
+/// let mut stat = nc::stat_t::default();
+/// let ret = nc::fstat64(fd, &mut stat);
+/// assert!(ret.is_ok());
+/// // Check fd is a directory.
+/// assert_eq!((stat.st_mode & nc::S_IFMT), nc::S_IFDIR);
+/// assert!(nc::close(fd).is_ok());
+/// ```
 pub fn fstat64(fd: i32, statbuf: &mut stat64_t) -> Result<(), Errno> {
     let fd = fd as usize;
     let statbuf_ptr = statbuf as *mut stat64_t as usize;

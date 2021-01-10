2871a5aab2862da4e88b126929d84e12985b15fd
Xu Shaohua <shaohua@biofan.org>
2021-01-11 07:49:32 +0800

    linux: Add read() unittest

--- src/platform/linux-mips/call.rs
+++ src/platform/linux-mips/call.rs
@@ -2868,6 +2868,18 @@ pub fn quotactl(cmd: i32, special: &str, id: qid_t, addr: usize) -> Result<(), E
 }
 
 /// Read from a file descriptor.
+/// ```
+/// let path = "/etc/passwd";
+/// let ret = nc::openat(nc::AT_FDCWD, path, nc::O_RDONLY, 0);
+/// assert!(ret.is_ok());
+/// let fd = ret.unwrap();
+/// let mut buf = [0_u8; 4 * 1024];
+/// let ret = nc::read(fd, buf.as_mut_ptr() as usize, buf.len());
+/// assert!(ret.is_ok());
+/// let n_read = ret.unwrap();
+/// assert!(n_read <= buf.len() as isize);
+/// assert!(nc::close(fd).is_ok());
+/// ```
 pub fn read(fd: i32, buf_ptr: usize, count: size_t) -> Result<ssize_t, Errno> {
     let fd = fd as usize;
     syscall3(SYS_READ, fd, buf_ptr, count).map(|ret| ret as ssize_t)

4b519a2ead1680816e410645b43b691e8f7b20ce
Xu Shaohua <shaohua@biofan.org>
2021-01-12 10:32:02 +0800

    linux: Fix getrusage() test failed

--- src/platform/linux-x86_64/call.rs
+++ src/platform/linux-x86_64/call.rs
@@ -1241,7 +1241,7 @@ pub fn getrlimit(resource: i32, rlim: &mut rlimit_t) -> Result<(), Errno> {
 /// let mut usage = nc::rusage_t::default();
 /// let ret = nc::getrusage(nc::RUSAGE_SELF, &mut usage);
 /// assert!(ret.is_ok());
-/// assert_eq!(usage.ru_nswap, 0);
+/// assert!(usage.ru_utime.tv_usec > 0);
 /// assert!(usage.ru_maxrss > 0);
 /// ```
 pub fn getrusage(who: i32, usage: &mut rusage_t) -> Result<(), Errno> {

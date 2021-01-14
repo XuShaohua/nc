72b680dcaf598e33c597888bfd32e17704555577
Xu Shaohua <shaohua@biofan.org>
2021-01-14 11:13:58 +0800

    linux: Add writev() unittest

--- src/platform/linux-x86_64/call.rs
+++ src/platform/linux-x86_64/call.rs
@@ -3207,7 +3207,7 @@ pub fn pwrite64(fd: i32, buf: usize, count: size_t, offset: off_t) -> Result<ssi
 /// assert!(ret.is_ok());
 /// assert_eq!(ret, Ok(capacity as nc::ssize_t));
 /// assert!(nc::close(fd).is_ok());
-
+///
 /// let path_out = "/tmp/nc-pwritev";
 /// let ret = nc::open(path_out, nc::O_WRONLY | nc::O_CREAT, 0o644);
 /// assert!(ret.is_ok());
@@ -3216,6 +3216,7 @@ pub fn pwrite64(fd: i32, buf: usize, count: size_t, offset: off_t) -> Result<ssi
 /// assert!(ret.is_ok());
 /// assert_eq!(ret, Ok(capacity as nc::ssize_t));
 /// assert!(nc::close(fd).is_ok());
+/// assert!(nc::unlink(path_out).is_ok());
 /// ```
 pub fn pwritev(fd: i32, vec: &[iovec_t], pos_l: usize, pos_h: usize) -> Result<ssize_t, Errno> {
     let fd = fd as usize;
@@ -4721,6 +4722,34 @@ pub fn write(fd: i32, buf_ptr: usize, count: size_t) -> Result<ssize_t, Errno> {
 }
 
 /// Write to a file descriptor from multiple buffers.
+/// ```
+/// let path = "/etc/passwd";
+/// let ret = nc::open(path, nc::O_RDONLY, 0);
+/// assert!(ret.is_ok());
+/// let fd = ret.unwrap();
+/// let mut buf = [[0_u8; 64]; 4];
+/// let capacity = 4 * 64;
+/// let mut iov = Vec::with_capacity(buf.len());
+/// for ref mut item in (&mut buf).iter() {
+///     iov.push(nc::iovec_t {
+///         iov_len: item.len(),
+///         iov_base: item.as_ptr() as usize,
+///     });
+/// }
+/// let ret = nc::readv(fd, &mut iov);
+/// assert!(ret.is_ok());
+/// assert_eq!(ret, Ok(capacity as nc::ssize_t));
+/// assert!(nc::close(fd).is_ok());
+///
+/// let path_out = "/tmp/nc-writev";
+/// let ret = nc::open(path_out, nc::O_WRONLY | nc::O_CREAT, 0o644);
+/// assert!(ret.is_ok());
+/// let fd = ret.unwrap();
+/// let ret = nc::writev(fd, &iov);
+/// assert!(ret.is_ok());
+/// assert_eq!(ret, Ok(capacity as nc::ssize_t));
+/// assert!(nc::close(fd).is_ok());
+/// ```
 pub fn writev(fd: i32, iov: &[iovec_t]) -> Result<ssize_t, Errno> {
     let fd = fd as usize;
     let iov_ptr = iov.as_ptr() as usize;

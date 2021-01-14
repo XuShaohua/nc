d89f66ac513621cb8ece88898e4cf30b12ad2cdb
Xu Shaohua <shaohua@biofan.org>
2021-01-14 11:07:01 +0800

    linux: Update preadv2() api

--- src/platform/linux-x86/call.rs
+++ src/platform/linux-x86/call.rs
@@ -3249,21 +3249,42 @@ pub fn pread64(fd: i32, buf: usize, count: usize, offset: off_t) -> Result<ssize
 /// ```
 pub fn preadv(fd: i32, vec: &mut [iovec_t], pos_l: usize, pos_h: usize) -> Result<ssize_t, Errno> {
     let fd = fd as usize;
-    let vec_ptr = vec.as_ptr() as usize;
+    let vec_ptr = vec.as_mut_ptr() as usize;
     let vec_len = vec.len();
     syscall5(SYS_PREADV, fd, vec_ptr, vec_len, pos_l, pos_h).map(|ret| ret as ssize_t)
 }
 
 /// Read from a file descriptor without changing file offset.
+/// ```
+/// let path = "/etc/passwd";
+/// let ret = nc::open(path, nc::O_RDONLY, 0);
+/// assert!(ret.is_ok());
+/// let fd = ret.unwrap();
+/// let mut buf = [[0_u8; 64]; 4];
+/// let capacity = 4 * 64;
+/// let mut iov = Vec::with_capacity(buf.len());
+/// for ref mut item in (&mut buf).iter() {
+///     iov.push(nc::iovec_t {
+///         iov_len: item.len(),
+///         iov_base: item.as_ptr() as usize,
+///     });
+/// }
+/// let iov_len = iov.len();
+/// let flags = 0;
+/// let ret = nc::preadv2(fd, &mut iov, 0, iov_len - 1, flags);
+/// assert!(ret.is_ok());
+/// assert_eq!(ret, Ok(capacity as nc::ssize_t));
+/// assert!(nc::close(fd).is_ok());
+/// ```
 pub fn preadv2(
     fd: i32,
-    vec: &[iovec_t],
+    vec: &mut [iovec_t],
     pos_l: usize,
     pos_h: usize,
     flags: rwf_t,
 ) -> Result<ssize_t, Errno> {
     let fd = fd as usize;
-    let vec_ptr = vec.as_ptr() as usize;
+    let vec_ptr = vec.as_mut_ptr() as usize;
     let vec_len = vec.len();
     let flags = flags as usize;
     syscall6(SYS_PREADV2, fd, vec_ptr, vec_len, pos_l, pos_h, flags).map(|ret| ret as ssize_t)
@@ -3546,6 +3567,7 @@ pub fn readlinkat(dirfd: i32, filename: &str, buf: &mut [u8]) -> Result<ssize_t,
 /// let capacity = 4 * 64;
 /// let mut iov = Vec::with_capacity(buf.len());
 /// for ref mut item in (&mut buf).iter() {
+/// // TODO(Shaohua): Replace with as_mut_ptr()
 ///     iov.push(nc::iovec_t {
 ///         iov_len: item.len(),
 ///         iov_base: item.as_ptr() as usize,

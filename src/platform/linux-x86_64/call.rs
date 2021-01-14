7f0916e94d7c381219f6621298b7f36b900d8af8
Xu Shaohua <shaohua@biofan.org>
2021-01-14 11:03:24 +0800

    linux: Add preadv() unittest

--- src/platform/linux-x86_64/call.rs
+++ src/platform/linux-x86_64/call.rs
@@ -2991,6 +2991,26 @@ pub fn pread64(fd: i32, buf: usize, count: usize, offset: off_t) -> Result<ssize
 }
 
 /// Read from a file descriptor without changing file offset.
+/// ```
+/// let path = "/etc/passwd";
+/// let ret = nc::open(path, nc::O_RDONLY, 0);
+/// assert!(ret.is_ok());
+/// let fd = ret.unwrap();
+/// let mut buf = [[0_u8; 64]; 4];
+/// let capacity = 4 * 64;
+/// let mut iov = Vec::with_capacity(buf.len());
+/// for ref mut item in (&mut buf).iter() {
+///     iov.push(nc::iovec_t {
+///         iov_len: item.len(),
+///         iov_base: item.as_ptr() as usize,
+///     });
+/// }
+/// let iov_len = iov.len();
+/// let ret = nc::preadv(fd, &mut iov, 0, iov_len - 1);
+/// assert!(ret.is_ok());
+/// assert_eq!(ret, Ok(capacity as nc::ssize_t));
+/// assert!(nc::close(fd).is_ok());
+/// ```
 pub fn preadv(fd: i32, vec: &mut [iovec_t], pos_l: usize, pos_h: usize) -> Result<ssize_t, Errno> {
     let fd = fd as usize;
     let vec_ptr = vec.as_ptr() as usize;

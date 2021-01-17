cdae22a26678da83f224217c392b3b85863bf507
Xu Shaohua <shaohua@biofan.org>
2021-01-17 15:02:11 +0800

    linux: Add shmat() unittest

--- src/platform/linux-ppc64/call.rs
+++ src/platform/linux-ppc64/call.rs
@@ -4468,6 +4468,28 @@ pub fn sgetmask() {
 }
 
 /// Attach the System V shared memory segment.
+/// ```
+/// let size = 4 * nc::PAGE_SIZE;
+/// let flags = nc::IPC_CREAT | nc::IPC_EXCL | 0o600;
+/// let ret = nc::shmget(nc::IPC_PRIVATE, size, flags);
+/// assert!(ret.is_ok());
+/// let shmid = ret.unwrap();
+///
+/// let addr: usize = 0;
+/// let ret = nc::shmat(shmid, addr, 0);
+/// assert!(ret.is_ok());
+/// let addr = ret.unwrap();
+///
+/// let mut buf = nc::shmid_ds_t::default();
+/// let ret = nc::shmctl(shmid, nc::IPC_STAT, &mut buf);
+/// assert!(ret.is_ok());
+///
+/// let ret = nc::shmdt(addr);
+/// assert!(ret.is_ok());
+///
+/// let ret = nc::shmctl(shmid, nc::IPC_RMID, &mut buf);
+/// assert!(ret.is_ok());
+/// ```
 pub fn shmat(shmid: i32, shmaddr: usize, shmflg: i32) -> Result<usize, Errno> {
     let shmid = shmid as usize;
     let shmflg = shmflg as usize;

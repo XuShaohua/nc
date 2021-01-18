fc35f495907fba6e0dc1a085103940b46a2ae928
Xu Shaohua <shaohua@biofan.org>
2021-01-18 10:47:18 +0800

    linux: Add fadvise64_64() unittest

--- src/linux_call.rs
+++ src/linux_call.rs
@@ -4938,6 +4938,15 @@ pub fn arch_prctl(code: i32, arg2: usize) -> Result<(), Errno> {
 }
 
 /// Predeclare an access pattern for file data.
+/// let path = "/etc/passwd";
+/// let ret = nc::open(path, nc::O_RDONLY, 0);
+/// assert!(ret.is_ok());
+/// let fd = ret.unwrap();
+/// let offset = 0;
+/// let len = 4 * 1024;
+/// let ret = nc::fadvise64_64(fd, offset, len, nc::POSIX_FADV_NORMAL);
+/// assert!(ret.is_ok());
+/// assert!(nc::close(fd).is_ok());
 pub fn fadvise64_64(fd: i32, offset: loff_t, len: loff_t, advice: i32) -> Result<(), Errno> {
     let fd = fd as usize;
     let offset = offset as usize;
--- src/platform/linux-x86/call.rs
+++ src/platform/linux-x86/call.rs
@@ -689,6 +689,15 @@ pub fn fadvise64(fd: i32, offset: loff_t, len: size_t, advice: i32) -> Result<()
 }
 
 /// Predeclare an access pattern for file data.
+/// let path = "/etc/passwd";
+/// let ret = nc::open(path, nc::O_RDONLY, 0);
+/// assert!(ret.is_ok());
+/// let fd = ret.unwrap();
+/// let offset = 0;
+/// let len = 4 * 1024;
+/// let ret = nc::fadvise64_64(fd, offset, len, nc::POSIX_FADV_NORMAL);
+/// assert!(ret.is_ok());
+/// assert!(nc::close(fd).is_ok());
 pub fn fadvise64_64(fd: i32, offset: loff_t, len: loff_t, advice: i32) -> Result<(), Errno> {
     let fd = fd as usize;
     let offset = offset as usize;

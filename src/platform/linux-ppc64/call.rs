034749d28793c0d2197fb5db57ed06fda483e92f
Xu Shaohua <shaohua@biofan.org>
2021-01-23 13:36:01 +0800

    linux: Add fchownat() unittest

--- src/platform/linux-ppc64/call.rs
+++ src/platform/linux-ppc64/call.rs
@@ -790,6 +790,17 @@ pub fn fchown(fd: i32, user: uid_t, group: gid_t) -> Result<(), Errno> {
 }
 
 /// Change ownership of a file.
+/// ```
+/// let filename = "/tmp/nc-fchown";
+/// let ret = nc::creat(filename, 0o644);
+/// assert!(ret.is_ok());
+/// let fd = ret.unwrap();
+/// assert!(nc::close(fd).is_ok());
+/// let ret = nc::fchownat(nc::AT_FDCWD, filename, 0, 0, 0);
+/// assert!(ret.is_err());
+/// assert_eq!(ret, Err(nc::EPERM));
+/// assert!(nc::unlink(filename).is_ok());
+/// ```
 pub fn fchownat(
     dirfd: i32,
     filename: &str,

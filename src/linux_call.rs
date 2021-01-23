e9e3e4eeef821f87fb569e6a1bbb329d11c0730b
Xu Shaohua <shaohua@biofan.org>
2021-01-23 13:41:24 +0800

    linux: Add gettimeofday() unittest

--- src/linux_call.rs
+++ src/linux_call.rs
@@ -1380,6 +1380,13 @@ pub fn gettid() -> pid_t {
 }
 
 /// Get time.
+/// ```
+/// let mut tv = nc::timeval_t::default();
+/// let mut tz = nc::timezone_t::default();
+/// let ret = nc::gettimeofday(&mut tv, &mut tz);
+/// assert!(ret.is_ok());
+/// assert!(tv.tv_sec > 1611380386);
+/// ```
 pub fn gettimeofday(timeval: &mut timeval_t, tz: &mut timezone_t) -> Result<(), Errno> {
     let timeval_ptr = timeval as *mut timeval_t as usize;
     let tz_ptr = tz as *mut timezone_t as usize;

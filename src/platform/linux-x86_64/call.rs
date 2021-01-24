2b74bc779e58aaf123a6d47436da0a3766fbd5cf
Xu Shaohua <shaohua@biofan.org>
2021-01-24 20:14:00 +0800

    linux: Add utimes() unittest

--- src/platform/linux-x86_64/call.rs
+++ src/platform/linux-x86_64/call.rs
@@ -5218,6 +5218,13 @@ pub fn timer_settime(
 }
 
 /// Get process times.
+/// ```
+/// let mut tms = nc::tms_t::default();
+/// let ret = nc::times(&mut tms);
+/// assert!(ret.is_ok());
+/// let clock = ret.unwrap();
+/// assert!(clock > 0);
+/// ```
 pub fn times(buf: &mut tms_t) -> Result<clock_t, Errno> {
     let buf_ptr = buf as *mut tms_t as usize;
     syscall1(SYS_TIMES, buf_ptr).map(|ret| ret as clock_t)

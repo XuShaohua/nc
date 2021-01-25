d8215560df52137373b2a71470bd13553d91ed15
Xu Shaohua <shaohua@biofan.org>
2021-01-25 14:52:38 +0800

    linux: Add tee() unittest

--- src/platform/linux-mips64/call.rs
+++ src/platform/linux-mips64/call.rs
@@ -5239,6 +5239,39 @@ pub fn sysmips() {
 }
 
 /// Duplicate pipe content.
+/// ```
+/// let mut fds_left = [0, 0];
+/// let ret = nc::pipe(&mut fds_left);
+/// assert!(ret.is_ok());
+///
+/// let mut fds_right = [0, 0];
+/// let ret = nc::pipe(&mut fds_right);
+/// assert!(ret.is_ok());
+///
+/// let msg = "Hello, Rust";
+/// let ret = nc::write(fds_left[1], msg.as_ptr() as usize, msg.len());
+/// assert!(ret.is_ok());
+/// let n_write = ret.unwrap() as nc::size_t;
+/// assert_eq!(n_write, msg.len());
+///
+/// let ret = nc::tee(fds_left[0], fds_right[1], n_write, nc::SPLICE_F_NONBLOCK);
+/// assert!(ret.is_ok());
+///
+/// let mut buf = [0u8; 64];
+/// let buf_len = buf.len();
+/// let ret = nc::read(fds_right[0], buf.as_mut_ptr() as usize, buf_len);
+/// assert!(ret.is_ok());
+/// let n_read = ret.unwrap() as nc::size_t;
+/// assert_eq!(n_read, n_write);
+/// let read_msg = std::str::from_utf8(&buf[..n_read]);
+/// assert!(read_msg.is_ok());
+/// assert_eq!(Ok(msg), read_msg);
+///
+/// assert!(nc::close(fds_left[0]).is_ok());
+/// assert!(nc::close(fds_left[1]).is_ok());
+/// assert!(nc::close(fds_right[0]).is_ok());
+/// assert!(nc::close(fds_right[1]).is_ok());
+/// ```
 pub fn tee(fd_in: i32, fd_out: i32, len: size_t, flags: u32) -> Result<ssize_t, Errno> {
     let fd_in = fd_in as usize;
     let fd_out = fd_out as usize;

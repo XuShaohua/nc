f8c636c1cbfeab9cc166362e3649b4e307e108e2
Xu Shaohua <shaohua@biofan.org>
2021-01-26 14:19:07 +0800

    linux: Update mq_timedreceive() api

--- src/platform/linux-x86/call.rs
+++ src/platform/linux-x86/call.rs
@@ -3302,15 +3302,15 @@ pub fn mq_open(
 /// Receive a message from a message queue
 pub fn mq_timedreceive(
     mqdes: mqd_t,
-    msg: &str,
-    msg_prio: u32,
+    msg: &mut [u8],
+    msg_len: usize,
+    msg_prio: &mut u32,
     abs_timeout: &timespec_t,
 ) -> Result<ssize_t, Errno> {
     let mqdes = mqdes as usize;
     let msg = CString::new(msg);
     let msg_ptr = msg.as_ptr() as usize;
-    let msg_len = msg.len();
-    let msg_prio = msg_prio as usize;
+    let msg_prio = msg_prio as *mut u32 as usize;
     let abs_timeout_ptr = abs_timeout as *const timespec_t as usize;
     syscall5(
         SYS_MQ_TIMEDRECEIVE,

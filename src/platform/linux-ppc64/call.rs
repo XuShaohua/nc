ed706b11ecf423ca07494d00b0e66d4215272a08
Xu Shaohua <shaohua@biofan.org>
2021-01-26 14:19:24 +0800

    linux: Add mq_timedreceive() unittest

--- src/platform/linux-ppc64/call.rs
+++ src/platform/linux-ppc64/call.rs
@@ -3071,6 +3071,53 @@ pub fn mq_open(
 }
 
 /// Receive a message from a message queue
+/// ```
+/// let name = "nc-mq-timedreceive";
+/// let ret = nc::mq_open(
+///     name,
+///     nc::O_CREAT | nc::O_RDWR | nc::O_EXCL,
+///     (nc::S_IRUSR | nc::S_IWUSR) as nc::umode_t,
+///     None,
+/// );
+/// assert!(ret.is_ok());
+/// let mq_id = ret.unwrap();
+///
+/// let mut attr = nc::mq_attr_t::default();
+/// let ret = nc::mq_getsetattr(mq_id, None, Some(&mut attr));
+/// assert!(ret.is_ok());
+/// println!("attr: {:?}", attr);
+///
+/// let msg = "Hello, Rust";
+/// let prio = 42;
+/// let timeout = nc::timespec_t {
+///     tv_sec: 1,
+///     tv_nsec: 0,
+/// };
+/// let ret = nc::mq_timedsend(mq_id, msg.as_bytes(), msg.len(), prio, &timeout);
+/// assert!(ret.is_ok());
+///
+/// let ret = nc::mq_getsetattr(mq_id, None, Some(&mut attr));
+/// assert!(ret.is_ok());
+/// assert_eq!(attr.mq_curmsgs, 1);
+///
+/// let mut buf = vec![0_u8; attr.mq_msgsize as usize];
+/// let buf_len = buf.len();
+/// let mut recv_prio = 0;
+/// let read_timeout = nc::timespec_t {
+///     tv_sec: 1,
+///     tv_nsec: 0,
+/// };
+/// let ret = nc::mq_timedreceive(mq_id, &mut buf, buf_len, &mut recv_prio, &read_timeout);
+/// if let Err(errno) = ret {
+///     eprintln!("mq_timedreceive() error: {}", nc::strerror(errno));
+/// }
+/// assert!(ret.is_ok());
+/// let n_read = ret.unwrap() as usize;
+/// assert_eq!(n_read, msg.len());
+///
+/// assert!(nc::close(mq_id).is_ok());
+/// assert!(nc::mq_unlink(name).is_ok());
+/// ```
 pub fn mq_timedreceive(
     mqdes: mqd_t,
     msg: &mut [u8],

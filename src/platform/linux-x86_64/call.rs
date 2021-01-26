c41f80a32ac867afe85160e4ca1cb9c005fb0dd8
Xu Shaohua <shaohua@biofan.org>
2021-01-26 14:06:05 +0800

    linux: Update mq_timedsend() api

--- src/platform/linux-x86_64/call.rs
+++ src/platform/linux-x86_64/call.rs
@@ -3035,14 +3035,14 @@ pub fn mq_timedreceive(
 /// Send message to a message queue
 pub fn mq_timedsend(
     mqdes: mqd_t,
-    msg: &str,
+    msg: &[u8],
+    msg_len: usize,
     msg_prio: u32,
     abs_timeout: &timespec_t,
 ) -> Result<(), Errno> {
     let mqdes = mqdes as usize;
     let msg = CString::new(msg);
     let msg_ptr = msg.as_ptr() as usize;
-    let msg_len = msg.len();
     let msg_prio = msg_prio as usize;
     let abs_timeout_ptr = abs_timeout as *const timespec_t as usize;
     syscall5(

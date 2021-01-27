995448c910eb92ff7252bdb29a0a2a337e9595c1
Xu Shaohua <shaohua@biofan.org>
2021-01-27 15:53:19 +0800

    linux: Add sched_getparam() unittest

--- src/platform/linux-ppc64/call.rs
+++ src/platform/linux-ppc64/call.rs
@@ -4707,6 +4707,12 @@ pub fn sched_getattr(
 }
 
 /// Get scheduling paramters.
+/// ```
+/// let mut param = nc::sched_param_t::default();
+/// let ret = nc::sched_getparam(0, &mut param);
+/// assert!(ret.is_ok());
+/// assert_eq!(param.sched_priority, 0);
+/// ```
 pub fn sched_getparam(pid: pid_t, param: &mut sched_param_t) -> Result<(), Errno> {
     let pid = pid as usize;
     let param_ptr = param as *mut sched_param_t as usize;

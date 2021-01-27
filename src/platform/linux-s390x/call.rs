0cdf8ddd66b6fc8b58b49fe8080f5b41b26bbfb9
Xu Shaohua <shaohua@biofan.org>
2021-01-27 18:27:50 +0800

    linux: Update sched_getaffinity() api

--- src/platform/linux-s390x/call.rs
+++ src/platform/linux-s390x/call.rs
@@ -4565,9 +4565,9 @@ pub fn s390_sthyi() {
 }
 
 /// Get a thread's CPU affinity mask.
-pub fn sched_getaffinity(pid: pid_t, len: u32, user_mask_ptr: usize) -> Result<(), Errno> {
+pub fn sched_getaffinity(pid: pid_t, len: usize, user_mask: &mut [usize]) -> Result<(), Errno> {
     let pid = pid as usize;
-    let len = len as usize;
+    let user_mask_ptr = user_mask.as_mut_ptr() as usize;
     syscall3(SYS_SCHED_GETAFFINITY, pid, len, user_mask_ptr).map(drop)
 }
 

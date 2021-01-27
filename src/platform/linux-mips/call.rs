9fc3cf4642bb002f39c5719cadbfe596019199dc
Xu Shaohua <shaohua@biofan.org>
2021-01-27 15:19:28 +0800

    linux: Add sched_get_priority_min() unittest

--- src/platform/linux-mips/call.rs
+++ src/platform/linux-mips/call.rs
@@ -4848,6 +4848,12 @@ pub fn sched_get_priority_max(policy: i32) -> Result<i32, Errno> {
 }
 
 /// Get static priority min value.
+/// ```
+/// let ret = nc::sched_get_priority_min(nc::SCHED_RR);
+/// assert!(ret.is_ok());
+/// let min_prio = ret.unwrap();
+/// assert_eq!(min_prio, 1);
+/// ```
 pub fn sched_get_priority_min(policy: i32) -> Result<i32, Errno> {
     let policy = policy as usize;
     syscall1(SYS_SCHED_GET_PRIORITY_MIN, policy).map(|ret| ret as i32)

66737453a70553225830abd26d4c3fa8812b2105
Xu Shaohua <shaohua@biofan.org>
2021-01-28 17:17:49 +0800

    linux: Add setgid() unittest

--- src/platform/linux-arm/call.rs
+++ src/platform/linux-arm/call.rs
@@ -5135,6 +5135,11 @@ pub fn setfsuid32() {
 }
 
 /// Set the group ID of the calling process to `gid`.
+/// ```
+/// let ret = nc::setgid(0);
+/// assert!(ret.is_err());
+/// assert_eq!(ret, Err(nc::EPERM));
+/// ```
 pub fn setgid(gid: gid_t) -> Result<(), Errno> {
     let gid = gid as usize;
     syscall1(SYS_SETGID, gid).map(drop)

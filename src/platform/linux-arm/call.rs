8bd7c5b6017e7cc95f4548145c3d87d2e1caab57
Xu Shaohua <shaohua@biofan.org>
2021-01-29 10:34:39 +0800

    linux: Add timer_delete() unittest

--- src/platform/linux-arm/call.rs
+++ src/platform/linux-arm/call.rs
@@ -6048,6 +6048,13 @@ pub fn timer_create(
 }
 
 /// Delete a per-process timer
+/// ```
+/// let mut timer_id = nc::timer_t::default();
+/// let ret = nc::timer_create(nc::CLOCK_MONOTONIC, None, &mut timer_id);
+/// assert!(ret.is_ok());
+/// let ret = nc::timer_delete(timer_id);
+/// assert!(ret.is_ok());
+/// ```
 pub fn timer_delete(timer_id: timer_t) -> Result<(), Errno> {
     let timer_id = timer_id as usize;
     syscall1(SYS_TIMER_DELETE, timer_id).map(drop)

04cb660a21cd655b9cee1d8805632605cd3b56ec
Xu Shaohua <shaohua@biofan.org>
2021-07-16 13:26:03 +0800

    linux: Fix type defines for mips

--- src/platform/linux-types/mips/stat.rs
+++ src/platform/linux-types/mips/stat.rs
@@ -6,30 +6,30 @@ use super::super::types::*;
 
 #[repr(C)]
 pub struct stat_t {
-	pub st_dev: u32,
+    pub st_dev: u32,
     /// Reserved for network id
-	st_pad1: [isize, 3],
-	pub st_ino: ino_t,
-	pub st_mode: mode_t,
-	pub st_nlink: u32,
-	pub st_uid: uid_t,
-	pub st_gid: gid_t,
-	pub st_rdev: u32,
-	st_pad2: [isize, 2],
-	pub st_size: off_t,
-	st_pad3: isize,
+    st_pad1: [isize; 3],
+    pub st_ino: ino_t,
+    pub st_mode: mode_t,
+    pub st_nlink: u32,
+    pub st_uid: uid_t,
+    pub st_gid: gid_t,
+    pub st_rdev: u32,
+    st_pad2: [isize; 2],
+    pub st_size: off_t,
+    st_pad3: isize,
 
-	/// Actually this should be timestruc_t st_atime, st_mtime and st_ctime
+    /// Actually this should be timestruc_t st_atime, st_mtime and st_ctime
     /// but we don't have it under Linux.
-	pub st_atime: time_t,
-	pub st_atime_nsec: isize,
-	pub st_mtime: time_t,
-	pub st_mtime_nsec: isize,
-	pub st_ctime: time_t,
-	pub st_ctime_nsec: isize,
-	pub st_blksize: isize,
-	pub st_blocks: isize,
-	st_pad4: [isize; 14],
+    pub st_atime: time_t,
+    pub st_atime_nsec: isize,
+    pub st_mtime: time_t,
+    pub st_mtime_nsec: isize,
+    pub st_ctime: time_t,
+    pub st_ctime_nsec: isize,
+    pub st_blksize: isize,
+    pub st_blocks: isize,
+    st_pad4: [isize; 14],
 }
 
 /// This matches struct stat64 in glibc2.1, hence the absolutely insane
@@ -38,45 +38,44 @@ pub struct stat_t {
 
 #[repr(C)]
 pub struct stat64_t {
-	pub st_dev: usize,
+    pub st_dev: usize,
     /// Reserved for st_dev expansion  
-	st_pad0: [usize, 3],
+    st_pad0: [usize; 3],
 
-	pub st_ino: u64,
+    pub st_ino: u64,
 
-	pub st_mode: mode_t,
-	pub st_nlink: u32,
+    pub st_mode: mode_t,
+    pub st_nlink: u32,
 
-	pub st_uid: uid_t,
-	pub st_gid: gid_t,
+    pub st_uid: uid_t,
+    pub st_gid: gid_t,
 
-	pub st_rdev: usize,
+    pub st_rdev: usize,
 
     /// Reserved for st_rdev expansion  
-	st_pad1: [usize; 3],
+    st_pad1: [usize; 3],
 
-	pub st_size: i64,
+    pub st_size: i64,
 
-	/// Actually this should be timestruc_t st_atime, st_mtime and st_ctime
+    /// Actually this should be timestruc_t st_atime, st_mtime and st_ctime
     /// but we don't have it under Linux.
-	pub st_atime: time_t,
+    pub st_atime: time_t,
 
     /// Reserved for st_atime expansion
-	pub st_atime_nsec: usize,
+    pub st_atime_nsec: usize,
 
-	pub st_mtime: time_t,
+    pub st_mtime: time_t,
     /// Reserved for st_mtime expansion
-	pub st_mtime_nsec: usize,
+    pub st_mtime_nsec: usize,
 
-	pub st_ctime: time_t,
+    pub st_ctime: time_t,
     /// Reserved for st_ctime expansion
-	pub st_ctime_nsec: usize,
+    pub st_ctime_nsec: usize,
 
-	pub st_blksize: usize,
-	st_pad2: usize,
+    pub st_blksize: usize,
+    st_pad2: usize,
 
-	pub st_blocks: i64,
+    pub st_blocks: i64,
 }
 
 pub const STAT_HAVE_NSEC: i32 = 1;
-

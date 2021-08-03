6119f88ed5160ab2d3bbd8ffd51760eead8200a4
Xu Shaohua <shaohua@biofan.org>
2021-08-03 10:51:52 +0800

    linux: Add lib document

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,90 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+//! Execute system call directly. `nc` do not depend on `std`.
+//!
+//! - [Documentation](https://docs.rs/nc)
+//! - [Release notes](https://github.com/xushaohua/nc/releases)
+//!
+//! ## Usage
+//! Add this to `Cargo.toml`:
+//! ```toml
+//! [dependencies]
+//! nc = "0.7"
+//! ```
+//!
+//! ## Examples
+//! Get file stat:
+//! ```rust
+//! let mut statbuf = nc::stat_t::default();
+//! match nc::stat("/etc/passwd", &mut statbuf) {
+//!     Ok(_) => println!("s: {:?}", statbuf),
+//!     Err(errno) => eprintln!("Failed to get file status, got errno: {}", errno),
+//! }
+//! ```
+//!
+//! Fork process:
+//! ```rust
+//! let pid = nc::fork();
+//! match pid {
+//!     Ok(pid) => {
+//!         if pid == 0 {
+//!             println!("child process: {}", pid);
+//!             let args = [""];
+//!             let env = [""];
+//!             match nc::execve("/bin/ls", &args, &env) {
+//!                 Ok(_) => {},
+//!                 Err(errno) => eprintln!("`ls` got err: {}", errno),
+//!             }
+//!         } else if pid < 0 {
+//!             eprintln!("fork() error!");
+//!         } else {
+//!             println!("parent process!");
+//!         }
+//!     },
+//!     Err(errno) => eprintln!("errno: {}", errno),
+//! }
+//! ```
+//!
+//! Kill self:
+//! ```rust
+//! let pid = nc::getpid();
+//! let ret = nc::kill(pid, nc::SIGTERM);
+//! // Never reach here.
+//! println!("ret: {:?}", ret);
+//! ```
+//!
+//! Or handle signals:
+//! ```rust
+//! fn handle_alarm(signum: i32) {
+//!     assert_eq!(signum, nc::SIGALRM);
+//! }
+//!
+//! fn main() {
+//!     let ret = nc::signal(nc::SIGALRM, handle_alarm as nc::sighandler_t);
+//!     assert!(ret.is_ok());
+//!     let remaining = nc::alarm(1);
+//!     let ret = nc::pause();
+//!     assert!(ret.is_err());
+//!     assert_eq!(ret, Err(nc::EINTR));
+//!     assert_eq!(remaining, 0);
+//! }
+//! ```
+//!
+//! ## Stable version
+//! For stable version of rustc, please install a C compiler (`gcc` or `clang`) first.
+//! As `asm!` feature is unavailable in stable version.
+//!
+//!
+//! ## Related projects
+//! * [nix][nix]
+//! * [syscall][syscall]
+//! * [relibc][relibc]
+//!
+//! [syscall]: https://github.com/kmcallister/syscall.rs
+//! [relibc]: https://gitlab.redox-os.org/redox-os/relibc.git
+//! [nix]: https://github.com/nix-rust/nix
+
 #![allow(dead_code)]
 #![no_std]
 #![cfg_attr(has_asm, feature(llvm_asm))]

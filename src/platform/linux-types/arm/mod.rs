a7be767cf45dae727e9036541a387b5f525bbdf4
Xu Shaohua <shaohua@biofan.org>
2021-08-07 21:07:32 +0800

    linux-types: Add page header to arm

--- src/platform/linux-types/arm/mod.rs
+++ src/platform/linux-types/arm/mod.rs
@@ -4,12 +4,14 @@
 
 mod fcntl;
 mod ioctls;
+mod page;
 mod posix_types;
 mod signal;
 mod stat;
 
 pub use fcntl::*;
 pub use ioctls::*;
+pub use page::*;
 pub use posix_types::*;
 pub use signal::*;
 pub use stat::*;

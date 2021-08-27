4655c760a47b5130c73ad09faae090107198e4e9
Xu Shaohua <shaohua@biofan.org>
2021-08-27 09:32:48 +0800

    examples: Fix stat() not found error

--- examples/stat.rs
+++ examples/stat.rs
@@ -6,39 +6,23 @@ extern crate nc;
 
 fn main() {
     let mut statbuf = nc::stat_t::default();
-    let filepath = "/etc/passwd";
+    let filepath = "/dev/fd/0";
 
-    #[cfg(target_arch = "x86_64")]
-    match nc::newfstatat(nc::AT_FDCWD, filepath, &mut statbuf, 0) {
-        Ok(_) => {
-            println!("s: {:?}", statbuf);
-        }
-        Err(errno) => {
-            eprintln!("Failed to get file status, got errno: {}", errno);
-        }
-    }
-    #[cfg(any(target_arch = "arm", target_arch = "mips", target_arch = "powerpc64"))]
-    match nc::stat(filepath, &mut statbuf) {
-        Ok(_) => {
-            println!("s: {:?}", statbuf);
-        }
-        Err(errno) => {
-            eprintln!("Failed to get file status, got errno: {}", errno);
-        }
-    }
+    let fd = nc::openat(nc::AT_FDCWD, filepath, nc::O_RDONLY, 0o644);
+    assert!(fd.is_ok());
+    let fd = fd.unwrap();
+    let ret = nc::fstat(fd, &mut statbuf);
+    assert!(nc::close(fd).is_ok());
 
-    #[cfg(not(any(
-        target_arch = "x86_64",
-        target_arch = "arm",
-        target_arch = "mips",
-        target_arch = "powerpc64"
-    )))]
-    match nc::fstatat(nc::AT_FDCWD, filepath, &mut statbuf) {
+    match ret {
         Ok(_) => {
             println!("s: {:?}", statbuf);
         }
         Err(errno) => {
-            eprintln!("Failed to get file status, got errno: {}", errno);
+            eprintln!(
+                "Failed to get file status, got errno: {}",
+                nc::strerror(errno)
+            );
         }
     }
 }

3196a5bc8e8ff910db3718f388d0545cca55ad60
Xu Shaohua <shaohua@biofan.org>
2021-08-27 11:04:15 +0800

    Update copyright statement

--- src/platform/linux-types/arm/posix_types.rs
+++ src/platform/linux-types/arm/posix_types.rs
@@ -1,5 +1,5 @@
 // Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
-// Use of this source is governed by General Public License that can be found
+// Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
 /// From arch/arm/include/asm/posix_types.h

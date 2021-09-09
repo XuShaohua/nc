a9d951726ea54f4fb8632e8026a1fa5c88aa8d45
chemicstry <chemicstry@gmail.com>
2021-09-10 00:03:44 +0100

    Fix rt_sigprocmask parameters

--- src/platform/linux-mips64/call.rs
+++ src/platform/linux-mips64/call.rs
@@ -4481,11 +4481,16 @@ pub fn rt_sigpending(set: &mut [sigset_t]) -> Result<(), Errno> {
 }
 
 /// Change the list of currently blocked signals.
-pub fn rt_sigprocmask(how: i32, set: &sigset_t, oldset: &mut sigset_t) -> Result<(), Errno> {
+pub fn rt_sigprocmask(
+    how: i32,
+    set: &sigset_t,
+    oldset: &mut sigset_t,
+    sigsetsize: size_t,
+) -> Result<(), Errno> {
     let how = how as usize;
     let set_ptr = set as *const sigset_t as usize;
     let oldset_ptr = oldset as *mut sigset_t as usize;
-    syscall3(SYS_RT_SIGPROCMASK, how, set_ptr, oldset_ptr).map(drop)
+    syscall4(SYS_RT_SIGPROCMASK, how, set_ptr, oldset_ptr, sigsetsize).map(drop)
 }
 
 /// Queue a signal and data.

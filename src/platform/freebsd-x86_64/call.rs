1f52d046acc3fdc8fe297decca0de4c2f3aebe54
Xu Shaohua <shaohua@biofan.org>
2021-09-12 23:17:04 +0800

    freebsd: Add symlink()

--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -401,6 +401,23 @@ pub fn rmdir<P: AsRef<Path>>(filename: P) -> Result<(), Errno> {
     syscall1(SYS_RMDIR, filename_ptr).map(drop)
 }
 
+/// Make a new name for a file.
+///
+/// ```
+/// let oldname = "/etc/passwd";
+/// let newname = "/tmp/nc-symlink";
+/// let ret = nc::symlink(oldname, newname);
+/// assert!(ret.is_ok());
+/// assert!(nc::unlink(newname).is_ok());
+/// ```
+pub fn symlink<P: AsRef<Path>>(oldname: P, newname: P) -> Result<(), Errno> {
+    let oldname = CString::new(oldname.as_ref());
+    let oldname_ptr = oldname.as_ptr() as usize;
+    let newname = CString::new(newname.as_ref());
+    let newname_ptr = newname.as_ptr() as usize;
+    syscall2(SYS_SYMLINK, oldname_ptr, newname_ptr).map(drop)
+}
+
 /// Commit filesystem caches to disk.
 ///
 /// ```

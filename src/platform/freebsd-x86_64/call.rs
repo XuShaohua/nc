955a3f6b23f059a4862d99c1f8e775b48e52853c
Xu Shaohua <shaohua@biofan.org>
2021-09-13 06:35:10 +0800

    freebsd: Add sched_setparam()

--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -1253,6 +1253,22 @@ pub fn rmdir<P: AsRef<Path>>(filename: P) -> Result<(), Errno> {
     syscall1(SYS_RMDIR, filename_ptr).map(drop)
 }
 
+/// Set scheduling paramters.
+///
+/// ```
+/// // This call always returns error because default scheduler is SCHED_NORMAL.
+/// // We shall call sched_setscheduler() and change to realtime policy
+/// // like SCHED_RR or SCHED_FIFO.
+/// let sched_param = nc::sched_param_t { sched_priority: 12 };
+/// let ret = nc::sched_setparam(0, &sched_param);
+/// assert_eq!(ret, Err(nc::EINVAL));
+/// ```
+pub fn sched_setparam(pid: pid_t, param: &sched_param_t) -> Result<(), Errno> {
+    let pid = pid as usize;
+    let param_ptr = param as *const sched_param_t as usize;
+    syscall2(SYS_SCHED_SETPARAM, pid, param_ptr).map(drop)
+}
+
 /// Sychronous I/O multiplexing.
 pub fn select(
     nfds: i32,

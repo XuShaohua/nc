0361e8428533711605259f18770c223c339e87db
Xu Shaohua <shaohua@biofan.org>
2021-09-13 05:39:59 +0800

    freebsd: Add wait4()

--- src/freebsd_call.rs
+++ src/freebsd_call.rs
@@ -86,6 +86,40 @@ pub fn close(fd: i32) -> Result<(), Errno> {
     syscall1(SYS_CLOSE, fd).map(drop)
 }
 
+/// Wait for process to change state.
+///
+/// ```
+/// let ret = nc::fork();
+/// match ret {
+///     Err(errno) => {
+///         eprintln!("fork() error: {}", nc::strerror(errno));
+///         nc::exit(1);
+///     }
+///     Ok(0) => println!("[child] pid is: {}", nc::getpid()),
+///     Ok(pid) => {
+///         let mut status = 0;
+///         let mut usage = nc::rusage_t::default();
+///         let ret = nc::wait4(-1, &mut status, 0, &mut usage);
+///         assert!(ret.is_ok());
+///         println!("status: {}", status);
+///         let exited_pid = ret.unwrap();
+///         assert_eq!(exited_pid, pid);
+///     }
+/// }
+/// ```
+pub fn wait4(
+    pid: pid_t,
+    wstatus: &mut i32,
+    options: i32,
+    rusage: &mut rusage_t,
+) -> Result<pid_t, Errno> {
+    let pid = pid as usize;
+    let wstatus_ptr = wstatus as *mut i32 as usize;
+    let options = options as usize;
+    let rusage_ptr = rusage as *mut rusage_t as usize;
+    syscall4(SYS_WAIT4, pid, wstatus_ptr, options, rusage_ptr).map(|ret| ret as pid_t)
+}
+
 /// Make a new name for a file.
 ///
 /// ```
--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -874,6 +874,40 @@ pub fn vfork() -> Result<pid_t, Errno> {
     syscall0(SYS_VFORK).map(|ret| ret as pid_t)
 }
 
+/// Wait for process to change state.
+///
+/// ```
+/// let ret = nc::fork();
+/// match ret {
+///     Err(errno) => {
+///         eprintln!("fork() error: {}", nc::strerror(errno));
+///         nc::exit(1);
+///     }
+///     Ok(0) => println!("[child] pid is: {}", nc::getpid()),
+///     Ok(pid) => {
+///         let mut status = 0;
+///         let mut usage = nc::rusage_t::default();
+///         let ret = nc::wait4(-1, &mut status, 0, &mut usage);
+///         assert!(ret.is_ok());
+///         println!("status: {}", status);
+///         let exited_pid = ret.unwrap();
+///         assert_eq!(exited_pid, pid);
+///     }
+/// }
+/// ```
+pub fn wait4(
+    pid: pid_t,
+    wstatus: &mut i32,
+    options: i32,
+    rusage: &mut rusage_t,
+) -> Result<pid_t, Errno> {
+    let pid = pid as usize;
+    let wstatus_ptr = wstatus as *mut i32 as usize;
+    let options = options as usize;
+    let rusage_ptr = rusage as *mut rusage_t as usize;
+    syscall4(SYS_WAIT4, pid, wstatus_ptr, options, rusage_ptr).map(|ret| ret as pid_t)
+}
+
 /// Write to a file descriptor.
 ///
 /// ```

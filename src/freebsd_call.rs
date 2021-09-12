18d52323c224ab5f2c63c1c3e867589e3c7cece0
Xu Shaohua <shaohua@biofan.org>
2021-09-13 05:41:53 +0800

    freebsd: Add recvfrom()

--- src/freebsd_call.rs
+++ src/freebsd_call.rs
@@ -369,6 +369,32 @@ pub fn sendmsg(sockfd: i32, msg: &msghdr_t, flags: i32) -> Result<ssize_t, Errno
     syscall3(SYS_SENDMSG, sockfd, msg_ptr, flags).map(|ret| ret as ssize_t)
 }
 
+/// Receive a message from a socket.
+pub fn recvfrom(
+    sockfd: i32,
+    buf: &mut [u8],
+    flags: i32,
+    src_addr: &mut sockaddr_in_t,
+    addrlen: &mut socklen_t,
+) -> Result<ssize_t, Errno> {
+    let sockfd = sockfd as usize;
+    let buf_ptr = buf.as_mut_ptr() as usize;
+    let buflen = buf.len();
+    let flags = flags as usize;
+    let src_addr_ptr = src_addr as *mut sockaddr_in_t as usize;
+    let addrlen_ptr = addrlen as *mut socklen_t as usize;
+    syscall6(
+        SYS_RECVFROM,
+        sockfd,
+        buf_ptr,
+        buflen,
+        flags,
+        src_addr_ptr,
+        addrlen_ptr,
+    )
+    .map(|ret| ret as ssize_t)
+}
+
 /// Check user's permission for a file.
 ///
 /// ```
--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -674,6 +674,32 @@ pub fn reboot(magic: i32, magci2: i32, cmd: u32, arg: usize) -> Result<(), Errno
     syscall4(SYS_REBOOT, magic, magic2, cmd, arg).map(drop)
 }
 
+/// Receive a message from a socket.
+pub fn recvfrom(
+    sockfd: i32,
+    buf: &mut [u8],
+    flags: i32,
+    src_addr: &mut sockaddr_in_t,
+    addrlen: &mut socklen_t,
+) -> Result<ssize_t, Errno> {
+    let sockfd = sockfd as usize;
+    let buf_ptr = buf.as_mut_ptr() as usize;
+    let buflen = buf.len();
+    let flags = flags as usize;
+    let src_addr_ptr = src_addr as *mut sockaddr_in_t as usize;
+    let addrlen_ptr = addrlen as *mut socklen_t as usize;
+    syscall6(
+        SYS_RECVFROM,
+        sockfd,
+        buf_ptr,
+        buflen,
+        flags,
+        src_addr_ptr,
+        addrlen_ptr,
+    )
+    .map(|ret| ret as ssize_t)
+}
+
 /// Delete a directory.
 ///
 /// ```

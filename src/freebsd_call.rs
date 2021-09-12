f9132a122c20fba00b73390e3e9a10d5fe167526
Xu Shaohua <shaohua@biofan.org>
2021-09-13 06:35:27 +0800

    freebsd: Add getresgid()

--- src/freebsd_call.rs
+++ src/freebsd_call.rs
@@ -1920,3 +1920,22 @@ pub fn getresuid(ruid: &mut uid_t, euid: &mut uid_t, suid: &mut uid_t) -> Result
     let suid_ptr = suid as *mut uid_t as usize;
     syscall3(SYS_GETRESUID, ruid_ptr, euid_ptr, suid_ptr).map(drop)
 }
+
+/// Get real, effect and saved group ID.
+///
+/// ```
+/// let mut rgid = 0;
+/// let mut egid = 0;
+/// let mut sgid = 0;
+/// let ret = nc::getresgid(&mut rgid, &mut egid, &mut sgid);
+/// assert!(ret.is_ok());
+/// assert!(rgid > 0);
+/// assert!(egid > 0);
+/// assert!(sgid > 0);
+/// ```
+pub fn getresgid(rgid: &mut gid_t, egid: &mut gid_t, sgid: &mut gid_t) -> Result<(), Errno> {
+    let rgid_ptr = rgid as *mut gid_t as usize;
+    let egid_ptr = egid as *mut gid_t as usize;
+    let sgid_ptr = sgid as *mut gid_t as usize;
+    syscall3(SYS_GETRESGID, rgid_ptr, egid_ptr, sgid_ptr).map(drop)
+}
--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -574,6 +574,25 @@ pub fn getppid() -> pid_t {
     syscall0(SYS_GETPPID).expect("getppid() failed") as pid_t
 }
 
+/// Get real, effect and saved group ID.
+///
+/// ```
+/// let mut rgid = 0;
+/// let mut egid = 0;
+/// let mut sgid = 0;
+/// let ret = nc::getresgid(&mut rgid, &mut egid, &mut sgid);
+/// assert!(ret.is_ok());
+/// assert!(rgid > 0);
+/// assert!(egid > 0);
+/// assert!(sgid > 0);
+/// ```
+pub fn getresgid(rgid: &mut gid_t, egid: &mut gid_t, sgid: &mut gid_t) -> Result<(), Errno> {
+    let rgid_ptr = rgid as *mut gid_t as usize;
+    let egid_ptr = egid as *mut gid_t as usize;
+    let sgid_ptr = sgid as *mut gid_t as usize;
+    syscall3(SYS_GETRESGID, rgid_ptr, egid_ptr, sgid_ptr).map(drop)
+}
+
 /// Get real, effect and saved user ID.
 ///
 /// ```

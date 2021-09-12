699947b4fbd2c8de581e53b696656cdf712c44fc
Xu Shaohua <shaohua@biofan.org>
2021-09-13 06:35:40 +0800

    freebsd: Add faccessat()

--- src/freebsd_call.rs
+++ src/freebsd_call.rs
@@ -2100,3 +2100,16 @@ pub fn ftruncate(fd: i32, length: off_t) -> Result<(), Errno> {
     let length = length as usize;
     syscall2(SYS_FTRUNCATE, fd, length).map(drop)
 }
+
+/// Check user's permission for a file.
+///
+/// ```
+/// assert!(nc::faccessat(nc::AT_FDCWD, "/etc/passwd", nc::F_OK).is_ok());
+/// ```
+pub fn faccessat<P: AsRef<Path>>(dfd: i32, filename: P, mode: i32) -> Result<(), Errno> {
+    let dfd = dfd as usize;
+    let filename = CString::new(filename.as_ref());
+    let filename_ptr = filename.as_ptr() as usize;
+    let mode = mode as usize;
+    syscall3(SYS_FACCESSAT, dfd, filename_ptr, mode).map(drop)
+}
--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -279,6 +279,19 @@ pub fn exit(status: i32) {
     unreachable!();
 }
 
+/// Check user's permission for a file.
+///
+/// ```
+/// assert!(nc::faccessat(nc::AT_FDCWD, "/etc/passwd", nc::F_OK).is_ok());
+/// ```
+pub fn faccessat<P: AsRef<Path>>(dfd: i32, filename: P, mode: i32) -> Result<(), Errno> {
+    let dfd = dfd as usize;
+    let filename = CString::new(filename.as_ref());
+    let filename_ptr = filename.as_ptr() as usize;
+    let mode = mode as usize;
+    syscall3(SYS_FACCESSAT, dfd, filename_ptr, mode).map(drop)
+}
+
 /// Change working directory.
 ///
 /// ```

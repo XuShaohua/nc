7f75dd5ce56df43429b1439a7fd203ccd173c0b5
Xu Shaohua <shaohua@biofan.org>
2021-09-13 05:51:54 +0800

    freebsd: Add gettimeofday()

--- src/freebsd_call.rs
+++ src/freebsd_call.rs
@@ -1100,6 +1100,21 @@ pub fn listen(sockfd: i32, backlog: i32) -> Result<(), Errno> {
     syscall2(SYS_LISTEN, sockfd, backlog).map(drop)
 }
 
+/// Get time.
+///
+/// ```
+/// let mut tv = nc::timeval_t::default();
+/// let mut tz = nc::timezone_t::default();
+/// let ret = nc::gettimeofday(&mut tv, &mut tz);
+/// assert!(ret.is_ok());
+/// assert!(tv.tv_sec > 1611380386);
+/// ```
+pub fn gettimeofday(timeval: &mut timeval_t, tz: &mut timezone_t) -> Result<(), Errno> {
+    let timeval_ptr = timeval as *mut timeval_t as usize;
+    let tz_ptr = tz as *mut timezone_t as usize;
+    syscall2(SYS_GETTIMEOFDAY, timeval_ptr, tz_ptr).map(drop)
+}
+
 /// Create a directory.
 ///
 /// ```
--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -462,6 +462,21 @@ pub fn getsockname(
     syscall3(SYS_GETSOCKNAME, sockfd, addr_ptr, addrlen_ptr).map(drop)
 }
 
+/// Get time.
+///
+/// ```
+/// let mut tv = nc::timeval_t::default();
+/// let mut tz = nc::timezone_t::default();
+/// let ret = nc::gettimeofday(&mut tv, &mut tz);
+/// assert!(ret.is_ok());
+/// assert!(tv.tv_sec > 1611380386);
+/// ```
+pub fn gettimeofday(timeval: &mut timeval_t, tz: &mut timezone_t) -> Result<(), Errno> {
+    let timeval_ptr = timeval as *mut timeval_t as usize;
+    let tz_ptr = tz as *mut timezone_t as usize;
+    syscall2(SYS_GETTIMEOFDAY, timeval_ptr, tz_ptr).map(drop)
+}
+
 /// Get the real user ID of the calling process.
 ///
 /// ```

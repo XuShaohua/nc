82f80df032ba87ed16fd7f99d46b564d1fd20015
Xu Shaohua <shaohua@biofan.org>
2021-09-13 06:33:51 +0800

    freebsd: Add getrlimit()

--- src/freebsd_call.rs
+++ src/freebsd_call.rs
@@ -1460,3 +1460,18 @@ pub fn setgid(gid: gid_t) -> Result<(), Errno> {
     let gid = gid as usize;
     syscall1(SYS_SETGID, gid).map(drop)
 }
+
+/// Get resource limit.
+///
+/// ```
+/// let mut rlimit = nc::rlimit_t::default();
+/// let ret = nc::getrlimit(nc::RLIMIT_NOFILE, &mut rlimit);
+/// assert!(ret.is_ok());
+/// assert!(rlimit.rlim_cur > 0);
+/// assert!(rlimit.rlim_max > 0);
+/// ```
+pub fn getrlimit(resource: i32, rlim: &mut rlimit_t) -> Result<(), Errno> {
+    let resource = resource as usize;
+    let rlim_ptr = rlim as *mut rlimit_t as usize;
+    syscall2(SYS_GETRLIMIT, resource, rlim_ptr).map(drop)
+}
--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -494,6 +494,21 @@ pub fn getppid() -> pid_t {
     syscall0(SYS_GETPPID).expect("getppid() failed") as pid_t
 }
 
+/// Get resource limit.
+///
+/// ```
+/// let mut rlimit = nc::rlimit_t::default();
+/// let ret = nc::getrlimit(nc::RLIMIT_NOFILE, &mut rlimit);
+/// assert!(ret.is_ok());
+/// assert!(rlimit.rlim_cur > 0);
+/// assert!(rlimit.rlim_max > 0);
+/// ```
+pub fn getrlimit(resource: i32, rlim: &mut rlimit_t) -> Result<(), Errno> {
+    let resource = resource as usize;
+    let rlim_ptr = rlim as *mut rlimit_t as usize;
+    syscall2(SYS_GETRLIMIT, resource, rlim_ptr).map(drop)
+}
+
 /// Get resource usage.
 ///
 /// ```

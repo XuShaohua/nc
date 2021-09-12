6b475d87ad269283f6244bcf113536328d493cf4
Xu Shaohua <shaohua@biofan.org>
2021-09-13 06:35:49 +0800

    freebsd: Add readlinkat()

--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -1446,6 +1446,34 @@ pub fn readlink<P: AsRef<Path>>(filename: P, buf: &mut [u8]) -> Result<ssize_t, 
     syscall3(SYS_READLINK, filename_ptr, buf_ptr, buf_len).map(|ret| ret as ssize_t)
 }
 
+/// Read value of a symbolic link.
+///
+/// ```
+/// let oldname = "/etc/passwd";
+/// let newname = "/tmp/nc-readlinkat";
+/// let ret = nc::symlink(oldname, newname);
+/// assert!(ret.is_ok());
+/// let mut buf = [0_u8; nc::PATH_MAX as usize];
+/// let ret = nc::readlinkat(nc::AT_FDCWD, newname, &mut buf);
+/// assert!(ret.is_ok());
+/// let n_read = ret.unwrap() as usize;
+/// assert_eq!(n_read, oldname.len());
+/// assert_eq!(oldname.as_bytes(), &buf[0..n_read]);
+/// assert!(nc::unlink(newname).is_ok());
+/// ```
+pub fn readlinkat<P: AsRef<Path>>(
+    dirfd: i32,
+    filename: P,
+    buf: &mut [u8],
+) -> Result<ssize_t, Errno> {
+    let dirfd = dirfd as usize;
+    let filename = CString::new(filename.as_ref());
+    let filename_ptr = filename.as_ptr() as usize;
+    let buf_ptr = buf.as_mut_ptr() as usize;
+    let buf_len = buf.len();
+    syscall4(SYS_READLINKAT, dirfd, filename_ptr, buf_ptr, buf_len).map(|ret| ret as ssize_t)
+}
+
 /// Read from a file descriptor into multiple buffers.
 ///
 /// ```

512386447167308ee4a126b1bdbf13c5944a28f1
Xu Shaohua <shaohua@biofan.org>
2021-09-13 05:46:18 +0800

    freebsd: Add sigaltstack

--- src/freebsd_call.rs
+++ src/freebsd_call.rs
@@ -545,6 +545,13 @@ pub fn acct<P: AsRef<Path>>(filename: P) -> Result<(), Errno> {
     syscall1(SYS_ACCT, filename_ptr).map(drop)
 }
 
+/// Get/set signal stack context.
+pub fn sigaltstack(uss: &sigaltstack_t, uoss: &mut sigaltstack_t) -> Result<(), Errno> {
+    let uss_ptr = uss as *const sigaltstack_t as usize;
+    let uoss_ptr = uoss as *mut sigaltstack_t as usize;
+    syscall2(SYS_SIGALTSTACK, uss_ptr, uoss_ptr).map(drop)
+}
+
 /// Reboot or enable/disable Ctrl-Alt-Del.
 ///
 /// ```
--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -881,6 +881,13 @@ pub fn setpgid(pid: pid_t, pgid: pid_t) -> Result<(), Errno> {
     syscall2(SYS_SETPGID, pid, pgid).map(drop)
 }
 
+/// Get/set signal stack context.
+pub fn sigaltstack(uss: &sigaltstack_t, uoss: &mut sigaltstack_t) -> Result<(), Errno> {
+    let uss_ptr = uss as *const sigaltstack_t as usize;
+    let uoss_ptr = uoss as *mut sigaltstack_t as usize;
+    syscall2(SYS_SIGALTSTACK, uss_ptr, uoss_ptr).map(drop)
+}
+
 /// Start swapping to file/device.
 ///
 /// ```

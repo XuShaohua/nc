9eb163c970e44827bcbdf049786f229676877620
Xu Shaohua <shaohua@biofan.org>
2021-09-13 06:34:58 +0800

    freebsd: Add clock_nanosleep()

--- src/freebsd_call.rs
+++ src/freebsd_call.rs
@@ -1605,3 +1605,26 @@ pub fn nanosleep(req: &timespec_t, rem: Option<&mut timespec_t>) -> Result<(), E
     };
     syscall2(SYS_NANOSLEEP, req_ptr, rem_ptr).map(drop)
 }
+
+/// High resolution sleep with a specific clock.
+///
+/// ```
+/// let t = nc::timespec_t {
+///     tv_sec: 1,
+///     tv_nsec: 0,
+/// };
+/// let mut rem = nc::timespec_t::default();
+/// assert!(nc::clock_nanosleep(nc::CLOCK_MONOTONIC, 0, &t, &mut rem).is_ok());
+/// ```
+pub fn clock_nanosleep(
+    which_clock: clockid_t,
+    flags: i32,
+    rqtp: &timespec_t,
+    rmtp: &mut timespec_t,
+) -> Result<(), Errno> {
+    let which_clock = which_clock as usize;
+    let flags = flags as usize;
+    let rqtp_ptr = rqtp as *const timespec_t as usize;
+    let rmtp_ptr = rmtp as *mut timespec_t as usize;
+    syscall4(SYS_CLOCK_NANOSLEEP, which_clock, flags, rqtp_ptr, rmtp_ptr).map(drop)
+}
--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -159,6 +159,29 @@ pub fn clock_gettime(which_clock: clockid_t, tp: &mut timespec_t) -> Result<(), 
     syscall2(SYS_CLOCK_GETTIME, which_clock, tp_ptr).map(drop)
 }
 
+/// High resolution sleep with a specific clock.
+///
+/// ```
+/// let t = nc::timespec_t {
+///     tv_sec: 1,
+///     tv_nsec: 0,
+/// };
+/// let mut rem = nc::timespec_t::default();
+/// assert!(nc::clock_nanosleep(nc::CLOCK_MONOTONIC, 0, &t, &mut rem).is_ok());
+/// ```
+pub fn clock_nanosleep(
+    which_clock: clockid_t,
+    flags: i32,
+    rqtp: &timespec_t,
+    rmtp: &mut timespec_t,
+) -> Result<(), Errno> {
+    let which_clock = which_clock as usize;
+    let flags = flags as usize;
+    let rqtp_ptr = rqtp as *const timespec_t as usize;
+    let rmtp_ptr = rmtp as *mut timespec_t as usize;
+    syscall4(SYS_CLOCK_NANOSLEEP, which_clock, flags, rqtp_ptr, rmtp_ptr).map(drop)
+}
+
 /// Set time of specific clock.
 ///
 /// ```

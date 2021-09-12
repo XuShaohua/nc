ee24eebe3139d0b04bf4c856c5734e6559626b57
Xu Shaohua <shaohua@biofan.org>
2021-09-12 23:27:15 +0800

    freebsd: Add swapon()

--- src/freebsd_call.rs
+++ src/freebsd_call.rs
@@ -743,6 +743,21 @@ pub fn setitimer(
     syscall3(SYS_SETITIMER, which, new_val_ptr, old_val_ptr).map(drop)
 }
 
+/// Start swapping to file/device.
+///
+/// ```
+/// let filename = "/dev/sda-no-exist";
+/// let ret = nc::swapon(filename, nc::SWAP_FLAG_PREFER);
+/// assert!(ret.is_err());
+/// assert_eq!(ret, Err(nc::EPERM));
+/// ```
+pub fn swapon<P: AsRef<Path>>(filename: P, flags: i32) -> Result<(), Errno> {
+    let filename = CString::new(filename.as_ref());
+    let filename_ptr = filename.as_ptr() as usize;
+    let flags = flags as usize;
+    syscall2(SYS_SWAPON, filename_ptr, flags).map(drop)
+}
+
 /// Create a directory.
 ///
 /// ```
--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -680,6 +680,21 @@ pub fn setpgid(pid: pid_t, pgid: pid_t) -> Result<(), Errno> {
     syscall2(SYS_SETPGID, pid, pgid).map(drop)
 }
 
+/// Start swapping to file/device.
+///
+/// ```
+/// let filename = "/dev/sda-no-exist";
+/// let ret = nc::swapon(filename, nc::SWAP_FLAG_PREFER);
+/// assert!(ret.is_err());
+/// assert_eq!(ret, Err(nc::EPERM));
+/// ```
+pub fn swapon<P: AsRef<Path>>(filename: P, flags: i32) -> Result<(), Errno> {
+    let filename = CString::new(filename.as_ref());
+    let filename_ptr = filename.as_ptr() as usize;
+    let flags = flags as usize;
+    syscall2(SYS_SWAPON, filename_ptr, flags).map(drop)
+}
+
 /// Make a new name for a file.
 ///
 /// ```

fc934b879a865ca3d1e1c2ffd1bf371409aa84a3
Xu Shaohua <shaohua@biofan.org>
2021-09-13 06:34:54 +0800

    freebsd: Add clock_settime()

--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -145,6 +145,23 @@ pub fn clock_gettime(which_clock: clockid_t, tp: &mut timespec_t) -> Result<(), 
     syscall2(SYS_CLOCK_GETTIME, which_clock, tp_ptr).map(drop)
 }
 
+/// Set time of specific clock.
+///
+/// ```
+/// let mut tp = nc::timespec_t::default();
+/// let ret = nc::clock_gettime(nc::CLOCK_REALTIME, &mut tp);
+/// assert!(ret.is_ok());
+/// assert!(tp.tv_sec > 0);
+/// let ret = nc::clock_settime(nc::CLOCK_REALTIME, &tp);
+/// assert!(ret.is_err());
+/// assert_eq!(ret, Err(nc::EPERM));
+/// ```
+pub fn clock_settime(which_clock: clockid_t, tp: &timespec_t) -> Result<(), Errno> {
+    let which_clock = which_clock as usize;
+    let tp_ptr = tp as *const timespec_t as usize;
+    syscall2(SYS_CLOCK_SETTIME, which_clock, tp_ptr).map(drop)
+}
+
 /// Close a file descriptor.
 ///
 /// ```

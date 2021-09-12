66bfc21d539615c598adf1701f6b8774ba3a0bfd
Xu Shaohua <shaohua@biofan.org>
2021-09-13 06:35:35 +0800

    freebsd: Add mmap()

--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -879,6 +879,57 @@ pub fn mlockall(flags: i32) -> Result<(), Errno> {
     syscall1(SYS_MLOCKALL, flags).map(drop)
 }
 
+/// Map files or devices into memory.
+///
+/// ```
+/// let path = "/etc/passwd";
+/// let ret = nc::open(path, nc::O_RDONLY, 0o644);
+/// assert!(ret.is_ok());
+/// let fd = ret.unwrap();
+///
+/// let mut sb = nc::stat_t::default();
+/// let ret = nc::fstat(fd, &mut sb);
+/// assert!(ret.is_ok());
+///
+/// let offset: usize = 0;
+/// let length: usize = sb.st_size as usize - offset;
+/// // Offset for mmap must be page aligned.
+/// let pa_offset: usize = offset & !(nc::PAGE_SIZE - 1);
+/// let map_length = length + offset - pa_offset;
+///
+/// let addr = nc::mmap(
+///     0, // 0 as NULL
+///     map_length,
+///     nc::PROT_READ,
+///     nc::MAP_PRIVATE,
+///     fd,
+///     pa_offset as nc::off_t,
+/// );
+/// assert!(addr.is_ok());
+/// let addr = addr.unwrap();
+///
+/// let n_write = nc::write(1, addr + offset - pa_offset, length);
+/// assert!(n_write.is_ok());
+/// assert_eq!(n_write, Ok(length as nc::ssize_t));
+/// assert!(nc::munmap(addr, map_length).is_ok());
+/// assert!(nc::close(fd).is_ok());
+/// ```
+pub fn mmap(
+    start: usize,
+    len: size_t,
+    prot: i32,
+    flags: i32,
+    fd: i32,
+    offset: off_t,
+) -> Result<usize, Errno> {
+    let len = len as usize;
+    let prot = prot as usize;
+    let flags = flags as usize;
+    let fd = fd as usize;
+    let offset = offset as usize;
+    syscall6(SYS_MMAP, start, len, prot, flags, fd, offset)
+}
+
 /// Mount filesystem.
 ///
 /// ```

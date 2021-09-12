942ae353f0e43082c6e9114376c3cc5c2bc98e82
Xu Shaohua <shaohua@biofan.org>
2021-09-13 05:53:00 +0800

    freebsd: Add getsockopt()

--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -477,6 +477,30 @@ pub fn getsockname(
     syscall3(SYS_GETSOCKNAME, sockfd, addr_ptr, addrlen_ptr).map(drop)
 }
 
+/// Get options on sockets
+pub fn getsockopt(
+    sockfd: i32,
+    level: i32,
+    optname: i32,
+    optval: &mut usize,
+    optlen: &mut socklen_t,
+) -> Result<(), Errno> {
+    let sockfd = sockfd as usize;
+    let level = level as usize;
+    let optname = optname as usize;
+    let optval_ptr = optval as *mut usize as usize;
+    let optlen_ptr = optlen as *mut socklen_t as usize;
+    syscall5(
+        SYS_GETSOCKOPT,
+        sockfd,
+        level,
+        optname,
+        optval_ptr,
+        optlen_ptr,
+    )
+    .map(drop)
+}
+
 /// Get time.
 ///
 /// ```

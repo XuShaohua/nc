72cec0ec8f2a24b0e46ed0ca914e51b85c898e2e
Xu Shaohua <shaohua@biofan.org>
2021-09-13 05:52:27 +0800

    freebsd: Add getrusage()

--- src/freebsd_call.rs
+++ src/freebsd_call.rs
@@ -1115,6 +1115,21 @@ pub fn gettimeofday(timeval: &mut timeval_t, tz: &mut timezone_t) -> Result<(), 
     syscall2(SYS_GETTIMEOFDAY, timeval_ptr, tz_ptr).map(drop)
 }
 
+/// Get resource usage.
+///
+/// ```
+/// let mut usage = nc::rusage_t::default();
+/// let ret = nc::getrusage(nc::RUSAGE_SELF, &mut usage);
+/// assert!(ret.is_ok());
+/// assert!(usage.ru_maxrss > 0);
+/// assert_eq!(usage.ru_nswap, 0);
+/// ```
+pub fn getrusage(who: i32, usage: &mut rusage_t) -> Result<(), Errno> {
+    let who = who as usize;
+    let usage_ptr = usage as *mut rusage_t as usize;
+    syscall2(SYS_GETRUSAGE, who, usage_ptr).map(drop)
+}
+
 /// Create a directory.
 ///
 /// ```

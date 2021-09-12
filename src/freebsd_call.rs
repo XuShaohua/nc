ff6712b1da5610fa656f3301b405c34117668324
Xu Shaohua <shaohua@biofan.org>
2021-09-13 05:47:20 +0800

    freebsd: Add ioctl()

--- src/freebsd_call.rs
+++ src/freebsd_call.rs
@@ -552,6 +552,28 @@ pub fn sigaltstack(uss: &sigaltstack_t, uoss: &mut sigaltstack_t) -> Result<(), 
     syscall2(SYS_SIGALTSTACK, uss_ptr, uoss_ptr).map(drop)
 }
 
+/// Control device.
+///
+/// ```
+/// let path = "/tmp/nc-ioctl";
+/// let ret = nc::open(path, nc::O_WRONLY | nc::O_CREAT, 0o644);
+/// assert!(ret.is_ok());
+/// let fd = ret.unwrap();
+/// let mut attr: i32 = 0;
+/// let cmd = -2146933247; // nc::FS_IOC_GETFLAGS
+/// let ret = nc::ioctl(fd, cmd, &mut attr as *mut i32 as usize);
+/// assert!(ret.is_ok());
+/// println!("attr: {}", attr);
+///
+/// assert!(nc::close(fd).is_ok());
+/// assert!(nc::unlink(path).is_ok());
+/// ```
+pub fn ioctl(fd: i32, cmd: i32, arg: usize) -> Result<(), Errno> {
+    let fd = fd as usize;
+    let cmd = cmd as usize;
+    syscall3(SYS_IOCTL, fd, cmd, arg).map(drop)
+}
+
 /// Reboot or enable/disable Ctrl-Alt-Del.
 ///
 /// ```

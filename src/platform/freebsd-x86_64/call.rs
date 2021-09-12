35a5fdc3a8af0516143f038132399c51a854e9f4
Xu Shaohua <shaohua@biofan.org>
2021-09-13 06:35:44 +0800

    freebsd: Add futimesat()

--- src/platform/freebsd-x86_64/call.rs
+++ src/platform/freebsd-x86_64/call.rs
@@ -491,6 +491,40 @@ pub fn ftruncate(fd: i32, length: off_t) -> Result<(), Errno> {
     syscall2(SYS_FTRUNCATE, fd, length).map(drop)
 }
 
+/// Change timestamp of a file relative to a directory file discriptor.
+///
+/// ```
+/// let path = "/tmp/nc-futimesat";
+/// let ret = nc::open(path, nc::O_WRONLY | nc::O_CREAT, 0o644);
+/// assert!(ret.is_ok());
+/// let fd = ret.unwrap();
+/// assert!(nc::close(fd).is_ok());
+/// let times = [
+///     nc::timeval_t {
+///         tv_sec: 100,
+///         tv_usec: 0,
+///     },
+///     nc::timeval_t {
+///         tv_sec: 10,
+///         tv_usec: 0,
+///     },
+/// ];
+/// let ret = nc::futimesat(nc::AT_FDCWD, path, &times);
+/// assert!(ret.is_ok());
+/// assert!(nc::unlink(path).is_ok());
+/// ```
+pub fn futimesat<P: AsRef<Path>>(
+    dirfd: i32,
+    filename: P,
+    times: &[timeval_t; 2],
+) -> Result<(), Errno> {
+    let dirfd = dirfd as usize;
+    let filename = CString::new(filename.as_ref());
+    let filename_ptr = filename.as_ptr() as usize;
+    let times_ptr = times.as_ptr() as usize;
+    syscall3(SYS_FUTIMESAT, dirfd, filename_ptr, times_ptr).map(drop)
+}
+
 /// Get the effective group ID of the calling process.
 ///
 /// ```

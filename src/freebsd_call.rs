336a68803abe55e198a4955a614a5145e9b8c09f
Xu Shaohua <shaohua@biofan.org>
2021-09-16 15:25:13 +0800

    freebsd: Update getsockopt()

--- src/freebsd_call.rs
+++ src/freebsd_call.rs
@@ -1147,13 +1147,13 @@ pub fn getsockopt(
     sockfd: i32,
     level: i32,
     optname: i32,
-    optval: &mut usize,
+    optval: &mut sockaddr_t,
     optlen: &mut socklen_t,
 ) -> Result<(), Errno> {
     let sockfd = sockfd as usize;
     let level = level as usize;
     let optname = optname as usize;
-    let optval_ptr = optval as *mut usize as usize;
+    let optval_ptr = optval as *mut sockaddr_t as usize;
     let optlen_ptr = optlen as *mut socklen_t as usize;
     syscall5(
         SYS_GETSOCKOPT,

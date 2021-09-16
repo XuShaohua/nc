88a52800f02a662667748ecd01411acd746bb6d4
Xu Shaohua <shaohua@biofan.org>
2021-09-16 15:02:14 +0800

    linux: Update readlink()

--- src/platform/linux-x86/call.rs
+++ src/platform/linux-x86/call.rs
@@ -4675,18 +4675,21 @@ pub fn readdir() {
 /// let ret = nc::symlink(oldname, newname);
 /// assert!(ret.is_ok());
 /// let mut buf = [0_u8; nc::PATH_MAX as usize];
-/// let ret = nc::readlink(newname, &mut buf);
+/// let ret = nc::readlink(newname, &mut buf, buf.len());
 /// assert!(ret.is_ok());
 /// let n_read = ret.unwrap() as usize;
 /// assert_eq!(n_read, oldname.len());
 /// assert_eq!(oldname.as_bytes(), &buf[0..n_read]);
 /// assert!(nc::unlink(newname).is_ok());
 /// ```
-pub fn readlink<P: AsRef<Path>>(filename: P, buf: &mut [u8]) -> Result<ssize_t, Errno> {
+pub fn readlink<P: AsRef<Path>>(
+    filename: P,
+    buf: &mut [u8],
+    buf_len: size_t,
+) -> Result<ssize_t, Errno> {
     let filename = CString::new(filename.as_ref());
     let filename_ptr = filename.as_ptr() as usize;
     let buf_ptr = buf.as_mut_ptr() as usize;
-    let buf_len = buf.len();
     syscall3(SYS_READLINK, filename_ptr, buf_ptr, buf_len).map(|ret| ret as ssize_t)
 }
 

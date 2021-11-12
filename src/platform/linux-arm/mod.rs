acec6d8d99e34d52a6b2621244d98c1e03398464
Xu Shaohua <shaohua@biofan.org>
2021-11-12 09:18:07 +0800

    linux: Fix submodule visibility errors for arm and aarch64

--- src/platform/linux-arm/mod.rs
+++ src/platform/linux-arm/mod.rs
@@ -2,9 +2,9 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-mod call;
-mod errno;
-mod sysno;
+pub mod call;
+pub mod errno;
+pub mod sysno;
 
 pub use call::*;
 pub use errno::*;

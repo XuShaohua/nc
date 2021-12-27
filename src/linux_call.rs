1c6322354fb1221e1b0e94f837b43534f8fa9fc7
Xu Shaohua <shaohua@biofan.org>
2021-12-27 14:53:17 +0800

    linux: Add unittest for clone3()

--- src/linux_call.rs
+++ src/linux_call.rs
@@ -5636,6 +5636,17 @@ pub fn clone(
 }
 
 /// New api to create child process.
+///
+/// ```
+/// let mut args = nc::clone_args_t::default();
+/// let mut pid_fd: i32 = -1;
+/// args.exit_signal = nc::SIGCHLD as u64;
+/// args.pidfd = &mut pid_fd as *mut i32 as usize as u64;
+/// args.flags = nc::CLONE_PIDFD as u64 | nc::CLONE_PARENT_SETTID as u64;
+/// let pid = nc::clone3(&mut args, core::mem::size_of::<nc::clone_args_t>());
+/// println!("ret: {:?}", pid);
+/// assert!(pid.is_ok());
+/// ```
 pub fn clone3(cl_args: &mut clone_args_t, size: size_t) -> Result<pid_t, Errno> {
     let cl_args_ptr = cl_args as *mut clone_args_t as usize;
     syscall2(SYS_CLONE3, cl_args_ptr, size).map(|ret| ret as pid_t)

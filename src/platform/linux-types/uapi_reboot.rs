a99637ab31dc1a502bc97036a1b64a9bd816c226
Xu Shaohua <shaohua@biofan.org>
2022-09-14 01:08:44 +0800

    linux/types: Fix lint errors in uapi_reboot

--- src/platform/linux-types/uapi_reboot.rs
+++ src/platform/linux-types/uapi_reboot.rs
@@ -4,7 +4,7 @@
 
 // From uapi/linux/reboot.h
 
-/// Magic values required to use _reboot() system call.
+/// Magic values required to use `_reboot()` system call.
 #[allow(overflowing_literals)]
 pub const LINUX_REBOOT_MAGIC1: i32 = 0xfee1_dead;
 pub const LINUX_REBOOT_MAGIC2: i32 = 672_274_793;
@@ -12,16 +12,16 @@ pub const LINUX_REBOOT_MAGIC2A: i32 = 85_072_278;
 pub const LINUX_REBOOT_MAGIC2B: i32 = 369_367_448;
 pub const LINUX_REBOOT_MAGIC2C: i32 = 537_993_216;
 
-/// Commands accepted by the _reboot() system call.
+/// Commands accepted by the `_reboot()` system call.
 ///
-/// RESTART     Restart system using default command and mode.
-/// HALT        Stop OS and give system control to ROM monitor, if any.
-/// CAD_ON      Ctrl-Alt-Del sequence causes RESTART command.
-/// CAD_OFF     Ctrl-Alt-Del sequence sends SIGINT to init task.
-/// POWER_OFF   Stop OS and remove all power from system, if possible.
-/// RESTART2    Restart system using given command string.
-/// SW_SUSPEND  Suspend system using software suspend if compiled in.
-/// KEXEC       Restart system using a previously loaded Linux kernel
+/// - `RESTART`     Restart system using default command and mode.
+/// - `HALT`        Stop OS and give system control to ROM monitor, if any.
+/// - `CAD_ON`      Ctrl-Alt-Del sequence causes RESTART command.
+/// - `CAD_OFF`     Ctrl-Alt-Del sequence sends SIGINT to init task.
+/// - `POWER_OFF`   Stop OS and remove all power from system, if possible.
+/// - `RESTART2`    Restart system using given command string.
+/// - `SW_SUSPEND`  Suspend system using software suspend if compiled in.
+/// - `KEXEC`       Restart system using a previously loaded Linux kernel
 pub const LINUX_REBOOT_CMD_RESTART: u32 = 0x0123_4567;
 pub const LINUX_REBOOT_CMD_HALT: u32 = 0xCDEF_0123;
 pub const LINUX_REBOOT_CMD_CAD_ON: u32 = 0x89AB_CDEF;

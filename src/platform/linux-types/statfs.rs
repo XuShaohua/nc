9e8825ad6abfc570f90560269b5ef97a687e9715
Xu Shaohua <shaohua@biofan.org>
2022-09-15 00:12:49 +0800

    linux/types: Fix lint errors in statfs

--- src/platform/linux-types/statfs.rs
+++ src/platform/linux-types/statfs.rs
@@ -4,15 +4,17 @@
 
 //! From include/uapi/asm-generic/statfs.h
 
-use super::basic_types::*;
+use super::basic_types::fsid_t;
 
 /// Most 64-bit platforms use 'long', while most 32-bit platforms use '__u32'.
 /// Yes, they differ in signedness as well as size.
 /// Special cases can override it for themselves -- except for S390x, which
 /// is just a little too special for us. And MIPS, which I'm not touching
 /// with a 10' pole.
+#[allow(clippy::module_name_repetitions)]
 pub type _statfs_word = isize;
 
+#[allow(clippy::module_name_repetitions)]
 #[repr(C)]
 #[derive(Debug, Default, Clone, Copy)]
 pub struct statfs_t {
@@ -32,6 +34,7 @@ pub struct statfs_t {
 
 /// ARM needs to avoid the 32-bit padding at the end, for consistency
 /// between EABI and OABI
+#[allow(clippy::module_name_repetitions)]
 #[repr(C)]
 #[derive(Debug, Default, Clone, Copy)]
 pub struct statfs64_t {
@@ -49,7 +52,7 @@ pub struct statfs64_t {
     pub f_spare: [isize; 4],
 }
 
-/// IA64 and x86_64 need to avoid the 32-bit padding at the end,
+/// IA64 and `x86_64` need to avoid the 32-bit padding at the end,
 /// to be compatible with the i386 ABI
 #[repr(C)]
 #[derive(Debug, Default, Clone, Copy)]

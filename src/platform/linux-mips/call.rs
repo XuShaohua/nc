d6289e9242217fc3183db95da98c971a14a14afd
Xu Shaohua <shaohua@biofan.org>
2022-09-17 00:37:12 +0800

    linux: Replace with pointer.cast()

--- src/platform/linux-mips/call.rs
+++ src/platform/linux-mips/call.rs
@@ -5534,7 +5534,7 @@ pub unsafe fn recvmmsg(
     timeout: &mut timespec_t,
 ) -> Result<i32, Errno> {
     let sockfd = sockfd as usize;
-    let msgvec_ptr = msgvec as *mut [mmsghdr_t] as *mut mmsghdr_t as usize;
+    let msgvec_ptr = (msgvec as *mut [mmsghdr_t]).cast::<*mut mmsghdr_t>() as usize;
     let vlen = msgvec.len();
     let flags = flags as usize;
     let timeout_ptr = timeout as *mut timespec_t as usize;
@@ -6305,7 +6305,7 @@ pub unsafe fn sendfile64(
 /// Send multiple messages on a socket
 pub unsafe fn sendmmsg(sockfd: i32, msgvec: &mut [mmsghdr_t], flags: i32) -> Result<i32, Errno> {
     let sockfd = sockfd as usize;
-    let msgvec_ptr = msgvec as *mut [mmsghdr_t] as *mut mmsghdr_t as usize;
+    let msgvec_ptr = (msgvec as *mut [mmsghdr_t]).cast::<*mut mmsghdr_t>() as usize;
     let vlen = msgvec.len();
     let flags = flags as usize;
     syscall4(SYS_SENDMMSG, sockfd, msgvec_ptr, vlen, flags).map(|ret| ret as i32)

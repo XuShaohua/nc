ce370c4cd168132f9371cf67c3a4acf038ebeff5
Xu Shaohua <shaohua@biofan.org>
2022-09-17 00:23:47 +0800

    linux/types: Fix lint error in memfd

--- src/platform/linux-types/memfd.rs
+++ src/platform/linux-types/memfd.rs
@@ -2,18 +2,24 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-use super::hugetlb_encode::*;
+use super::hugetlb_encode::{
+    HUGETLB_FLAG_ENCODE_16GB, HUGETLB_FLAG_ENCODE_16MB, HUGETLB_FLAG_ENCODE_1GB,
+    HUGETLB_FLAG_ENCODE_1MB, HUGETLB_FLAG_ENCODE_256MB, HUGETLB_FLAG_ENCODE_2GB,
+    HUGETLB_FLAG_ENCODE_2MB, HUGETLB_FLAG_ENCODE_32MB, HUGETLB_FLAG_ENCODE_512KB,
+    HUGETLB_FLAG_ENCODE_512MB, HUGETLB_FLAG_ENCODE_64KB, HUGETLB_FLAG_ENCODE_8MB,
+    HUGETLB_FLAG_ENCODE_MASK, HUGETLB_FLAG_ENCODE_SHIFT,
+};
 
-/// flags for memfd_create(2) (unsigned int)
+/// flags for `memfd_create(2)` (unsigned int)
 pub const MFD_CLOEXEC: u32 = 0x0001;
 pub const MFD_ALLOW_SEALING: u32 = 0x0002;
 pub const MFD_HUGETLB: u32 = 0x0004;
 
-/// Huge page size encoding when MFD_HUGETLB is specified, and a huge page
-/// size other than the default is desired.  See hugetlb_encode.h.
+/// Huge page size encoding when `MFD_HUGETLB` is specified, and a huge page
+/// size other than the default is desired.  See `hugetlb_encode.h`.
 /// All known huge page size encodings are provided here.  It is the
 /// responsibility of the application to know which sizes are supported on
-/// the running system.  See mmap(2) man page for details.
+/// the running system.  See `mmap(2)` man page for details.
 pub const MFD_HUGE_SHIFT: i32 = HUGETLB_FLAG_ENCODE_SHIFT;
 pub const MFD_HUGE_MASK: i32 = HUGETLB_FLAG_ENCODE_MASK;
 

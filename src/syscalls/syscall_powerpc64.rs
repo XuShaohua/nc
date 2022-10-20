7d7bc0ff388f8612da06112ce6c6da9102ae4065
Xu Shaohua <shaohua@biofan.org>
2022-10-20 10:58:40 +0800

    syscalls: Ignore lint warnings

--- src/syscalls/syscall_powerpc64.rs
+++ src/syscalls/syscall_powerpc64.rs
@@ -2,7 +2,9 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+#![allow(clippy::missing_errors_doc)]
 #![allow(clippy::missing_safety_doc)]
+#![allow(clippy::module_name_repetitions)]
 
 use super::types::*;
 

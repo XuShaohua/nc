400d700c5c7c8a51ff508b195994b498e6955d2e
Xu Shaohua <shaohua@biofan.org>
2022-10-24 10:12:19 +0800

    linux: Add unittest for rt_sigsuspend()

--- src/platform/linux-s390x/call.rs
+++ src/platform/linux-s390x/call.rs
@@ -5523,6 +5523,35 @@ pub unsafe fn rt_sigreturn() {
 /// Wait for a signal.
 ///
 /// Always returns Errno, normally EINTR.
+///
+/// # Example
+/// ```
+/// use core::mem::size_of;
+///
+/// let pid = unsafe { nc::fork() };
+/// assert!(pid.is_ok());
+/// let pid = pid.unwrap();
+/// assert!(pid >= 0);
+///
+/// if pid == 0 {
+///     // child process.
+///     let mask = nc::sigset_t::default();
+///     let ret = unsafe { nc::rt_sigsuspend(&mask, size_of::<nc::sigset_t>()) };
+///     assert!(ret.is_ok());
+/// } else {
+///     // parent process.
+///     let t = nc::timespec_t {
+///         tv_sec: 1,
+///         tv_nsec: 0,
+///     };
+///     let ret = unsafe { nc::nanosleep(&t, None) };
+///     assert!(ret.is_ok());
+///
+///     // parent process.
+///     let ret = unsafe { nc::kill(pid, nc::SIGTERM) };
+///     assert!(ret.is_ok());
+/// }
+/// ```
 pub unsafe fn rt_sigsuspend(set: &sigset_t, sigsetsize: size_t) -> Result<(), Errno> {
     let set_ptr = set as *const sigset_t as usize;
     syscall2(SYS_RT_SIGSUSPEND, set_ptr, sigsetsize).map(drop)

17eeb1110bdb14612644107c08f0261b18174fdf
Xu Shaohua <shaohua@biofan.org>
2022-10-24 10:24:54 +0800

    linux: Update unittest for timer_overrun()

--- src/platform/linux-mips64/call.rs
+++ src/platform/linux-mips64/call.rs
@@ -7119,8 +7119,8 @@ pub unsafe fn timer_delete(timer_id: timer_t) -> Result<(), Errno> {
 ///     assert!(ret.is_ok());
 ///     println!("cur time: {:?}", cur_time);
 ///
-///     let ret = nc::util::pause();
-///     assert_eq!(ret, Err(nc::EINTR));
+///     let mask = nc::sigset_t::default();
+///     let ret = unsafe { nc::rt_sigsuspend(&mask, size_of::<nc::sigset_t>()) };
 ///
 ///     let ret = unsafe { nc::timer_getoverrun(timer_id) };
 ///     assert!(ret.is_ok());

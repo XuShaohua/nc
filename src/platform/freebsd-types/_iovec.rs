48bdd80c9212b45290b256862ab5cdcea4810088
Xu Shaohua <shaohua@biofan.org>
2022-10-24 14:31:59 +0800

    freebsd: Fix lint errors in _sigset.rs

--- src/platform/freebsd-types/_iovec.rs
+++ src/platform/freebsd-types/_iovec.rs
@@ -2,12 +2,13 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-//! From sys/sys/_iovec.h
+//! From `sys/sys/_iovec.h`
 
 use crate::size_t;
 
 #[repr(C)]
 #[derive(Debug, Default)]
+#[allow(clippy::module_name_repetitions)]
 pub struct iovec_t {
     /// Base address.
     pub iov_base: usize,
--- src/platform/freebsd-types/_sigset.rs
+++ src/platform/freebsd-types/_sigset.rs
@@ -2,32 +2,40 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-//! From sys/sys/_sigset.h
+//! From `sys/sys/_sigset.h`
 
 pub const _SIG_WORDS: usize = 4;
 pub const _SIG_MAXSIG: i32 = 128;
 
+#[must_use]
 pub const fn _SIG_IDX(sig: i32) -> i32 {
     sig - 1
 }
+
+#[must_use]
 pub const fn _SIG_WORD(sig: i32) -> i32 {
     _SIG_IDX(sig) >> 5
 }
+
+#[must_use]
 pub const fn _SIG_BIT(sig: i32) -> i32 {
     1 << (_SIG_IDX(sig) & 31)
 }
+
+#[must_use]
 pub const fn _SIG_VALID(sig: i32) -> bool {
     sig <= _SIG_MAXSIG && sig > 0
 }
 
-/// sigset_t macros.
+/// `sigset_t` macros.
 #[repr(C)]
 #[derive(Debug, Default)]
+#[allow(clippy::module_name_repetitions)]
 pub struct sigset_t {
     pub bits: [u32; _SIG_WORDS],
 }
 
 pub type osigset_t = u32;
 
-/// Alias to [osigset_t]
+/// Alias to `osigset_t`
 pub type old_sigset_t = osigset_t;

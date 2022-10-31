620fa66c6272aff8502b7e802b9d71d70fc48e56
Xu Shaohua <shaohua@biofan.org>
2022-10-31 17:27:31 +0800

    freebsd: Generate sysno list based on local header file

--- src/mksysnum_freebsd.py
+++ src/mksysnum_freebsd.py
@@ -7,19 +7,23 @@ import os
 import re
 import subprocess
 import sys
-import urllib.request
 
 
-def download_syscall_file(url):
-    with urllib.request.urlopen(url) as f:
-        return f.read()
+HEADER_FILE = "/usr/include/sys/syscall.h"
+
+
+def read_sysno(header_file):
+    cmd = ["cc", "-E", "-dD", header_file]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    if p.returncode != 0 or err:
+        print(err)
+        sys.exit(1)
+    return parse_sysno(out.decode())
+
 
 def parse_sysno(content):
-    sysnum_pattern = re.compile("^([0-9]+)\s*\S*\s*STD\s*({\s*\S*\s*(\w+).*)$")
-    proto_pattern = re.compile("\s{2,}")
-
-    # Merge multiple lines into one
-    content = content.replace("\\\n", "")
+    sysnum_pattern = re.compile("^#define\s+SYS_([a-z_0-9]+)\s+(\d+)$")
 
     lines = [
         "",
@@ -31,63 +35,36 @@ def parse_sysno(content):
     names = []
 
     for line in content.split("\n"):
+        if not line:
+            continue
         m = sysnum_pattern.match(line)
         if m:
-            num = m.group(1)
-            proto = m.group(2)
-            name = m.group(3)
+            name = m.group(1)
+            num = m.group(2)
 
-            if name == "sys_exit":
-                name = "exit"
-
-            # Replace multiple space chars with only one
-            proto = proto_pattern.sub(" ", proto)
-
-            line = "pub const SYS_{0}: Sysno = {1}; // {2}".format(name.upper(), num, proto)
+            line = "pub const SYS_{0}: Sysno = {1};".format(name.upper(), num)
             names.append(name)
             lines.append(line)
     return (lines, names)
 
-def print_sysnums(names):
-    temp = '''
-pub fn %s() {
-    core::unimplemented!();
-    // syscall0(SYS_%s);
-}
-'''
-    raw_identifiers = ["break", "yield"]
-    output = []
-    for name in names:
-        upper_name = name.upper()
-        if name in raw_identifiers:
-            name = "r#" + name
-        output.append(temp % (name, upper_name))
-    return output
 
 def rust_fmt(filename):
     subprocess.run(["rustfmt", filename])
 
+
 def main():
     if len(sys.argv) != 2:
-        print("Usage: %s os arch" % sys.argv[0])
+        print("Usage: %s arch" % sys.argv[0])
         sys.exit(1)
 
     arch_name = sys.argv[1]
     platform_folder = os.path.join("platform", "freebsd-%s" % arch_name)
 
-    url = "https://svn.freebsd.org/base/stable/12/sys/kern/syscalls.master"
-    content = download_syscall_file(url)
-
-    lines, names = parse_sysno(content.decode())
+    lines, names = read_sysno(HEADER_FILE)
     sysnum_file = os.path.join(platform_folder, "sysno.rs")
     with open(sysnum_file, "w") as fh:
         fh.write("\n".join(lines))
     rust_fmt(sysnum_file)
-
-    call_file = os.path.join(platform_folder, "call.rs")
-    with open(call_file, "w") as fh:
-        fh.writelines(print_sysnums(names))
-    rust_fmt(call_file)
 
 if __name__ == "__main__":
     main()

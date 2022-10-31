51705294bbd1d5047cea924786f73f7e8cdaa29a
Xu Shaohua <shaohua@biofan.org>
2022-10-31 22:57:16 +0800

    netbsd: Add errno mod

--- src/platform/netbsd-x86_64/mod.rs
+++ src/platform/netbsd-x86_64/mod.rs
@@ -2,6 +2,10 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+pub mod call;
+pub mod errno;
 pub mod sysno;
 
+pub use call::*;
+pub use errno::*;
 pub use sysno::*;

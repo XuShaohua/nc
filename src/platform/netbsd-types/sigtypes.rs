3fc529d3d9d969d0b2ba6bd2ede78b60a8ea66ec
Xu Shaohua <shaohua@biofan.org>
2022-11-01 09:17:18 +0800

    netbsd: add sigtypes header

---
+++ src/platform/netbsd-types/sigtypes.rs
@@ -0,0 +1,10 @@
+// Copyright (c) 2020 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+//! From `/usr/include/sys/sigtypes.h`
+
+#[repr(C)]
+pub struct sigset_t {
+    pub __bits: [u32; 4],
+}

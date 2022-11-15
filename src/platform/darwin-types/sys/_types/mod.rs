26ec0ac6172a2fa4ba74f1bc26b7b5a9d3a7d2e7
Xu Shaohua <shaohua@biofan.org>
2022-11-15 16:36:31 +0800

    darwin: Add _mode_t header

--- src/platform/darwin-types/sys/_types/mod.rs
+++ src/platform/darwin-types/sys/_types/mod.rs
@@ -5,6 +5,7 @@
 mod _blkcnt_t;
 mod _blksize_t;
 mod _dev_t;
+mod _mode_t;
 mod _o_dsync;
 mod _o_sync;
 mod _pid_t;
@@ -13,6 +14,7 @@ mod _seek_set;
 pub use _blkcnt_t::*;
 pub use _blksize_t::*;
 pub use _dev_t::*;
+pub use _mode_t::*;
 pub use _o_dsync::*;
 pub use _o_sync::*;
 pub use _pid_t::*;

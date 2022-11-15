f510d51b1da9f42d7c65707c91003b4741213d31
Xu Shaohua <shaohua@biofan.org>
2022-11-15 16:55:57 +0800

    darwin: Add ino_t header

---
+++ src/platform/darwin-types/sys/_types/_ino_t.rs
@@ -0,0 +1,10 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+//! From `sys/_types/_ino_t.h`
+
+/// inode number
+///
+/// Used for 64 bit inodes
+pub type ino_t = u64;

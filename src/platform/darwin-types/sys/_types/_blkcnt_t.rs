62a9d3666b83e79513d93f6d73729517c06b8766
Xu Shaohua <shaohua@biofan.org>
2022-11-15 16:25:37 +0800

    darwin: Add _blkcnt_t header

---
+++ src/platform/darwin-types/sys/_types/_blkcnt_t.rs
@@ -0,0 +1,8 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+//! From `sys/_types/_blkcnt_t.h`
+
+/// total blocks
+pub type blkcnt_t = i64;
--- src/platform/darwin-types/sys/_types/mod.rs
+++ src/platform/darwin-types/sys/_types/mod.rs
@@ -2,8 +2,10 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+mod _blkcnt_t;
 mod _o_dsync;
 mod _seek_set;
 
+pub use _blkcnt_t::*;
 pub use _o_dsync::*;
 pub use _seek_set::*;

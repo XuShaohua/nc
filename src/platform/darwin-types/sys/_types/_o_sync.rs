879fe8a685eaadc9d975ceffad3cd4b87bf629b7
Xu Shaohua <shaohua@biofan.org>
2022-11-15 16:32:16 +0800

    darwin: Add _o_sync header

---
+++ src/platform/darwin-types/sys/_types/_o_sync.rs
@@ -0,0 +1,8 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+//! From `sys/_types/_o_sync.h`
+
+/// synch I/O file integrity
+pub const O_SYNC: i32 = 0x0080;
--- src/platform/darwin-types/sys/_types/mod.rs
+++ src/platform/darwin-types/sys/_types/mod.rs
@@ -6,10 +6,12 @@ mod _blkcnt_t;
 mod _blksize_t;
 mod _dev_t;
 mod _o_dsync;
+mod _o_sync;
 mod _seek_set;
 
 pub use _blkcnt_t::*;
 pub use _blksize_t::*;
 pub use _dev_t::*;
 pub use _o_dsync::*;
+pub use _o_sync::*;
 pub use _seek_set::*;

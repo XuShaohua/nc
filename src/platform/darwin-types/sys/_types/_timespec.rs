0f724c5e845025563d4078c4ccd75e18d49bfc0b
Xu Shaohua <shaohua@biofan.org>
2022-11-16 09:30:29 +0800

    darwin: Add _timespec header

---
+++ src/platform/darwin-types/sys/_types/_timespec.rs
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+use crate::__darwin_time_t;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[repr(C)]
+pub struct timespec_t {
+    pub tv_sec: __darwin_time_t,
+    pub tv_nsec: isize,
+}

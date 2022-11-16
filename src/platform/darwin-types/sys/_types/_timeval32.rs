82cb4893072e74dd672b773f450dcaf833e765b8
Xu Shaohua <shaohua@biofan.org>
2022-11-16 09:35:44 +0800

    darwin: Add _timeval32 header

---
+++ src/platform/darwin-types/sys/_types/_timeval32.rs
@@ -0,0 +1,14 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+//! From `sys/_types/_timeval32.h`
+
+#[derive(Debug, Close, PartialEq, Eq)]
+#[repr(C)]
+pub struct timeval32_t {
+    /// seconds
+    pub v_sec: i32,
+    /// and microseconds
+    pub tv_usec: i32,
+}

8b454ce474477ae69832540dc5d1bf2f1a6c9ae4
Xu Shaohua <shaohua@biofan.org>
2022-11-16 08:41:54 +0800

    darwin: Add _id_t header

---
+++ src/platform/darwin-types/sys/_types/_id_t.rs
@@ -0,0 +1,8 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+//! From `sys/_types/_id_t.h`
+
+/// can hold pid_t, gid_t, or uid_t
+pub type id_t = u32;

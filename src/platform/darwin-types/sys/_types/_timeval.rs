fc9c036640e4fc177bae20ce794fb1f6cff9edf3
Xu Shaohua <shaohua@biofan.org>
2022-11-16 09:33:37 +0800

    darwin: Add _timeval header

--- src/platform/darwin-types/sys/_types/_timeval.rs
+++ src/platform/darwin-types/sys/_types/_timeval.rs
@@ -2,13 +2,15 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-//! From `sys/_types/_timespec.h`
+//! From `sys/_types/_timeval.h`
 
-use crate::__darwin_time_t;
+use crate::{__darwin_suseconds_t, __darwin_time_t};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[repr(C)]
-pub struct timespec_t {
+pub struct timeval_t {
+    /// seconds
     pub tv_sec: __darwin_time_t,
-    pub tv_nsec: isize,
+    /// and microseconds
+    pub tv_usec: __darwin_suseconds_t,
 }

7a82541f3c8e25fc80980794ea4ef2b4b0c5f137
Xu Shaohua <shaohua@biofan.org>
2022-11-16 08:46:10 +0800

    darwin: Add _fsfilcnt_t header

---
+++ src/platform/darwin-types/sys/_types/_fsfilcnt_t.rs
@@ -0,0 +1,8 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+//! From `sys/_types/_fsfilcnt_t.h`
+
+/// Used by statvfs and fstatvfs
+pub type fsfilcnt_t = u32;

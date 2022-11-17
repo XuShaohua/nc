1c19d061988b0459c07daea7d23b8654c140871b
Xu Shaohua <shaohua@biofan.org>
2022-11-17 08:24:14 +0800

    darwin: Add _in_port_t header

---
+++ src/platform/darwin-types/sys/_types/_in_port_t.rs
@@ -0,0 +1,7 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+//! From `sys/_types/_in_port_t.h`
+
+pub type in_port_t = u16;

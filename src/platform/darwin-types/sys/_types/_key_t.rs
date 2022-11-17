e8bc551b25ef1a59976e15988d519158a05643bd
Xu Shaohua <shaohua@biofan.org>
2022-11-17 08:31:52 +0800

    darwin: Add _key_t header

---
+++ src/platform/darwin-types/sys/_types/_key_t.rs
@@ -0,0 +1,8 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+//! From `sys/_types/_key_t.h`
+
+/// IPC key (for Sys V IPC)
+pub type key_t: i32;

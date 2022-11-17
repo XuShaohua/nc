45fb20824f8137675958ef4c03804d03a6b13803
Xu Shaohua <shaohua@biofan.org>
2022-11-17 08:23:02 +0800

    darwin: Add _in_addr_t header

--- src/platform/darwin-types/netinet/in.rs
+++ src/platform/darwin-types/netinet/in.rs
@@ -1,5 +1,5 @@
 // Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
-// Use of this source is governed by General Public License that can be found
+// Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
 //! From `netinet/in.h`

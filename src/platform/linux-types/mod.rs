5e3013a220f471e385cb8b09e746ce4b90def820
Xu Shaohua <shaohua@biofan.org>
2022-11-18 15:39:48 +0800

    linux: Move prctl to linux/

--- src/platform/linux-types/mod.rs
+++ src/platform/linux-types/mod.rs
@@ -90,7 +90,6 @@ mod perf_event;
 mod personality;
 mod poll;
 mod posix_types;
-mod prctl;
 mod rseq;
 mod uapi;
 
@@ -124,6 +123,5 @@ pub use perf_event::*;
 pub use personality::*;
 pub use poll::*;
 pub use posix_types::*;
-pub use prctl::*;
 pub use rseq::*;
 pub use uapi::*;

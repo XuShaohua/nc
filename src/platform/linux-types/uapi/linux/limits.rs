b57a63bca789aa72166601169e2a0ca566229837
Xu Shaohua <shaohua@biofan.org>
2022-11-18 16:00:21 +0800

    linux: Move limits to linux/

--- src/platform/linux-types/uapi/linux/limits.rs
+++ src/platform/linux-types/uapi/linux/limits.rs
@@ -2,6 +2,8 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+//! From `include/uapi/linux/limits.h`
+
 pub const NR_OPEN: i32 = 1024;
 
 /// supplemental group IDs are available

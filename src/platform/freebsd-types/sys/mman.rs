9ad56dc52b9686fa256bfe32726d1674e32acf16
Xu Shaohua <shaohua@biofan.org>
2022-11-19 11:54:39 +0800

    freebsd: Move mman to sys/

--- src/platform/freebsd-types/sys/mman.rs
+++ src/platform/freebsd-types/sys/mman.rs
@@ -2,7 +2,7 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-//! From sys/sys/mman.h
+//! From `sys/sys/mman.h`
 
 /// Inheritance for `minherit()`
 pub const INHERIT_SHARE: i32 = 0;

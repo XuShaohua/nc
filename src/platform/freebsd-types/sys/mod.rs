fc182049a67ffe265d5a8a39138a062c32829918
Xu Shaohua <shaohua@biofan.org>
2022-11-19 11:54:03 +0800

    freebsd: Move reboot to sys/

---
+++ src/platform/freebsd-types/sys/mod.rs
@@ -0,0 +1,7 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+mod reboot;
+
+pub use reboot::*;

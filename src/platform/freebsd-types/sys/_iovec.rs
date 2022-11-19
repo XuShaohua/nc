856b2cba9cb2c0c43efa9c01102c5b6f2a6f8db7
Xu Shaohua <shaohua@biofan.org>
2022-11-19 17:06:01 +0800

    freebsd: Update fields in struct iovec

--- src/platform/freebsd-types/sys/_iovec.rs
+++ src/platform/freebsd-types/sys/_iovec.rs
@@ -4,15 +4,27 @@
 
 //! From `sys/_iovec.h`
 
+use core::ffi::c_void;
+use core::ptr;
+
 use crate::size_t;
 
 #[repr(C)]
-#[derive(Debug, Default)]
+#[derive(Debug)]
 #[allow(clippy::module_name_repetitions)]
 pub struct iovec_t {
     /// Base address.
-    pub iov_base: usize,
+    pub iov_base: *const c_void,
 
     /// Length.
     pub iov_len: size_t,
+}
+
+impl Default for iovec_t {
+    fn default() -> Self {
+        Self {
+            iov_base: ptr::null(),
+            iov_len: 0,
+        }
+    }
 }

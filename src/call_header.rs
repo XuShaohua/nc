426e7b155819916d078daebc81d2c3fe434fa3d3
Xu Shaohua <shaohua@biofan.org>
2022-11-21 09:08:50 +0800

    Add call header file

---
+++ src/call_header.rs
@@ -0,0 +1,19 @@
+// Copyright (c) 2020 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+#![allow(clippy::cast_possible_truncation)]
+#![allow(clippy::cast_possible_wrap)]
+#![allow(clippy::cast_sign_loss)]
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::similar_names)]
+#![allow(clippy::wildcard_imports)]
+
+extern crate alloc;
+
+use crate::c_str::CString;
+use crate::path::Path;
+use crate::syscalls::*;
+use crate::sysno::*;
+use crate::types::*;

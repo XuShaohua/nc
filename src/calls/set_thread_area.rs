254400a1cedf80f4af6561ff85b37c49975a9453
Xu Shaohua <shaohua@biofan.org>
2022-11-21 09:17:41 +0800

    calls: Add bdflush

--- src/calls/set_thread_area.rs
+++ src/calls/set_thread_area.rs
@@ -1,5 +1,4 @@
 /// Set thread-local storage information.
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub unsafe fn set_thread_area(user_desc: &mut user_desc_t) -> Result<(), Errno> {
     let user_desc_ptr = user_desc as *mut user_desc_t as usize;
     syscall1(SYS_SET_THREAD_AREA, user_desc_ptr).map(drop)

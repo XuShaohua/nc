236792a68c2ecd9cd5641ffb4eee9070802f6a3b
Xu Shaohua <shaohua@biofan.org>
2022-11-21 15:06:58 +0800

    darwin: Add nlink_t header

---
+++ src/platform/darwin-types/sys/_types/_nlink_t.rs
@@ -0,0 +1,8 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+//! From `sys/_types/_nlink_t.h`
+
+/// link count
+pub type nlink_t = u16;

bdcce6c4fab12429aaafc3466d2911b1d81a7ce1
Xu Shaohua <shaohua@biofan.org>
2022-11-24 09:19:42 +0800

    linux: Impl faccessat2()

--- src/platform/linux-arm/call.rs
+++ src/platform/linux-arm/call.rs
@@ -921,6 +921,28 @@ pub unsafe fn faccessat<P: AsRef<Path>>(dfd: i32, filename: P, mode: i32) -> Res
     syscall3(SYS_FACCESSAT, dfd, filename_ptr, mode).map(drop)
 }
 
+/// Check user's permission for a file.
+///
+/// # Example
+///
+/// ```
+/// let ret = unsafe { nc::faccessat2(nc::AT_FDCWD, "/etc/passwd", nc::F_OK, nc::AT_SYMLINK_NOFOLLOW) };
+/// assert!(ret.is_ok());
+/// ```
+pub unsafe fn faccessat2<P: AsRef<Path>>(
+    dfd: i32,
+    filename: P,
+    mode: i32,
+    flags: i32,
+) -> Result<(), Errno> {
+    let dfd = dfd as usize;
+    let filename = CString::new(filename.as_ref());
+    let filename_ptr = filename.as_ptr() as usize;
+    let mode = mode as usize;
+    let flags = flags as usize;
+    syscall4(SYS_FACCESSAT2, dfd, filename_ptr, mode, flags).map(drop)
+}
+
 /// Manipulate file space.
 ///
 /// # Example

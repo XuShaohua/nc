cfc71fd6d551b7cb9c0e7e18033bfa0a100f16fc
Xu Shaohua <shaohua@biofan.org>
2022-11-24 09:06:47 +0800

    linux: Impl openat2()

--- src/platform/linux-arm/call.rs
+++ src/platform/linux-arm/call.rs
@@ -4209,6 +4209,37 @@ pub unsafe fn openat<P: AsRef<Path>>(
     syscall4(SYS_OPENAT, dirfd, filename_ptr, flags, mode).map(|ret| ret as i32)
 }
 
+/// Open and possibly create a file (extended)
+///
+/// # Example
+///
+/// ```
+/// use core::mem::size_of;
+/// let path = "/etc/passwd";
+/// let mut how = nc::open_how_t{
+///   flags: nc::O_RDONLY as u64,
+///   ..nc::open_how_t::default()
+/// };
+/// let how_size = size_of::<nc::open_how_t>();
+/// let ret = unsafe { nc::openat2(nc::AT_FDCWD, path, &mut how, how_size) };
+/// assert!(ret.is_ok());
+/// let fd = ret.unwrap();
+/// let ret = unsafe { nc::close(fd) };
+/// assert!(ret.is_ok());
+/// ```
+pub unsafe fn openat2<P: AsRef<Path>>(
+    dirfd: i32,
+    pathname: P,
+    how: *mut open_how_t,
+    size: size_t,
+) -> Result<i32, Errno> {
+    let dirfd = dirfd as usize;
+    let pathname = CString::new(pathname.as_ref());
+    let pathname_ptr = pathname.as_ptr() as usize;
+    let how_ptr = how as *const open_how_t as usize;
+    syscall4(SYS_OPENAT2, dirfd, pathname_ptr, how_ptr, size).map(|ret| ret as i32)
+}
+
 /// Obtain handle for an open file
 pub unsafe fn open_by_handle_at(
     mount_fd: i32,

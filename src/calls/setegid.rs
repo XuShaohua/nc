5cf51362f5fc1df26524c00e2fbfb796974f9cb7
Xu Shaohua <shaohua@biofan.org>
2023-01-13 09:32:11 +0800

    calls: Add setegi()

---
+++ src/calls/setegid.rs
@@ -0,0 +1,13 @@
+/// Set the effective group ID of the calling process to `gid`.
+///
+/// # Example
+///
+/// ```
+/// let ret = unsafe { nc::setegid(0) };
+/// assert!(ret.is_err());
+/// assert_eq!(ret, Err(nc::EPERM));
+/// ```
+pub unsafe fn setegid(gid: gid_t) -> Result<(), Errno> {
+    let gid = gid as usize;
+    syscall1(SYS_SETEGID, gid).map(drop)
+}

4212b60120ddc6658cd982879e4f01ec36a011b4
Xu Shaohua <shaohua@biofan.org>
2023-01-13 09:53:30 +0800

    calls: Add ktimer_create()

---
+++ src/calls/ktimer_create.rs
@@ -0,0 +1,11 @@
+/// Create a per-process timer (REALTIME)
+pub unsafe fn ktimer_craete(
+    clockid: clockid_t,
+    evp: &mut sigevent_t,
+    timer_id: &mut i32,
+) -> Result<(), Errno> {
+    let clockid = clockid as usize;
+    let env_ptr = evp as *mut sigevent_t as usize;
+    let timer_id_ptr = timer_id as *mut i32 as usize;
+    syscall3(SYS_KTIMER_CREATE, clockid, evp_ptr timer_id_ptr).map(drop)
+}

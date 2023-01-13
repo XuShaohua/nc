cb6e8c2905fdf7dab15dc18170e7f0cc6b6d433a
Xu Shaohua <shaohua@biofan.org>
2023-01-14 00:01:00 +0800

    calls: Add kmq_timedreceive()

--- src/calls/kmq_timedreceive.rs
+++ src/calls/kmq_timedreceive.rs
@@ -1,22 +1,22 @@
-/// Send a message to message queue (REALTIME)
-pub unsafe fn kmq_timedsend(
+/// Receive a message from message queue (REALTIME)
+pub unsafe fn kmq_timedreceive(
     mqd: i32,
-    msg: &[u8],
+    msg: &mut [u8],
+    msg_len: usize,
     msg_prio: u32,
     abs_timeout: &timespec_t,
-) -> Result<(), Errno> {
+) -> Result<ssize_t, Errno> {
     let mqd = mqd as usize;
-    let msg_ptr = msg.as_ptr() as usize;
-    let msg_len = msg.len();
+    let msg_ptr = msg.as_mut_ptr() as usize;
     let msg_prio = msg_prio as usize;
     let abs_timeout_ptr = abs_timeout.as_ptr() as usize;
     syscall5(
-        SYS_KMQ_TIMEDSEND,
+        SYS_KMQ_TIMEDRECEIVE,
         mqd,
         msg_ptr,
         msg_len,
         msg_prio,
         abs_timeout_ptr,
     )
-    .map(drop)
+    .map(|val| val as ssize_t)
 }

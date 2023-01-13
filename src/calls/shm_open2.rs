54cd7f812c4d456f62bb4f41b26dd63d570250ad
Xu Shaohua <shaohua@biofan.org>
2023-01-13 08:50:14 +0800

    calls: Add shm_open2()

---
+++ src/calls/shm_open2.rs
@@ -0,0 +1,28 @@
+/// Opens (or optionally creates) a POSIX shared memory object named `path`.
+pub unsafe fn shm_open2<P: AsRef<Path>>(
+    path: P,
+    flags: i32,
+    mode: mode_t,
+    shmflags: i32,
+    fcaps: *mut filecaps_t,
+    name: P,
+) -> Result<(), Errno> {
+    // TODO(Shaohua): Add AsRef<CStr>
+    let path = CString::new(path.as_ref());
+    let path_ptr = path.as_ptr() as usize;
+    let flags = flags as usize;
+    let mode = mode as usize;
+    let shmflags = shmflags as usize;
+    let name = CString::new(name.as_ref());
+    let name_ptr = name.as_ptr() as usize;
+    syscall6(
+        SYS_SHM_OPEN2,
+        path_ptr,
+        flags,
+        mode,
+        shmflags,
+        fcaps_ptr,
+        name_ptr,
+    )
+    .map(drop)
+}

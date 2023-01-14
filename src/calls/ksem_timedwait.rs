ce6f644958f505d3f2787bb8afdae42e394193c4
Xu Shaohua <shaohua@biofan.org>
2023-01-14 21:21:25 +0800

    calls: Add ksem_timedwait()

---
+++ src/calls/ksem_timedwait.rs
@@ -0,0 +1,6 @@
+/// Decrement (lock) a semaphore.
+pub unsafe fn ksem_timedwait(id: semid_t, abstime: &timespect_t) -> Result<(), Errno> {
+    let id = id as usize;
+    let abstime_ptr = abstime as *const timespect_t as usize;
+    syscall2(SYS_KSEM_TIMEDWAIT, id, abstime_ptr).map(drop)
+}

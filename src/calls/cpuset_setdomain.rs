de0793c29c4d395ef8b719ee72e9612abb25607b
Xu Shaohua <shaohua@biofan.org>
2023-01-15 20:31:13 +0800

    calls: Add cpuset_setdomain()

---
+++ src/calls/cpuset_setdomain.rs
@@ -0,0 +1,27 @@
+/// Attempts to set the mask and policy for the object specified by `level`,
+/// `which` and `id` to the values in `mask` and `policy`.
+
+pub unsafe fn cpuset_setdomain(
+    level: cpulevel_t,
+    which: cpuwhich_t,
+    id: id_t,
+    domainset_size: size_t,
+    mask: &mut domainset_t,
+    policy: i32,
+) -> Result<(), Errno> {
+    let level = level as usize;
+    let which = which as usize;
+    let id = id as usize;
+    let mask_ptr = mask as *mut domainset_t as usize;
+    let policy = policy as usize;
+    syscall6(
+        SYS_CPUSET_SETDOMAIN,
+        level,
+        which,
+        id,
+        domainset_size,
+        mask_ptr,
+        policy,
+    )
+    .map(drop)
+}

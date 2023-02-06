7fd8f890a8b0c2034cd62f68d94b904da64a349c
Xu Shaohua <shaohua@biofan.org>
2023-02-06 10:28:35 +0800

    calls: Fix return value error in accept()
    https://github.com/XuShaohua/nc/issues/25

--- src/platform/linux-ppc64/call.rs
+++ src/platform/linux-ppc64/call.rs
@@ -19,15 +19,17 @@ use crate::sysno::*;
 use crate::types::*;
 
 /// Accept a connection on a socket.
+///
+/// On success, `accept()` return a file descriptor for the accepted socket.
 pub unsafe fn accept(
     sockfd: i32,
     addr: &mut sockaddr_in_t,
     addrlen: &mut socklen_t,
-) -> Result<(), Errno> {
+) -> Result<i32, Errno> {
     let sockfd = sockfd as usize;
     let addr_ptr = addr as *mut sockaddr_in_t as usize;
     let addrlen_ptr = addrlen as *mut socklen_t as usize;
-    syscall3(SYS_ACCEPT, sockfd, addr_ptr, addrlen_ptr).map(drop)
+    syscall3(SYS_ACCEPT, sockfd, addr_ptr, addrlen_ptr).map(|val| val as i32)
 }
 
 /// Accept a connection on a socket.

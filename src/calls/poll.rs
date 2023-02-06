7b678640fd15a7acf0b62c2f8ca1126d18dd8536
Xu Shaohua <shaohua@biofan.org>
2023-02-06 10:02:03 +0800

    calls: poll() returns integar to indicate events occurred.
    https://github.com/XuShaohua/nc/issues/27

--- src/calls/poll.rs
+++ src/calls/poll.rs
@@ -1,7 +1,13 @@
 /// Wait for some event on file descriptors.
-pub unsafe fn poll(fds: &mut [pollfd_t], timeout: i32) -> Result<(), Errno> {
+///
+/// ## Return value
+/// On success, it returns a nonnegative value which is the number of
+/// events in the `fds` whose `revents` fields have been set to a nonzero
+/// value.
+pub unsafe fn poll(fds: &mut [pollfd_t], timeout: i32) -> Result<i32, Errno> {
+    // TODO(Shaohua): Add some unittest
     let fds_ptr = fds.as_mut_ptr() as usize;
     let nfds = fds.len();
     let timeout = timeout as usize;
-    syscall3(SYS_POLL, fds_ptr, nfds, timeout).map(drop)
+    syscall3(SYS_POLL, fds_ptr, nfds, timeout).map(|ret| ret as i32)
 }

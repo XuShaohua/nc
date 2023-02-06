4906f20843aefdf20742d58947a2e0636a341df8
Xu Shaohua <shaohua@biofan.org>
2023-02-06 10:25:23 +0800

    calls: Add unittest to poll()

--- src/platform/linux-s390x/call.rs
+++ src/platform/linux-s390x/call.rs
@@ -4607,12 +4607,51 @@ pub unsafe fn pkey_mprotect(
 
 /// Wait for some event on file descriptors.
 ///
+/// The `timeout` argument specifies the number of milliseconds that `poll()`
+/// should block waiting for a file descriptor to become ready. Specifying
+/// a timeout of zero causes `poll()` to return immediately, even if
+/// no file descriptors are ready.
+///
 /// ## Return value
-/// On success, it returns a nonnegative value which is the number of
-/// events in the `fds` whose `revents` fields have been set to a nonzero
-/// value.
+/// On success, it returns a nonnegative value which is the number of events
+/// in the `fds` whose `revents` fields have been set to a nonzero value.
+///
+/// # Examples
+/// ```rust
+/// use std::thread;
+/// use std::time::Duration;
+///
+/// const STDIN_FD: i32 = 0;
+/// const STDOUT_FD: i32 = 1;
+///
+/// fn main() {
+///     let mut fds = [
+///         nc::pollfd_t {
+///             fd: STDIN_FD,
+///             events: nc::POLLIN,
+///             revents: 0,
+///         },
+///         nc::pollfd_t {
+///             fd: STDOUT_FD,
+///             events: nc::POLLOUT,
+///             revents: 0,
+///         },
+///     ];
+///
+///     // Create a background thread to print some messages to stdout.
+///     let handle = thread::spawn(|| {
+///         thread::sleep(Duration::from_millis(100));
+///         println!("MESSAGES from rust");
+///     });
+///
+///     let ret = unsafe { nc::poll(&mut fds, 3000) };
+///     assert!(ret.is_ok());
+///     let num_ready = ret.unwrap();
+///     println!("num of fds ready to read: {num_ready}");
+///     assert!(handle.join().is_ok());
+/// }
+/// ```
 pub unsafe fn poll(fds: &mut [pollfd_t], timeout: i32) -> Result<i32, Errno> {
-    // TODO(Shaohua): Add some unittest
     let fds_ptr = fds.as_mut_ptr() as usize;
     let nfds = fds.len();
     let timeout = timeout as usize;

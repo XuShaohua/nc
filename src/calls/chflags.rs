20292e19f3a4a8dc7b8a4d42a8a31727d469591b
Xu Shaohua <shaohua@biofan.org>
2023-03-06 14:26:21 +0800

    calls: Add chflags()

---
+++ src/calls/chflags.rs
@@ -0,0 +1,7 @@
+/// Set file flags.
+pub unsafe fn chflags<P: AsRef<Path>>(path: P, flags: fflags_t) -> Result<(), Errno> {
+    let path = CString::new(path.as_ref());
+    let path_ptr = path.as_ptr() as usize;
+    let flags = flags as usize;
+    syscall2(SYS_CHFLAGS, path_ptr, flags).map(drop)
+}

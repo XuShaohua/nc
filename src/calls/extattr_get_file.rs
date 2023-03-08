c4291936fc2f42403fff4240d9b0fed7de7b5c0e
Xu Shaohua <shaohua@biofan.org>
2023-03-08 08:02:09 +0800

    calls: Add extattr_get_file()

---
+++ src/calls/extattr_get_file.rs
@@ -0,0 +1,23 @@
+/// Get the value of the VFS extended attribute specified.
+pub unsafe fn extattr_get_file<P: AsRef<Path>>(
+    path: P,
+    attr_namespace: i32,
+    attr_name: &str,
+    data: &mut [u8],
+) -> Result<(), Errno> {
+    let path = CString::new(path.as_ref());
+    let path_ptr = path.as_ptr() as usize;
+    let attr_namespace = attr_namespace as usize;
+    let attr_name_ptr = attr_name.as_ptr() as usize;
+    let data_ptr = data.as_mut_ptr() as usize;
+    let nbytes = data.len();
+    syscall5(
+        SYS_EXTATTR_GET_FILE,
+        path_ptr,
+        attr_namespace,
+        attr_name_ptr,
+        data_ptr,
+        nbytes,
+    )
+    .map(drop)
+}

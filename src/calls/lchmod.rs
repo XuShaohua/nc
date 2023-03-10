c40f4fd84dc297fc6c6974f469234e4213e6dc0e
Xu Shaohua <shaohua@biofan.org>
2023-03-10 08:26:38 +0800

    calls: Add lchmod()

---
+++ src/calls/lchmod.rs
@@ -0,0 +1,7 @@
+/// Change permissions of a file.
+pub unsafe fn lchmod<P: AsRef<Path>>(filename: P, mode: mode_t) -> Result<(), Errno> {
+    let filename = CString::new(filename.as_ref());
+    let filename_ptr = filename.as_ptr() as usize;
+    let mode = mode as usize;
+    syscall2(SYS_LCHMOD, filename_ptr, mode).map(drop)
+}

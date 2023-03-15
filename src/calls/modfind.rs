2235c4bfac4a1ca59c3ae819b41760bc62d580cc
Xu Shaohua <shaohua@biofan.org>
2023-03-15 08:45:29 +0800

    calls: Add modfind()

---
+++ src/calls/modfind.rs
@@ -0,0 +1,6 @@
+/// Return the modid of a kernel module.
+pub unsafe fn modfind(name: &str) -> Result<i32, Errno> {
+    let name = CString::new(name);
+    let name_ptr = name.as_ptr() as usize;
+    syscall2(SYS_MODFIND, name_ptr).map(|ret| ret as i32)
+}

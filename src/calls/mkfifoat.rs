afc37246c64b9801075693e3ea7073e873262a6a
Xu Shaohua <shaohua@biofan.org>
2023-03-15 08:43:34 +0800

    calls: Add mkfifoat()

---
+++ src/calls/mkfifoat.rs
@@ -0,0 +1,8 @@
+/// Create a fifo file.
+pub unsafe fn mkfifoat<P: AsRef<Path>>(dfd: i32, path: P, mode: mode_t) -> Result<(), Errno> {
+    let dfd = dfd as usize;
+    let path = CString::new(path.as_ref());
+    let path_ptr = path.as_ptr() as usize;
+    let mode = mode as usize;
+    syscall2(SYS_MKFIFOAT, dfd, path_ptr, mode).map(drop)
+}

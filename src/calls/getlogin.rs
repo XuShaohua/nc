b1679ef0ae0edb529900c33879e94c3e073c0724
Xu Shaohua <shaohua@biofan.org>
2023-03-15 09:34:29 +0800

    calls: Add setlogin()

--- src/calls/getlogin.rs
+++ src/calls/getlogin.rs
@@ -3,5 +3,5 @@ pub unsafe fn getlogin(name: &mut [u8]) -> Result<(), Errno> {
     // TODO(Shaohua): Convert to CString
     let name_ptr = name.as_mut_ptr() as usize;
     let len = name.len();
-    syscall2(SYS_GETLOGIN, name_ptr, len).map(|ret| ret as i32)
+    syscall2(SYS_GETLOGIN, name_ptr, len).map(drop)
 }

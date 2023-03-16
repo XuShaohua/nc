b05de0bb831b8082f37193b93dfef88eea3bbe38
Xu Shaohua <shaohua@biofan.org>
2023-03-16 09:28:24 +0800

    calls: Add setaudit_addr()

---
+++ src/calls/setaudit_addr.rs
@@ -0,0 +1,6 @@
+/// Set audit session state
+pub unsafe fn setaudit_addr(info: &mut auditinfo_addr_t, length: u32) -> Result<(), Errno> {
+    let info_ptr = info as *mut auditinfo_addr_t as usize;
+    let length = length as usize;
+    syscall2(SYS_SETAUDIT_ADDR, info_ptr, length).map(drop)
+}

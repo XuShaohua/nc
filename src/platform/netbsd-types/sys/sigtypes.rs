20855d66c3bd44ea19a1981b9e5dc0de20f6d80b
Xu Shaohua <shaohua@biofan.org>
2023-03-22 14:29:52 +0800

    netbsd: Add sigtypes header

--- src/platform/netbsd-types/sys/sigtypes.rs
+++ src/platform/netbsd-types/sys/sigtypes.rs
@@ -4,8 +4,25 @@
 
 //! From `sys/sigtypes.h`
 
+use crate::size_t;
+
 #[repr(C)]
 #[derive(Debug, Default, Clone)]
 pub struct sigset_t {
     pub __bits: [u32; 4],
 }
+
+#[repr(C)]
+#[derive(Debug, Default, Clone)]
+pub struct stack_t {
+    /// signal stack base
+    pub ss_sp: usize,
+
+    /// signal stack length
+    pub ss_size: size_t,
+
+    /// SS_DISABLE and/or SS_ONSTACK
+    pub ss_flags: i32,
+}
+
+pub type sigaltstack_t = stack_t;

dd4bcfab3002550a5acaab5cfb9865f7a2327ad0
Xu Shaohua <shaohua@biofan.org>
2023-03-23 15:15:04 +0800

    netbsd: Regenerate calls

--- src/platform/netbsd-x86_64/call.rs
+++ src/platform/netbsd-x86_64/call.rs
@@ -112,9 +112,9 @@ pub unsafe fn aio_write(job: &mut aiocb_t) -> Result<(), Errno> {
 }
 
 /// Bind a name to a socket.
-pub unsafe fn bind(sockfd: i32, addr: *const sockaddr_t, addrlen: socklen_t) -> Result<(), Errno> {
+pub unsafe fn bind(sockfd: i32, addr: &sockaddr_t, addrlen: socklen_t) -> Result<(), Errno> {
     let sockfd = sockfd as usize;
-    let addr_ptr = addr as usize;
+    let addr_ptr = addr as *const sockaddr_t as usize;
     let addrlen = addrlen as usize;
     syscall3(SYS_BIND, sockfd, addr_ptr, addrlen).map(drop)
 }

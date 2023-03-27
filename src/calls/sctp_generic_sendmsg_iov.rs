14f52960a616094def246af9244bcb3ce824c26c
Xu Shaohua <shaohua@biofan.org>
2023-03-27 08:14:01 +0800

    calls: Add sctp_generic_sendmsg_iov()

--- src/calls/sctp_generic_sendmsg_iov.rs
+++ src/calls/sctp_generic_sendmsg_iov.rs
@@ -1,23 +1,23 @@
 /// Send a message on a socket.
-pub unsafe fn sctp_generic_sendmsg(
+pub unsafe fn sctp_generic_sendmsg_iov(
     sockfd: i32,
-    msg: &[u8],
+    iov: &mut [iovec_t],
     to: &[&sockaddr_t],
     sinfo: &mut sctp_sndrcvinfo_t,
     flags: i32,
 ) -> Result<ssize_t, Errno> {
     let sockfd = sockfd as usize;
-    let msg_ptr = msg.as_ptr() as usize;
-    let msg_len = msg.len();
+    let iov_ptr = iov.as_mut_ptr() as usize;
+    let iov_len = iov.len();
     let to_ptr = to.as_ptr() as usize;
     let to_len = to.len();
     let sinfo_ptr = sinfo as *mut sctp_sndrcvinfo_t as usize;
     let flags = flags as usize;
     syscall7(
-        SYS_SCTP_GENERIC_SENDMSG,
+        SYS_SCTP_GENERIC_SENDMSG_IOV,
         sockfd,
-        msg_ptr,
-        msg_len,
+        iov_ptr,
+        iov_len,
         to_ptr,
         to_len,
         sinfo_ptr,

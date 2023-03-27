c400059cc0d9b5fde745a2f87043f4602da7e36d
Xu Shaohua <shaohua@biofan.org>
2023-03-27 09:05:12 +0800

    calls: Add rctl_remove_rule()

--- src/calls/rctl_remove_rule.rs
+++ src/calls/rctl_remove_rule.rs
@@ -1,11 +1,12 @@
-/// Adds the rule pointed to by inbuf to the resource limits database.
-pub unsafe fn rctl_add_rule(inbuf: &[u8], outbuf: &mut [u8]) -> Result<(), Errno> {
+/// Removes all rules matching the filter passed in inbuf from the resource limits
+/// database.
+pub unsafe fn rctl_remove_rule(inbuf: &[u8], outbuf: &mut [u8]) -> Result<(), Errno> {
     let inbuf_ptr = inbuf.as_ptr() as usize;
     let inbuf_len = inbuf.len();
     let outbuf_ptr = outbuf.as_mut_ptr() as usize;
     let outbuf_len = outbuf.len();
     syscall4(
-        SYS_RCTL_ADD_RULE,
+        SYS_RCTL_REMOVE_RULE,
         inbuf_ptr,
         inbuf_len,
         outbuf_ptr,

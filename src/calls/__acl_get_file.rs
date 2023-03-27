b3a33bcd4663ff62ff47042d0d34b36ee1ef1477
Xu Shaohua <shaohua@biofan.org>
2023-03-27 10:00:03 +0800

    calls: Add __acl_get_file()

---
+++ src/calls/__acl_get_file.rs
@@ -0,0 +1,11 @@
+pub unsafe fn __acl_get_file<P: AsRef<Path>>(
+    path: P,
+    type_: acl_type_t,
+    acl: &mut acl_t,
+) -> Result<(), Errno> {
+    let path = CString::new(path.as_ref());
+    let path_ptr = path.as_ptr() as usize;
+    let type_ = type_ as usize;
+    let acl_ptr = acl as *mut acl_t as usize;
+    syscall3(SYS___ACL_GET_FILE, path_ptr, type_, acl_ptr).map(drop)
+}

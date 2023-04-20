567a8d161857efcd112e97d904d8a7a259174524
Xu Shaohua <shaohua@biofan.org>
2023-04-20 09:55:39 +0800

    calls: Add getsockopt2()

---
+++ src/calls/getsockopt2.rs
@@ -0,0 +1,23 @@
+/// Get options on sockets
+pub unsafe fn getsockopt2(
+    sockfd: i32,
+    level: i32,
+    optname: i32,
+    optval: &mut usize,
+    optlen: &mut socklen_t,
+) -> Result<(), Errno> {
+    let sockfd = sockfd as usize;
+    let level = level as usize;
+    let optname = optname as usize;
+    let optval_ptr = optval as *mut usize as usize;
+    let optlen_ptr = optlen as *mut socklen_t as usize;
+    syscall5(
+        SYS_GETSOCKOPT2,
+        sockfd,
+        level,
+        optname,
+        optval_ptr,
+        optlen_ptr,
+    )
+    .map(drop)
+}

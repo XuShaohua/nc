2e3ed1ee9923420ff1ef3184f31942d5fd69ba06
Xu Shaohua <shaohua@biofan.org>
2023-04-21 10:58:20 +0800

    calls: Add __posix_lchown()

---
+++ src/calls/__posix_lchown.rs
@@ -0,0 +1,12 @@
+/// Change ownership of a file. Does not deference symbolic link.
+pub unsafe fn __posix_lchown<P: AsRef<Path>>(
+    filename: P,
+    user: uid_t,
+    group: gid_t,
+) -> Result<(), Errno> {
+    let filename = CString::new(filename.as_ref());
+    let filename_ptr = filename.as_ptr() as usize;
+    let user = user as usize;
+    let group = group as usize;
+    syscall3(SYS___POSIX_LCHOWN, filename_ptr, user, group).map(drop)
+}

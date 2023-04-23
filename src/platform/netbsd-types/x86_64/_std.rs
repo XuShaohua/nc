24355db949e5f5eb59cdc2a5e18e9422acf8673b
Xu Shaohua <shaohua@biofan.org>
2023-04-23 20:25:12 +0800

    netbsd: Add _std header

--- src/platform/netbsd-types/x86_64/_std.rs
+++ src/platform/netbsd-types/x86_64/_std.rs
@@ -2,8 +2,7 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-mod _std;
-mod mcontext;
+//! C compiler builtin
 
-pub use _std::*;
-pub use mcontext::*;
+pub type __intmax_type__ = i64;
+pub type __uintmax_type__ = u64;

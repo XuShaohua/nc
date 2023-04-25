678fc96cb6f0efb459f2302f4f319807a2709b95
Xu Shaohua <shaohua@biofan.org>
2023-04-25 10:14:35 +0800

    darwin: Add _uintptr_t header

---
+++ src/platform/darwin-types/sys/_types/_uintptr_t.rs
@@ -0,0 +1,7 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+//! From `sys/_types/_uintptr_t.h`
+
+pub type uintptr_t = usize;

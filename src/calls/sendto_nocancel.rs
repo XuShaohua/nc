9a09143ee66460b7ada9c49d2e265b85989c7512
Xu Shaohua <shaohua@biofan.org>
2023-04-27 09:44:25 +0800

    calls: Add sendto_nocancel()

--- src/calls/sendto_nocancel.rs
+++ src/calls/sendto_nocancel.rs
@@ -1,19 +1,19 @@
 /// Send a message on a socket.
-pub unsafe fn sendto(
+pub unsafe fn sendto_nocancel(
     sockfd: i32,
     buf: &[u8],
     flags: i32,
-    dest_addr: &sockaddr_in_t,
+    dest_addr: caddr_t,
     addrlen: socklen_t,
 ) -> Result<ssize_t, Errno> {
     let sockfd = sockfd as usize;
     let buf_ptr = buf.as_ptr() as usize;
-    let len = buf.len(),
+    let len = buf.len();
     let flags = flags as usize;
     let dest_addr_ptr = dest_addr as *const sockaddr_in_t as usize;
     let addrlen = addrlen as usize;
     syscall6(
-        SYS_SENDTO,
+        SYS_SENDTO_NOCANCEL,
         sockfd,
         buf_ptr,
         len,

a27b259dc945bebe51e68347356b36a0812570b6
Xu Shaohua <shaohua@biofan.org>
2023-04-27 09:49:54 +0800

    calls: Add sem_close()

---
+++ src/calls/sem_close.rs
@@ -0,0 +1,5 @@
+/// Close an semaphore.
+pub unsafe fn sem_close(sem: &mut sem_t) -> Result<(), Errno> {
+    let sem_ptr = sem as *mut sem_t as usize;
+    syscall1(SYS_SEM_CLOSE, sem_ptr).map(drop)
+}

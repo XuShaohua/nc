028b7d18dfb7222643d561d9acf266f53dd5c16d
Xu Shaohua <shaohua@biofan.org>
2023-06-06 09:57:17 +0800

    call: Update parameter type in openat2()

--- src/platform/linux-x86/call.rs
+++ src/platform/linux-x86/call.rs
@@ -4459,13 +4459,13 @@ pub unsafe fn openat<P: AsRef<Path>>(
 pub unsafe fn openat2<P: AsRef<Path>>(
     dirfd: i32,
     pathname: P,
-    how: *mut open_how_t,
+    how: &mut open_how_t,
     size: size_t,
 ) -> Result<i32, Errno> {
     let dirfd = dirfd as usize;
     let pathname = CString::new(pathname.as_ref());
     let pathname_ptr = pathname.as_ptr() as usize;
-    let how_ptr = how as *const open_how_t as usize;
+    let how_ptr = how as *mut open_how_t as usize;
     syscall4(SYS_OPENAT2, dirfd, pathname_ptr, how_ptr, size).map(|ret| ret as i32)
 }
 

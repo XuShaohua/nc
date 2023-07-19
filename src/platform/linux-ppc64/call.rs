ec933d9aec2724497aa40d08f0b0a2c65e88106c
Xu Shaohua <shaohua@biofan.org>
2023-07-19 08:06:49 +0800

    linux: Regenerate calls

--- src/platform/linux-ppc64/call.rs
+++ src/platform/linux-ppc64/call.rs
@@ -1665,6 +1665,52 @@ pub unsafe fn futex(
     .map(|ret| ret as i32)
 }
 
+/// Wait on a list of futexes.
+///
+/// - `waiters`: List of futexes to wait on
+/// - `nr_futexes`: Length of futexv
+/// - `flags`: Flag for timeout (monotonic/realtime)
+/// - `timeout`: Optional absolute timeout.
+/// - `clockid`: Clock to be used for the timeout, realtime or monotonic.
+///
+/// Given an array of `struct futex_waitv_t`, wait on each uaddr.
+/// The thread wakes if a `futex_wake()` is performed at any uaddr.
+/// The syscall returns immediately if any waiter has `*uaddr != val`.
+///
+/// `timeout` is an optional timeout value for the operation.
+///
+/// Each waiter has individual flags. The `flags` argument for the syscall
+/// should be used solely for specifying the timeout as realtime, if needed.
+/// Flags for private futexes, sizes, etc. should be used on the individual flags
+/// of each waiter.
+///
+/// Returns the array index of one of the woken futexes. No further information
+/// is provided: any number of other futexes may also have been woken by the
+/// same event, and if more than one futex was woken, the retrned index may
+/// refer to any one of them. (It is not necessaryily the futex with the
+/// smallest index, nor the one most recently woken, nor...)
+pub unsafe fn futex_waitv(
+    waiters: &mut [futex_waitv_t],
+    flags: u32,
+    timeout: &mut timespec_t,
+    clockid: clockid_t,
+) -> Result<i32, Errno> {
+    let waiters_ptr = waiters.as_mut_ptr() as usize;
+    let waiters_len = waiters.len();
+    let flags = flags as usize;
+    let timeout_ptr = timeout as *mut timespec_t as usize;
+    let clockid = clockid as usize;
+    syscall5(
+        SYS_FUTEX_WAITV,
+        waiters_ptr,
+        waiters_len,
+        flags,
+        timeout_ptr,
+        clockid,
+    )
+    .map(|ret| ret as i32)
+}
+
 /// Change timestamp of a file relative to a directory file discriptor.
 ///
 /// # Example
@@ -6570,6 +6616,15 @@ pub unsafe fn set_mempolicy(mode: i32, nmask: *const usize, maxnode: usize) -> R
     let mode = mode as usize;
     let nmask = nmask as usize;
     syscall3(SYS_SET_MEMPOLICY, mode, nmask, maxnode).map(drop)
+}
+
+pub unsafe fn set_mempolicy_home_node(
+    start: usize,
+    len: usize,
+    home_node: usize,
+    flags: usize,
+) -> Result<(), Errno> {
+    syscall4(SYS_SET_MEMPOLICY_HOME_NODE, start, len, home_node, flags).map(drop)
 }
 
 /// Set the robust-futex list head of a task.

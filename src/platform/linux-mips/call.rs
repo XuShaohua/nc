33ddb11f358745e261eb48e4eba8b75504557b75
Xu Shaohua <shaohua@biofan.org>
2023-09-19 09:01:44 +0800

    linux: Remove profil()

--- src/platform/linux-mips/call.rs
+++ src/platform/linux-mips/call.rs
@@ -5088,14 +5088,6 @@ pub unsafe fn process_vm_writev(
     .map(|ret| ret as ssize_t)
 }
 
-/// Control process profiling
-pub unsafe fn profil(samples: &mut [u8], offset: vm_offset_t, scale: i32) -> Result<(), Errno> {
-    let samples_ptr = samples.as_mut_ptr() as usize;
-    let size = samples.len();
-    let scale = scale as usize;
-    syscall4(SYS_PROFIL, samples_ptr, size, offset, scale).map(drop)
-}
-
 /// Sychronous I/O multiplexing.
 ///
 /// Most architectures can't handle 7-argument syscalls. So we provide a

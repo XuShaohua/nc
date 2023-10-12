d25447f4a2401fa766d971592d2d8213982c8d3e
Xu Shaohua <shaohua@biofan.org>
2023-10-12 11:41:24 +0800

    linux/arm: Move to sub-directory

--- src/platform/linux-types/arm/mod.rs
+++ src/platform/linux-types/arm/mod.rs
@@ -2,18 +2,6 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-mod fcntl;
-mod ioctls;
-mod posix_types;
-mod ptrace;
-mod signal;
-mod stat;
-mod uapi_signal;
+mod uapi;
 
-pub use fcntl::*;
-pub use ioctls::*;
-pub use posix_types::*;
-pub use ptrace::*;
-pub use signal::*;
-pub use stat::*;
-pub use uapi_signal::*;
+pub use uapi::*;

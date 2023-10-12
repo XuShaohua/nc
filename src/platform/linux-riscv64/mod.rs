d21359281c63b11abca023fa9582385acd539b79
Xu Shaohua <shaohua@biofan.org>
2023-10-12 11:29:54 +0800

    linux: Add riscv64 platform

---
+++ src/platform/linux-riscv64/mod.rs
@@ -0,0 +1,11 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+pub mod call;
+pub mod errno;
+pub mod sysno;
+
+pub use call::*;
+pub use errno::*;
+pub use sysno::*;

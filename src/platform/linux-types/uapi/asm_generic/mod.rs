f5cedaa60f98368705cabd13692cf1b6669b5080
Xu Shaohua <shaohua@biofan.org>
2024-05-29 21:59:09 +0800

    linux-types: Rename test_arch to target_arch

--- src/platform/linux-types/uapi/asm_generic/mod.rs
+++ src/platform/linux-types/uapi/asm_generic/mod.rs
@@ -15,9 +15,9 @@ mod signal;
 ))]
 pub use signal::*;
 
-#[cfg(any(test_arch = "loongarch64", target_arch = "riscv64"))]
+#[cfg(any(target_arch = "loongarch64", target_arch = "riscv64"))]
 mod stat;
-#[cfg(any(test_arch = "loongarch64", target_arch = "riscv64"))]
+#[cfg(any(target_arch = "loongarch64", target_arch = "riscv64"))]
 pub use stat::*;
 
 mod fcntl;

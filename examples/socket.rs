3c0ddb08a5a33e2edab46a1321f7ef087cbddb18
Xu Shaohua <shaohua@biofan.org>
2024-05-29 23:10:13 +0800

    examples: Listen and close socket

--- examples/socket.rs
+++ examples/socket.rs
@@ -2,33 +2,63 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-#![allow(unreachable_code)]
-
-use core::mem::{size_of, transmute};
 use nc::Errno;
+use std::mem::{size_of, transmute};
+use std::thread;
 
-fn htons(host: u16) -> u16 {
+const SERVER_PORT: u16 = 18080;
+
+#[must_use]
+#[inline]
+const fn htons(host: u16) -> u16 {
     host.to_be()
 }
 
 fn main() -> Result<(), Errno> {
     let listen_fd = unsafe { nc::socket(nc::AF_INET, nc::SOCK_STREAM, 0)? };
+    println!("listen fd: {listen_fd}");
 
     let addr = nc::sockaddr_in_t {
         sin_family: nc::AF_INET as nc::sa_family_t,
-        sin_port: htons(80),
+        sin_port: htons(SERVER_PORT),
         sin_addr: nc::in_addr_t {
             s_addr: nc::INADDR_ANY as u32,
         },
         ..Default::default()
     };
+    println!("addr: {addr:?}");
+
+    let ret = unsafe {
+        let addr_alias = transmute::<&nc::sockaddr_in_t, &nc::sockaddr_t>(&addr);
+        nc::bind(listen_fd, addr_alias, size_of::<nc::sockaddr_in_t>() as u32)
+    };
+    assert!(ret.is_ok());
+
+    // Start worker thread
+    thread::spawn(|| {
+        println!("worker thread started");
+        let socket_fd = unsafe { nc::socket(nc::AF_INET, nc::SOCK_STREAM, 0) };
+        assert!(socket_fd.is_ok());
+        if let Ok(socket_fd) = socket_fd {
+            let addr = nc::sockaddr_in_t {
+                sin_family: nc::AF_INET as nc::sa_family_t,
+                sin_port: htons(SERVER_PORT),
+                sin_addr: nc::in_addr_t {
+                    s_addr: nc::INADDR_ANY as u32,
+                },
+                ..Default::default()
+            };
+            unsafe {
+                let addr_alias = transmute::<&nc::sockaddr_in_t, &nc::sockaddr_t>(&addr);
+                let ret = nc::connect(socket_fd, addr_alias, size_of::<nc::sockaddr_in_t>() as u32);
+                assert_eq!(ret, Ok(()));
+            }
+        } else {
+            eprintln!("Failed to create socket");
+        }
+    });
 
     unsafe {
-        let addr_alias = transmute::<&nc::sockaddr_in_t, &nc::sockaddr_t>(&addr);
-        let ret = nc::bind(listen_fd, addr_alias, size_of::<nc::sockaddr_in_t>() as u32);
-        assert_eq!(ret, Err(nc::EACCES));
-        return Ok(());
-
         nc::listen(listen_fd, nc::SOCK_STREAM)?;
     }
 
@@ -42,7 +72,11 @@ fn main() -> Result<(), Errno> {
             nc::SOCK_CLOEXEC,
         )?
     };
-    println!("conn_fd: {:?}", conn_fd);
+    println!("conn_fd: {conn_fd}");
+
+    unsafe {
+        nc::close(listen_fd)?;
+    }
 
     Ok(())
 }

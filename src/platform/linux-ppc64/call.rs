047539e4519b2b22e8114b5ab3fb5d7e82c67c60
Xu Shaohua <shaohua@biofan.org>
2024-07-23 16:17:04 +0800

    linux: Add futex_wake()

--- src/platform/linux-ppc64/call.rs
+++ src/platform/linux-ppc64/call.rs
@@ -1793,6 +1793,22 @@ pub unsafe fn futex_waitv(
     .map(|ret| ret as i32)
 }
 
+/// Wake a number of futexes.
+///
+/// - uaddr: Address of the futex(es) to wake
+/// - mask: bitmask
+/// - nr: Number of the futexes to wake
+/// - flags: `FUTEX2` flags
+///
+/// Identical to the traditional `FUTEX_WAKE_BITSET` op, except it is part of the
+/// futex2 family of calls.
+pub unsafe fn futex_wake(uaddr: *mut (), mask: usize, nr: i32, flags: u32) -> Result<(), Errno> {
+    let uaddr = uaddr as usize;
+    let nr = nr as usize;
+    let flags = flags as usize;
+    syscall4(SYS_FUTEX_WAKE, uaddr, mask, nr, flags).map(drop)
+}
+
 /// Change timestamp of a file relative to a directory file discriptor.
 ///
 /// # Example

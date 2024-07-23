152a1e62e2817dd5341db2d81d7f6905c16fbb9a
Xu Shaohua <shaohua@biofan.org>
2024-07-23 16:55:19 +0800

    linux: Add listmount()

--- src/platform/linux-ppc64/call.rs
+++ src/platform/linux-ppc64/call.rs
@@ -3160,6 +3160,14 @@ pub unsafe fn listen(sockfd: i32, backlog: i32) -> Result<(), Errno> {
     syscall2(SYS_LISTEN, sockfd, backlog).map(drop)
 }
 
+pub unsafe fn listmount(req: &mnt_id_req_t, mnt_ids: &mut [u64], flags: u32) -> Result<(), Errno> {
+    let req_ptr = req as *const mnt_id_req_t as usize;
+    let mnt_ids_ptr = mnt_ids.as_mut_ptr() as usize;
+    let nr_mnt_ids = mnt_ids.len();
+    let flags = flags as usize;
+    syscall4(SYS_LISTMOUNT, req_ptr, mnt_ids_ptr, nr_mnt_ids, flags).map(drop)
+}
+
 /// List extended attribute names.
 ///
 /// # Example

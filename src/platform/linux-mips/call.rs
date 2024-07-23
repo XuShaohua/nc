4c392109b7b73f3b019b576a9f3f56c114b4e83d
Xu Shaohua <shaohua@biofan.org>
2024-07-23 15:50:53 +0800

    linux: Add fchmodat2()

--- src/platform/linux-mips/call.rs
+++ src/platform/linux-mips/call.rs
@@ -1192,6 +1192,45 @@ pub unsafe fn fchmodat<P: AsRef<Path>>(dirfd: i32, filename: P, mode: mode_t) ->
     syscall3(SYS_FCHMODAT, dirfd, filename_ptr, mode).map(drop)
 }
 
+/// Change permissions of a file.
+///
+/// Like `fchmodat()`, with specifying extra `flags`.
+///
+/// # Example
+///
+/// ```
+/// let filename = "/tmp/nc-fchmodat2";
+/// let fd = unsafe {
+///     nc::openat(
+///         nc::AT_FDCWD,
+///         filename,
+///         nc::O_CREAT | nc::O_WRONLY | nc::O_TRUNC,
+///         0o644
+///     )
+/// };
+/// assert!(fd.is_ok());
+/// let fd = fd.unwrap();
+/// let ret = unsafe { nc::close(fd) };
+/// assert!(ret.is_ok());
+/// let ret = unsafe { nc::fchmodat2(nc::AT_FDCWD, filename, 0o600, nc::AT_SYMLINK_NOFOLLOW) };
+/// assert!(ret.is_ok());
+/// let ret = unsafe { nc::unlinkat(nc::AT_FDCWD, filename, 0) };
+/// assert!(ret.is_ok());
+/// ```
+pub unsafe fn fchmodat2<P: AsRef<Path>>(
+    dirfd: i32,
+    filename: P,
+    mode: mode_t,
+    flags: u32,
+) -> Result<(), Errno> {
+    let dirfd = dirfd as usize;
+    let filename = CString::new(filename.as_ref());
+    let filename_ptr = filename.as_ptr() as usize;
+    let mode = mode as usize;
+    let flags = flags as usize;
+    syscall4(SYS_FCHMODAT, dirfd, filename_ptr, mode, flags).map(drop)
+}
+
 /// Change ownership of a file.
 ///
 /// # Example

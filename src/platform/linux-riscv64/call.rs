544a431d2111acb58dc7cf723fbd48d87ae5c0b1
Xu Shaohua <shaohua@biofan.org>
2024-07-29 22:28:09 +0800

    calls: Add unittest for futex()

--- src/platform/linux-riscv64/call.rs
+++ src/platform/linux-riscv64/call.rs
@@ -1457,6 +1457,50 @@ pub unsafe fn ftruncate(fd: i32, length: off_t) -> Result<(), Errno> {
 /// - `val`: expected value
 /// - `utime`: waiting timeout
 /// - `uaddr2`: target futext user address used for requeue
+///
+/// # Examples
+///
+/// ```rust
+/// use std::sync::atomic::{AtomicU32, Ordering};
+/// use std::thread;
+/// use std::time::Duration;
+///
+/// const NOTIFY_WAIT: u32 = 0;
+/// const NOTIFY_WAKE: u32 = 1;
+///
+/// fn wake_one(count: &AtomicU32) {
+///     let ret = unsafe { nc::futex(count, nc::FUTEX_WAKE, NOTIFY_WAKE, None, None, 0) };
+///     assert!(ret.is_ok());
+/// }
+///
+/// fn wait(count: &AtomicU32, expected: u32) {
+///     let ret = unsafe { nc::futex(count, nc::FUTEX_WAIT, expected, None, None, 0) };
+///     assert!(ret.is_ok());
+/// }
+///
+/// fn main() {
+///     let notify = AtomicU32::new(0);
+///
+///     thread::scope(|s| {
+///         // Create the notify thread.
+///         s.spawn(|| {
+///             // Wake up some other threads after one second.
+///             println!("[notify] Sleep for 1s");
+///             thread::sleep(Duration::from_secs(1));
+///             println!("[notify] Wake up main thread");
+///             notify.store(NOTIFY_WAKE, Ordering::Relaxed);
+///             wake_one(&notify);
+///         });
+///
+///         // Main thread will wait until the notify thread wakes it up.
+///         println!("[main] Waiting for notification..");
+///         while notify.load(Ordering::Relaxed) == NOTIFY_WAIT {
+///             wait(&notify, NOTIFY_WAIT);
+///         }
+///         println!("[main] Got wake up");
+///     });
+/// }
+/// ```
 pub unsafe fn futex(
     uaddr: &AtomicU32,
     op: i32,

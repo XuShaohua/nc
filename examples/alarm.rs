e3a65b1624171f3c5867085268d092f8916779a7
Xu Shaohua <shaohua@biofan.org>
2024-07-30 07:10:42 +0800

    examples: Update alarm

--- examples/alarm.rs
+++ examples/alarm.rs
@@ -4,19 +4,20 @@
 
 use core::mem::{size_of, size_of_val};
 
+#[no_mango]
 fn handle_alarm(signum: i32) {
-    println!("fuck alarm");
+    println!("handle alarm");
     assert_eq!(signum, nc::SIGALRM);
-    //nc::exit(3);
 }
 
 fn main() {
     let sa = nc::sigaction_t {
-        sa_handler: handle_alarm as nc::sighandler_t,
-        sa_flags: nc::SA_RESTART,
+        sa_handler: nc::SIG_IGN,
+        //sa_handler: handle_alarm as nc::sighandler_t,
+        sa_flags: nc::SA_RESTART | nc::SA_SIGINFO | nc::SA_ONSTACK,
         ..nc::sigaction_t::default()
     };
-    println!("sa.sa_mask size: {}", size_of_val(&sa));
+    println!("sa size: {}", size_of_val(&sa));
     let mut old_sa = nc::sigaction_t::default();
     let ret = unsafe { nc::rt_sigaction(nc::SIGALRM, &sa, &mut old_sa, size_of::<nc::sigset_t>()) };
     assert!(ret.is_ok());

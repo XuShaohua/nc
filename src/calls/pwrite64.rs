00c3c9bf7764b6e9b03b11a33f8c07f4fec13af3
Xu Shaohua <shaohua@biofan.org>
2024-07-30 10:07:05 +0800

    calls: Update params in pwrite() and pwrite64()

--- src/calls/pwrite64.rs
+++ src/calls/pwrite64.rs
@@ -7,22 +7,19 @@
 /// let ret = unsafe { nc::openat(nc::AT_FDCWD, path, nc::O_WRONLY | nc::O_CREAT, 0o644) };
 /// assert!(ret.is_ok());
 /// let fd = ret.unwrap();
-/// let buf = "Hello, Rust";
-/// let ret = unsafe { nc::pwrite64(fd, buf.as_ptr() as usize, buf.len(), 0) };
+/// let msg = "Hello, Rust";
+/// let ret = unsafe { nc::pwrite64(fd, msg.as_bytes(), 0) };
 /// assert!(ret.is_ok());
-/// assert_eq!(ret, Ok(buf.len() as nc::ssize_t));
+/// assert_eq!(ret, Ok(msg.len() as nc::ssize_t));
 /// let ret = unsafe { nc::close(fd) };
 /// assert!(ret.is_ok());
 /// let ret = unsafe { nc::unlinkat(nc::AT_FDCWD, path, 0) };
 /// assert!(ret.is_ok());
 /// ```
-pub unsafe fn pwrite64(
-    fd: i32,
-    buf: usize,
-    count: size_t,
-    offset: off_t,
-) -> Result<ssize_t, Errno> {
+pub unsafe fn pwrite64(fd: i32, buf: &[u8], offset: off_t) -> Result<ssize_t, Errno> {
     let fd = fd as usize;
+    let count = buf.len();
+    let buf_ptr = buf.as_ptr() as usize;
     let offset = offset as usize;
-    syscall4(SYS_PWRITE64, fd, buf, count, offset).map(|ret| ret as ssize_t)
+    syscall4(SYS_PWRITE64, fd, buf_ptr, count, offset).map(|ret| ret as ssize_t)
 }

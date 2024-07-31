00233611550fa702b004dd812ba763fabdb6535e
Xu Shaohua <shaohua@biofan.org>
2024-07-31 09:51:27 +0800

    calls: Add unittest to bind() and accept()

--- src/platform/linux-arm/call.rs
+++ src/platform/linux-arm/call.rs
@@ -21,6 +21,88 @@ use crate::types::*;
 /// Accept a connection on a socket.
 ///
 /// On success, `accept()` return a file descriptor for the accepted socket.
+///
+/// # Examples
+///
+/// ```
+/// use nc::Errno;
+/// use std::mem::{size_of, transmute};
+/// use std::thread;
+///
+/// const SERVER_PORT: u16 = 18082;
+///
+/// #[must_use]
+/// #[inline]
+/// const fn htons(host: u16) -> u16 {
+///     host.to_be()
+/// }
+///
+/// fn main() -> Result<(), Errno> {
+///     let listen_fd = unsafe { nc::socket(nc::AF_INET, nc::SOCK_STREAM, 0)? };
+///     println!("listen fd: {listen_fd}");
+///
+///     let addr = nc::sockaddr_in_t {
+///         sin_family: nc::AF_INET as nc::sa_family_t,
+///         sin_port: htons(SERVER_PORT),
+///         sin_addr: nc::in_addr_t {
+///             s_addr: nc::INADDR_ANY as u32,
+///         },
+///         ..Default::default()
+///     };
+///     println!("addr: {addr:?}");
+///
+///     let ret = unsafe {
+///         let addr_alias = transmute::<&nc::sockaddr_in_t, &nc::sockaddr_t>(&addr);
+///         nc::bind(listen_fd, addr_alias, size_of::<nc::sockaddr_in_t>() as u32)
+///     };
+///     assert!(ret.is_ok());
+///
+///     // Start worker thread
+///     thread::spawn(|| {
+///         println!("worker thread started");
+///         let socket_fd = unsafe { nc::socket(nc::AF_INET, nc::SOCK_STREAM, 0) };
+///         assert!(socket_fd.is_ok());
+///         if let Ok(socket_fd) = socket_fd {
+///             let addr = nc::sockaddr_in_t {
+///                 sin_family: nc::AF_INET as nc::sa_family_t,
+///                 sin_port: htons(SERVER_PORT),
+///                 sin_addr: nc::in_addr_t {
+///                     s_addr: nc::INADDR_ANY as u32,
+///                 },
+///                 ..Default::default()
+///             };
+///             unsafe {
+///                 let addr_alias = transmute::<&nc::sockaddr_in_t, &nc::sockaddr_t>(&addr);
+///                 let ret = nc::connect(socket_fd, addr_alias, size_of::<nc::sockaddr_in_t>() as u32);
+///                 assert_eq!(ret, Ok(()));
+///             }
+///         } else {
+///             eprintln!("Failed to create socket");
+///         }
+///     });
+///
+///     unsafe {
+///         nc::listen(listen_fd, nc::SOCK_STREAM)?;
+///     }
+///
+///     let mut conn_addr = nc::sockaddr_in_t::default();
+///     let mut conn_addr_len: nc::socklen_t = 0;
+///     let conn_fd = unsafe {
+///         nc::accept(
+///             listen_fd,
+///             &mut conn_addr as *mut nc::sockaddr_in_t as *mut nc::sockaddr_t,
+///             &mut conn_addr_len,
+///         )?
+///     };
+///     println!("conn_fd: {conn_fd}");
+///
+///     unsafe {
+///         nc::close(listen_fd)?;
+///     }
+///
+///     Ok(())
+/// }
+/// ```
 pub unsafe fn accept(
     sockfd: i32,
     addr: *mut sockaddr_t,
@@ -33,6 +115,89 @@ pub unsafe fn accept(
 }
 
 /// Accept a connection on a socket.
+///
+/// # Examples
+///
+/// ```
+/// use nc::Errno;
+/// use std::mem::{size_of, transmute};
+/// use std::thread;
+///
+/// const SERVER_PORT: u16 = 18083;
+///
+/// #[must_use]
+/// #[inline]
+/// const fn htons(host: u16) -> u16 {
+///     host.to_be()
+/// }
+///
+/// fn main() -> Result<(), Errno> {
+///     let listen_fd = unsafe { nc::socket(nc::AF_INET, nc::SOCK_STREAM, 0)? };
+///     println!("listen fd: {listen_fd}");
+///
+///     let addr = nc::sockaddr_in_t {
+///         sin_family: nc::AF_INET as nc::sa_family_t,
+///         sin_port: htons(SERVER_PORT),
+///         sin_addr: nc::in_addr_t {
+///             s_addr: nc::INADDR_ANY as u32,
+///         },
+///         ..Default::default()
+///     };
+///     println!("addr: {addr:?}");
+///
+///     let ret = unsafe {
+///         let addr_alias = transmute::<&nc::sockaddr_in_t, &nc::sockaddr_t>(&addr);
+///         nc::bind(listen_fd, addr_alias, size_of::<nc::sockaddr_in_t>() as u32)
+///     };
+///     assert!(ret.is_ok());
+///
+///     // Start worker thread
+///     thread::spawn(|| {
+///         println!("worker thread started");
+///         let socket_fd = unsafe { nc::socket(nc::AF_INET, nc::SOCK_STREAM, 0) };
+///         assert!(socket_fd.is_ok());
+///         if let Ok(socket_fd) = socket_fd {
+///             let addr = nc::sockaddr_in_t {
+///                 sin_family: nc::AF_INET as nc::sa_family_t,
+///                 sin_port: htons(SERVER_PORT),
+///                 sin_addr: nc::in_addr_t {
+///                     s_addr: nc::INADDR_ANY as u32,
+///                 },
+///                 ..Default::default()
+///             };
+///             unsafe {
+///                 let addr_alias = transmute::<&nc::sockaddr_in_t, &nc::sockaddr_t>(&addr);
+///                 let ret = nc::connect(socket_fd, addr_alias, size_of::<nc::sockaddr_in_t>() as u32);
+///                 assert_eq!(ret, Ok(()));
+///             }
+///         } else {
+///             eprintln!("Failed to create socket");
+///         }
+///     });
+///
+///     unsafe {
+///         nc::listen(listen_fd, nc::SOCK_STREAM)?;
+///     }
+///
+///     let mut conn_addr = nc::sockaddr_in_t::default();
+///     let mut conn_addr_len: nc::socklen_t = 0;
+///     let conn_fd = unsafe {
+///         nc::accept4(
+///             listen_fd,
+///             &mut conn_addr as *mut nc::sockaddr_in_t as *mut nc::sockaddr_t,
+///             &mut conn_addr_len,
+///             nc::SOCK_CLOEXEC,
+///         )?
+///     };
+///     println!("conn_fd: {conn_fd}");
+///
+///     unsafe {
+///         nc::close(listen_fd)?;
+///     }
+///
+///     Ok(())
+/// }
+/// ```
 pub unsafe fn accept4(
     sockfd: i32,
     addr: *mut sockaddr_t,
@@ -139,9 +304,92 @@ pub unsafe fn bdflush() {
 }
 
 /// Bind a name to a socket.
-pub unsafe fn bind(sockfd: i32, addr: &sockaddr_t, addrlen: socklen_t) -> Result<(), Errno> {
+///
+/// # Examples
+///
+/// ```
+/// use nc::Errno;
+/// use std::mem::{size_of, transmute};
+/// use std::thread;
+///
+/// const SERVER_PORT: u16 = 18084;
+///
+/// #[must_use]
+/// #[inline]
+/// const fn htons(host: u16) -> u16 {
+///     host.to_be()
+/// }
+///
+/// fn main() -> Result<(), Errno> {
+///     let listen_fd = unsafe { nc::socket(nc::AF_INET, nc::SOCK_STREAM, 0)? };
+///     println!("listen fd: {listen_fd}");
+///
+///     let addr = nc::sockaddr_in_t {
+///         sin_family: nc::AF_INET as nc::sa_family_t,
+///         sin_port: htons(SERVER_PORT),
+///         sin_addr: nc::in_addr_t {
+///             s_addr: nc::INADDR_ANY as u32,
+///         },
+///         ..Default::default()
+///     };
+///     println!("addr: {addr:?}");
+///
+///     let ret = unsafe {
+///         let addr_alias = transmute::<&nc::sockaddr_in_t, &nc::sockaddr_t>(&addr);
+///         nc::bind(listen_fd, addr_alias, size_of::<nc::sockaddr_in_t>() as u32)
+///     };
+///     assert!(ret.is_ok());
+///
+///     // Start worker thread
+///     thread::spawn(|| {
+///         println!("worker thread started");
+///         let socket_fd = unsafe { nc::socket(nc::AF_INET, nc::SOCK_STREAM, 0) };
+///         assert!(socket_fd.is_ok());
+///         if let Ok(socket_fd) = socket_fd {
+///             let addr = nc::sockaddr_in_t {
+///                 sin_family: nc::AF_INET as nc::sa_family_t,
+///                 sin_port: htons(SERVER_PORT),
+///                 sin_addr: nc::in_addr_t {
+///                     s_addr: nc::INADDR_ANY as u32,
+///                 },
+///                 ..Default::default()
+///             };
+///             unsafe {
+///                 let addr_alias = transmute::<&nc::sockaddr_in_t, &nc::sockaddr_t>(&addr);
+///                 let ret = nc::connect(socket_fd, addr_alias, size_of::<nc::sockaddr_in_t>() as u32);
+///                 assert_eq!(ret, Ok(()));
+///             }
+///         } else {
+///             eprintln!("Failed to create socket");
+///         }
+///     });
+///
+///     unsafe {
+///         nc::listen(listen_fd, nc::SOCK_STREAM)?;
+///     }
+///
+///     let mut conn_addr = nc::sockaddr_in_t::default();
+///     let mut conn_addr_len: nc::socklen_t = 0;
+///     let conn_fd = unsafe {
+///         nc::accept4(
+///             listen_fd,
+///             &mut conn_addr as *mut nc::sockaddr_in_t as *mut nc::sockaddr_t,
+///             &mut conn_addr_len,
+///             nc::SOCK_CLOEXEC,
+///         )?
+///     };
+///     println!("conn_fd: {conn_fd}");
+///
+///     unsafe {
+///         nc::close(listen_fd)?;
+///     }
+///
+///     Ok(())
+/// }
+/// ```
+pub unsafe fn bind(sockfd: i32, addr: *const sockaddr_t, addrlen: socklen_t) -> Result<(), Errno> {
     let sockfd = sockfd as usize;
-    let addr_ptr = addr as *const sockaddr_t as usize;
+    let addr_ptr = addr as usize;
     let addrlen = addrlen as usize;
     syscall3(SYS_BIND, sockfd, addr_ptr, addrlen).map(drop)
 }
@@ -482,6 +730,89 @@ pub unsafe fn close_range(fd: u32, max_fd: u32, flags: u32) -> Result<(), Errno>
 }
 
 /// Initialize a connection on a socket.
+///
+/// # Examples
+///
+/// ```
+/// use nc::Errno;
+/// use std::mem::{size_of, transmute};
+/// use std::thread;
+///
+/// const SERVER_PORT: u16 = 18081;
+///
+/// #[must_use]
+/// #[inline]
+/// const fn htons(host: u16) -> u16 {
+///     host.to_be()
+/// }
+///
+/// fn main() -> Result<(), Errno> {
+///     let listen_fd = unsafe { nc::socket(nc::AF_INET, nc::SOCK_STREAM, 0)? };
+///     println!("listen fd: {listen_fd}");
+///
+///     let addr = nc::sockaddr_in_t {
+///         sin_family: nc::AF_INET as nc::sa_family_t,
+///         sin_port: htons(SERVER_PORT),
+///         sin_addr: nc::in_addr_t {
+///             s_addr: nc::INADDR_ANY as u32,
+///         },
+///         ..Default::default()
+///     };
+///     println!("addr: {addr:?}");
+///
+///     let ret = unsafe {
+///         let addr_alias = transmute::<&nc::sockaddr_in_t, &nc::sockaddr_t>(&addr);
+///         nc::bind(listen_fd, addr_alias, size_of::<nc::sockaddr_in_t>() as u32)
+///     };
+///     assert!(ret.is_ok());
+///
+///     // Start worker thread
+///     thread::spawn(|| {
+///         println!("worker thread started");
+///         let socket_fd = unsafe { nc::socket(nc::AF_INET, nc::SOCK_STREAM, 0) };
+///         assert!(socket_fd.is_ok());
+///         if let Ok(socket_fd) = socket_fd {
+///             let addr = nc::sockaddr_in_t {
+///                 sin_family: nc::AF_INET as nc::sa_family_t,
+///                 sin_port: htons(SERVER_PORT),
+///                 sin_addr: nc::in_addr_t {
+///                     s_addr: nc::INADDR_ANY as u32,
+///                 },
+///                 ..Default::default()
+///             };
+///             unsafe {
+///                 let addr_alias = transmute::<&nc::sockaddr_in_t, &nc::sockaddr_t>(&addr);
+///                 let ret = nc::connect(socket_fd, addr_alias, size_of::<nc::sockaddr_in_t>() as u32);
+///                 assert_eq!(ret, Ok(()));
+///             }
+///         } else {
+///             eprintln!("Failed to create socket");
+///         }
+///     });
+///
+///     unsafe {
+///         nc::listen(listen_fd, nc::SOCK_STREAM)?;
+///     }
+///
+///     let mut conn_addr = nc::sockaddr_in_t::default();
+///     let mut conn_addr_len: nc::socklen_t = 0;
+///     let conn_fd = unsafe {
+///         nc::accept4(
+///             listen_fd,
+///             &mut conn_addr as *mut nc::sockaddr_in_t as *mut nc::sockaddr_t,
+///             &mut conn_addr_len,
+///             nc::SOCK_CLOEXEC,
+///         )?
+///     };
+///     println!("conn_fd: {conn_fd}");
+///
+///     unsafe {
+///         nc::close(listen_fd)?;
+///     }
+///
+///     Ok(())
+/// }
+/// ```
 pub unsafe fn connect(
     sockfd: i32,
     addr: *const sockaddr_t,

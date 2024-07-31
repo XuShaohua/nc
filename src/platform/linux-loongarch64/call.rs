f422e0a6ba1329d76f4743ffc31883c01b32f6be
Xu Shaohua <shaohua@biofan.org>
2024-07-31 16:35:02 +0800

    calls: Update params in execve()
    Fix issue: https://github.com/XuShaohua/nc/issues/22

--- src/platform/linux-loongarch64/call.rs
+++ src/platform/linux-loongarch64/call.rs
@@ -935,26 +935,32 @@ pub unsafe fn eventfd2(count: u32, flags: i32) -> Result<i32, Errno> {
 
 /// Execute a new program.
 ///
-/// TODO(Shaohua): type of argv and env will be changed.
-/// And return value might be changed too.
-///
 /// # Examples
 ///
 /// ```
-/// let args = [""];
-/// let env = [""];
+/// let args = ["ls", "-l", "-a"];
+/// let env = ["DISPLAY=:0"];
 /// let ret = unsafe { nc::execve("/bin/ls", &args, &env) };
 /// assert!(ret.is_ok());
 /// ```
-pub unsafe fn execve<P: AsRef<Path>>(
-    filename: P,
-    argv: &[&str],
-    env: &[&str],
-) -> Result<(), Errno> {
+pub unsafe fn execve<P: AsRef<Path>>(filename: P, argv: &[P], env: &[P]) -> Result<(), Errno> {
     let filename = CString::new(filename.as_ref());
     let filename_ptr = filename.as_ptr() as usize;
-    let argv_ptr = argv.as_ptr() as usize;
-    let env_ptr = env.as_ptr() as usize;
+
+    // Construct argument list.
+    let argv_data: Vec<CString> = argv.iter().map(|arg| CString::new(arg.as_ref())).collect();
+    let mut argv_data_ptr: Vec<*const u8> = argv_data.iter().map(|arg| arg.as_ptr()).collect();
+    // Null-terminated
+    argv_data_ptr.push(core::ptr::null::<u8>());
+    let argv_ptr = argv_data_ptr.as_ptr() as usize;
+
+    // Construct environment list.
+    let env_data: Vec<CString> = env.iter().map(|item| CString::new(item.as_ref())).collect();
+    let mut env_data_ptr: Vec<*const u8> = env_data.iter().map(|item| item.as_ptr()).collect();
+    // Null-terminated
+    env_data_ptr.push(core::ptr::null::<u8>());
+    let env_ptr = env_data_ptr.as_ptr() as usize;
+
     syscall3(SYS_EXECVE, filename_ptr, argv_ptr, env_ptr).map(drop)
 }
 

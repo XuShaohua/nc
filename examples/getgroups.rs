bf6ccda718b4144625daf26c810e2c51c026c6d7
Xu Shaohua <shaohua@biofan.org>
2024-07-31 09:02:38 +0800

    calls: Update params in getgroups()

--- examples/getgroups.rs
+++ examples/getgroups.rs
@@ -5,22 +5,22 @@
 fn main() {
     {
         let mut groups = vec![];
-        let ret = unsafe { nc::getgroups(0, &mut groups) };
+        let ret = unsafe { nc::getgroups(&mut groups) };
         assert!(ret.is_ok());
         let total_num = ret.unwrap();
         groups.resize(total_num as usize, 0);
 
-        let ret = unsafe { nc::getgroups(total_num, &mut groups) };
+        let ret = unsafe { nc::getgroups(&mut groups) };
         assert!(ret.is_ok());
         assert_eq!(ret, Ok(total_num));
     }
 
     {
         let mut groups = vec![0; 8];
-        let mut ret = unsafe { nc::getgroups(groups.len() as i32, &mut groups) };
+        let mut ret = unsafe { nc::getgroups(&mut groups) };
         while ret.is_err() && ret == Err(nc::EINVAL) {
             groups.resize(groups.len() * 2, 0);
-            ret = unsafe { nc::getgroups(groups.len() as i32, &mut groups) };
+            ret = unsafe { nc::getgroups(&mut groups) };
         }
     }
 }
--- src/calls/getgroups.rs
+++ src/calls/getgroups.rs
@@ -4,17 +4,17 @@
 ///
 /// ```
 /// let mut groups = vec![];
-/// let ret = unsafe { nc::getgroups(0, &mut groups) };
+/// let ret = unsafe { nc::getgroups(&mut groups) };
 /// assert!(ret.is_ok());
 /// let total_num = ret.unwrap();
 /// groups.resize(total_num as usize, 0);
 ///
-/// let ret = unsafe { nc::getgroups(total_num, &mut groups) };
+/// let ret = unsafe { nc::getgroups(&mut groups) };
 /// assert!(ret.is_ok());
 /// assert_eq!(ret, Ok(total_num));
 /// ```
-pub unsafe fn getgroups(size: i32, group_list: &mut [gid_t]) -> Result<i32, Errno> {
-    let size = size as usize;
+pub unsafe fn getgroups(group_list: &mut [gid_t]) -> Result<i32, Errno> {
+    let size = group_list.len() as usize;
     let group_ptr = group_list.as_mut_ptr() as usize;
     syscall2(SYS_GETGROUPS, size, group_ptr).map(|ret| ret as i32)
 }

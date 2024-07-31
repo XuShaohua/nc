a905f3dec0725def45e870cc01e6a551bcc5e44d
Xu Shaohua <shaohua@biofan.org>
2024-07-31 08:53:43 +0800

    calls: Update params in ftruncate() and ftruncate64()

--- src/platform/linux-arm/call.rs
+++ src/platform/linux-arm/call.rs
@@ -1809,14 +1809,14 @@ pub unsafe fn fsync(fd: i32) -> Result<(), Errno> {
 /// let ret = unsafe { nc::openat(nc::AT_FDCWD, path, nc::O_WRONLY | nc::O_CREAT, 0o644) };
 /// assert!(ret.is_ok());
 /// let fd = ret.unwrap();
-/// let ret = unsafe { nc::ftruncate(fd, 64 * 1024) };
+/// let ret = unsafe { nc::ftruncate(fd as u32, 64 * 1024) };
 /// assert!(ret.is_ok());
 /// let ret = unsafe { nc::close(fd) };
 /// assert!(ret.is_ok());
 /// let ret = unsafe { nc::unlinkat(nc::AT_FDCWD, path, 0) };
 /// assert!(ret.is_ok());
 /// ```
-pub unsafe fn ftruncate(fd: i32, length: off_t) -> Result<(), Errno> {
+pub unsafe fn ftruncate(fd: u32, length: off_t) -> Result<(), Errno> {
     let fd = fd as usize;
     let length = length as usize;
     syscall2(SYS_FTRUNCATE, fd, length).map(drop)
@@ -1831,14 +1831,14 @@ pub unsafe fn ftruncate(fd: i32, length: off_t) -> Result<(), Errno> {
 /// let ret = unsafe { nc::openat(nc::AT_FDCWD, path, nc::O_WRONLY | nc::O_CREAT, 0o644) };
 /// assert!(ret.is_ok());
 /// let fd = ret.unwrap();
-/// let ret = unsafe { nc::ftruncate64(fd, 64 * 1024) };
+/// let ret = unsafe { nc::ftruncate64(fd as u32, 64 * 1024) };
 /// assert!(ret.is_ok());
 /// let ret = unsafe { nc::close(fd) };
 /// assert!(ret.is_ok());
 /// let ret = unsafe { nc::unlinkat(nc::AT_FDCWD, path, 0) };
 /// assert!(ret.is_ok());
 /// ```
-pub unsafe fn ftruncate64(fd: i32, len: loff_t) -> Result<(), Errno> {
+pub unsafe fn ftruncate64(fd: u32, len: loff_t) -> Result<(), Errno> {
     let fd = fd as usize;
     let len = len as usize;
     syscall2(SYS_FTRUNCATE64, fd, len).map(drop)
--- src/platform/linux-s390x/call.rs
+++ src/platform/linux-s390x/call.rs
@@ -1771,14 +1771,14 @@ pub unsafe fn fsync(fd: i32) -> Result<(), Errno> {
 /// let ret = unsafe { nc::openat(nc::AT_FDCWD, path, nc::O_WRONLY | nc::O_CREAT, 0o644) };
 /// assert!(ret.is_ok());
 /// let fd = ret.unwrap();
-/// let ret = unsafe { nc::ftruncate(fd, 64 * 1024) };
+/// let ret = unsafe { nc::ftruncate(fd as u32, 64 * 1024) };
 /// assert!(ret.is_ok());
 /// let ret = unsafe { nc::close(fd) };
 /// assert!(ret.is_ok());
 /// let ret = unsafe { nc::unlinkat(nc::AT_FDCWD, path, 0) };
 /// assert!(ret.is_ok());
 /// ```
-pub unsafe fn ftruncate(fd: i32, length: off_t) -> Result<(), Errno> {
+pub unsafe fn ftruncate(fd: u32, length: off_t) -> Result<(), Errno> {
     let fd = fd as usize;
     let length = length as usize;
     syscall2(SYS_FTRUNCATE, fd, length).map(drop)

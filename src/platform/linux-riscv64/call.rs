fa3b996bb98c6801d46d19a004d435e0c71cf74c
Xu Shaohua <shaohua@biofan.org>
2024-07-31 10:53:15 +0800

    calls: Update params in getsockopt()

--- src/platform/linux-riscv64/call.rs
+++ src/platform/linux-riscv64/call.rs
@@ -2661,25 +2661,68 @@ pub unsafe fn getsockname(
 }
 
 /// Get options on sockets
+///
+/// # Examples
+/// ```
+/// use std::mem::size_of_val;
+///
+/// fn main() -> Result<(), nc::Errno> {
+///     let socket_fd = unsafe { nc::socket(nc::AF_INET, nc::SOCK_STREAM, 0)? };
+///
+///     let set_queue_len: i32 = 5;
+///     {
+///         let buf_len = size_of_val(&set_queue_len) as nc::socklen_t;
+///         let ret = unsafe {
+///             nc::setsockopt(
+///                 socket_fd,
+///                 nc::IPPROTO_TCP,
+///                 nc::TCP_FASTOPEN,
+///                 &set_queue_len as *const i32 as *const _,
+///                 buf_len,
+///             )
+///         };
+///         assert!(ret.is_ok());
+///     }
+///
+///     let mut get_queue_len: i32 = 0;
+///     {
+///         let mut buf_len = size_of_val(&get_queue_len) as nc::socklen_t;
+///         let ret = unsafe {
+///             nc::getsockopt(
+///                 socket_fd,
+///                 nc::IPPROTO_TCP,
+///                 nc::TCP_FASTOPEN,
+///                 &mut get_queue_len as *mut i32 as *mut _,
+///                 &mut buf_len,
+///             )
+///         };
+///         assert!(ret.is_ok());
+///         println!("queue len: {get_queue_len}");
+///     }
+///     assert_eq!(set_queue_len, get_queue_len);
+///
+///     unsafe { nc::close(socket_fd) }
+/// }
+/// ```
 pub unsafe fn getsockopt(
     sockfd: i32,
     level: i32,
-    optname: i32,
-    optval: &mut usize,
-    optlen: &mut socklen_t,
+    opt_name: i32,
+    opt_val: *mut core::ffi::c_void,
+    opt_len: &mut socklen_t,
 ) -> Result<(), Errno> {
     let sockfd = sockfd as usize;
     let level = level as usize;
-    let optname = optname as usize;
-    let optval_ptr = optval as *mut usize as usize;
-    let optlen_ptr = optlen as *mut socklen_t as usize;
+    let opt_name = opt_name as usize;
+    let opt_val_ptr = opt_val as usize;
+    let opt_len_ptr = opt_len as *mut socklen_t as usize;
     syscall5(
         SYS_GETSOCKOPT,
         sockfd,
         level,
-        optname,
-        optval_ptr,
-        optlen_ptr,
+        opt_name,
+        opt_val_ptr,
+        opt_len_ptr,
     )
     .map(drop)
 }

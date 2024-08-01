a8cdc712fe393d4433fc4157244819798ae386cd
Xu Shaohua <shaohua@biofan.org>
2024-08-01 09:27:46 +0800

    calls: Update params in clock_nanosleep()

--- src/platform/linux-x86_64/call.rs
+++ src/platform/linux-x86_64/call.rs
@@ -645,21 +645,29 @@ pub unsafe fn clock_gettime(which_clock: clockid_t, tp: &mut timespec_t) -> Resu
 ///     tv_sec: 1,
 ///     tv_nsec: 0,
 /// };
-/// let mut rem = nc::timespec_t::default();
-/// let ret = unsafe { nc::clock_nanosleep(nc::CLOCK_MONOTONIC, 0, &t, &mut rem) };
+/// let ret = unsafe { nc::clock_nanosleep(nc::CLOCK_MONOTONIC, 0, &t, None) };
 /// assert!(ret.is_ok());
 /// ```
 pub unsafe fn clock_nanosleep(
     which_clock: clockid_t,
     flags: i32,
-    rqtp: &timespec_t,
-    rmtp: &mut timespec_t,
+    request: &timespec_t,
+    remain: Option<&mut timespec_t>,
 ) -> Result<(), Errno> {
     let which_clock = which_clock as usize;
     let flags = flags as usize;
-    let rqtp_ptr = rqtp as *const timespec_t as usize;
-    let rmtp_ptr = rmtp as *mut timespec_t as usize;
-    syscall4(SYS_CLOCK_NANOSLEEP, which_clock, flags, rqtp_ptr, rmtp_ptr).map(drop)
+    let request_ptr = request as *const timespec_t as usize;
+    let remain_ptr = remain.map_or(core::ptr::null_mut::<timespec_t>() as usize, |remain| {
+        remain as *mut timespec_t as usize
+    });
+    syscall4(
+        SYS_CLOCK_NANOSLEEP,
+        which_clock,
+        flags,
+        request_ptr,
+        remain_ptr,
+    )
+    .map(drop)
 }
 
 /// Set time of specific clock.

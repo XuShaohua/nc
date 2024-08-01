b7f68251d917b8949bf06f6f972b243447dfb0f4
Xu Shaohua <shaohua@biofan.org>
2024-08-01 09:32:45 +0800

    calls: Update params in init_module()

--- src/platform/linux-s390x/call.rs
+++ src/platform/linux-s390x/call.rs
@@ -3194,13 +3194,14 @@ pub unsafe fn idle() -> Result<(), Errno> {
 
 /// Load a kernel module.
 pub unsafe fn init_module<P: AsRef<Path>>(
-    module_image: usize,
-    len: usize,
+    module_image: &[u8],
     param_values: P,
 ) -> Result<(), Errno> {
+    let module_image_ptr = module_image.as_ptr() as usize;
+    let len = module_image.len();
     let param_values = CString::new(param_values.as_ref());
     let param_values_ptr = param_values.as_ptr() as usize;
-    syscall3(SYS_INIT_MODULE, module_image, len, param_values_ptr).map(drop)
+    syscall3(SYS_INIT_MODULE, module_image_ptr, len, param_values_ptr).map(drop)
 }
 
 /// Add a watch to an initialized inotify instance.

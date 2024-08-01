326a413fb844692744ca4c4fd31a691516eb4181
Xu Shaohua <shaohua@biofan.org>
2024-08-01 10:20:41 +0800

    calls: Update params in send() and recv()

--- src/platform/linux-mips/call.rs
+++ src/platform/linux-mips/call.rs
@@ -6450,6 +6450,45 @@ pub unsafe fn reboot(magic: i32, magci2: i32, cmd: u32, arg: usize) -> Result<()
 }
 
 /// Receive a datagram from a socket.
+///
+/// # Examples
+///
+/// ```
+/// use std::thread;
+///
+/// const READ_SIDE: usize = 0;
+/// const WRITE_SIDE: usize = 1;
+///
+/// fn main() {
+///     let mut fds = [-1_i32; 2];
+///
+///     let ret = unsafe { nc::socketpair(nc::AF_UNIX, nc::SOCK_STREAM, 0, &mut fds) };
+///     assert!(ret.is_ok());
+///     println!("socket pairs: {}, {}", fds[0], fds[1]);
+///
+///     // Start worker thread
+///     thread::spawn(move || {
+///         println!("worker thread started");
+///         let msg = "Hello, Rust";
+///         println!("[worker] Will send msg: {msg}");
+///         let ret = unsafe { nc::send(fds[WRITE_SIDE], msg.as_bytes(), 0) };
+///         assert!(ret.is_ok());
+///         assert_eq!(ret, Ok(msg.len() as nc::ssize_t));
+///     });
+///
+///     let mut buf = [0_u8; 1024];
+///     let ret = unsafe { nc::recv(fds[READ_SIDE], &mut buf, 0) };
+///     assert!(ret.is_ok());
+///     let nread = ret.unwrap();
+///     let msg = std::str::from_utf8(&buf[..nread as usize]).unwrap();
+///     println!("[main] recv msg: {msg}");
+///
+///     unsafe {
+///         let _ = nc::close(fds[0]);
+///         let _ = nc::close(fds[1]);
+///     }
+/// }
+/// ```
 pub unsafe fn recv(sockfd: i32, buf: &mut [u8], flags: i32) -> Result<ssize_t, Errno> {
     let sockfd = sockfd as usize;
     let buf_ptr = buf.as_mut_ptr() as usize;
@@ -7119,6 +7158,45 @@ pub unsafe fn semget(key: key_t, nsems: i32, semflg: i32) -> Result<i32, Errno> 
 }
 
 /// Send a message on a socket.
+///
+/// # Examples
+///
+/// ```
+/// use std::thread;
+///
+/// const READ_SIDE: usize = 0;
+/// const WRITE_SIDE: usize = 1;
+///
+/// fn main() {
+///     let mut fds = [-1_i32; 2];
+///
+///     let ret = unsafe { nc::socketpair(nc::AF_UNIX, nc::SOCK_STREAM, 0, &mut fds) };
+///     assert!(ret.is_ok());
+///     println!("socket pairs: {}, {}", fds[0], fds[1]);
+///
+///     // Start worker thread
+///     thread::spawn(move || {
+///         println!("worker thread started");
+///         let msg = "Hello, Rust";
+///         println!("[worker] Will send msg: {msg}");
+///         let ret = unsafe { nc::send(fds[WRITE_SIDE], msg.as_bytes(), 0) };
+///         assert!(ret.is_ok());
+///         assert_eq!(ret, Ok(msg.len() as nc::ssize_t));
+///     });
+///
+///     let mut buf = [0_u8; 1024];
+///     let ret = unsafe { nc::recv(fds[READ_SIDE], &mut buf, 0) };
+///     assert!(ret.is_ok());
+///     let nread = ret.unwrap();
+///     let msg = std::str::from_utf8(&buf[..nread as usize]).unwrap();
+///     println!("[main] recv msg: {msg}");
+///
+///     unsafe {
+///         let _ = nc::close(fds[0]);
+///         let _ = nc::close(fds[1]);
+///     }
+/// }
+/// ```
 pub unsafe fn send(sockfd: i32, buf: &[u8], flags: i32) -> Result<ssize_t, Errno> {
     let sockfd = sockfd as usize;
     let buf_ptr = buf.as_ptr() as usize;

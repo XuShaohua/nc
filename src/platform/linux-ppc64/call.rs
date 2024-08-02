36070cfc3f1f7bdcb8a44bf8e40a05ce2141a9f8
Xu Shaohua <shaohua@biofan.org>
2024-08-02 10:19:58 +0800

    calls: Update params in quotactl()

--- src/platform/linux-ppc64/call.rs
+++ src/platform/linux-ppc64/call.rs
@@ -6255,15 +6255,16 @@ pub unsafe fn pwritev2(
 
 /// Manipulate disk quotes.
 pub unsafe fn quotactl<P: AsRef<Path>>(
-    cmd: i32,
+    cmd: u32,
     special: P,
     id: qid_t,
-    addr: usize,
+    addr: *const core::ffi::c_void,
 ) -> Result<(), Errno> {
     let cmd = cmd as usize;
     let special = CString::new(special.as_ref());
     let special_ptr = special.as_ptr() as usize;
     let id = id as usize;
+    let addr = addr as usize;
     syscall4(SYS_QUOTACTL, cmd, special_ptr, id, addr).map(drop)
 }
 

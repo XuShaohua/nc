bdac62259a1581d4e1f58fe5a7bdebeb5062eb43
Xu Shaohua <shaohua@biofan.org>
2024-08-04 01:50:02 +0800

    linux-types: Add umount() flags

--- src/platform/linux-types/linux/fs.rs
+++ src/platform/linux-types/linux/fs.rs
@@ -10,6 +10,29 @@ pub struct file_handle_t {
     pub handle_bytes: u32,
     pub handle_type: i32,
     /// file identifier
-    /// FIXME(Shaohua): Replace with a pointer
     pub f_handle: [u8; 0],
+}
+
+/// Umount options
+/// Attempt to forcibily umount
+pub const MNT_FORCE: u32 = 0x0000_0001;
+/// Just detach from the tree
+pub const MNT_DETACH: u32 = 0x0000_0002;
+/// Mark for expiry
+pub const MNT_EXPIRE: u32 = 0x0000_0004;
+/// Don't follow symlink on umount
+pub const UMOUNT_NOFOLLOW: u32 = 0x0000_0008;
+/// Flag guaranteed to be unused
+pub const UMOUNT_UNUSED: u32 = 0x8000_0000;
+
+#[cfg(test)]
+mod tests {
+    use std::mem::size_of;
+
+    use super::file_handle_t;
+
+    #[test]
+    fn test_size_of_file_handle() {
+        assert_eq!(size_of::<file_handle_t>(), 8);
+    }
 }

a19db2a9b4bd6aac0634a4f530853a4a13781a92
Xu Shaohua <shaohua@biofan.org>
2024-08-04 22:04:01 +0800

    calls: Update params in select()

--- src/platform/linux-x86_64/call.rs
+++ src/platform/linux-x86_64/call.rs
@@ -7163,22 +7163,32 @@ pub unsafe fn seccomp(operation: u32, flags: u32, args: usize) -> Result<(), Err
 /// Sychronous I/O multiplexing.
 pub unsafe fn select(
     nfds: i32,
-    readfds: &mut fd_set_t,
-    writefds: &mut fd_set_t,
-    exceptfds: &mut fd_set_t,
-    timeout: &mut timeval_t,
+    read_fds: Option<&mut fd_set_t>,
+    write_fds: Option<&mut fd_set_t>,
+    except_fds: Option<&mut fd_set_t>,
+    timeout: Option<&timeval_t>,
 ) -> Result<i32, Errno> {
+    use core::ptr::{null, null_mut};
+
     let nfds = nfds as usize;
-    let readfds_ptr = readfds as *mut fd_set_t as usize;
-    let writefds_ptr = writefds as *mut fd_set_t as usize;
-    let exceptfds_ptr = exceptfds as *mut fd_set_t as usize;
-    let timeout_ptr = timeout as *mut timeval_t as usize;
+    let read_fds_ptr = read_fds.map_or(null_mut::<fd_set_t>() as usize, |read_fds| {
+        read_fds as *mut fd_set_t as usize
+    });
+    let write_fds_ptr = write_fds.map_or(null_mut::<fd_set_t>() as usize, |write_fds| {
+        write_fds as *mut fd_set_t as usize
+    });
+    let except_fds_ptr = except_fds.map_or(null_mut::<fd_set_t>() as usize, |except_fds| {
+        except_fds as *mut fd_set_t as usize
+    });
+    let timeout_ptr = timeout.map_or(null::<timeval_t> as usize, |timeout| {
+        timeout as *const timeval_t as usize
+    });
     syscall5(
         SYS_SELECT,
         nfds,
-        readfds_ptr,
-        writefds_ptr,
-        exceptfds_ptr,
+        read_fds_ptr,
+        write_fds_ptr,
+        except_fds_ptr,
         timeout_ptr,
     )
     .map(|ret| ret as i32)

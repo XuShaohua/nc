21ea8d5bd65f5b5a078a8608c8c5a28cedcbbf36
Xu Shaohua <shaohua@biofan.org>
2024-08-05 23:53:28 +0800

    calls: Update params in pselect6()

--- src/platform/linux-arm/call.rs
+++ src/platform/linux-arm/call.rs
@@ -6116,24 +6116,35 @@ pub unsafe fn process_vm_writev(
 /// the sigset size.
 pub unsafe fn pselect6(
     nfds: i32,
-    readfds: &mut fd_set_t,
-    writefds: &mut fd_set_t,
-    exceptfds: &mut fd_set_t,
-    timeout: &timespec_t,
-    sigmask: &sigset_t,
+    read_fds: Option<&mut fd_set_t>,
+    write_fds: Option<&mut fd_set_t>,
+    except_fds: Option<&mut fd_set_t>,
+    timeout: Option<&timespec_t>,
+    sigmask: Option<&sigset_t>,
 ) -> Result<i32, Errno> {
+    use core::ptr::{null, null_mut};
     let nfds = nfds as usize;
-    let readfds_ptr = readfds as *mut fd_set_t as usize;
-    let writefds_ptr = writefds as *mut fd_set_t as usize;
-    let exceptfds_ptr = exceptfds as *mut fd_set_t as usize;
-    let timeout_ptr = timeout as *const timespec_t as usize;
-    let sigmask_ptr = sigmask as *const sigset_t as usize;
+    let read_fds_ptr = read_fds.map_or(null_mut::<fd_set_t>() as usize, |read_fds| {
+        read_fds as *mut fd_set_t as usize
+    });
+    let write_fds_ptr = write_fds.map_or(null_mut::<fd_set_t>() as usize, |write_fds| {
+        write_fds as *mut fd_set_t as usize
+    });
+    let except_fds_ptr = except_fds.map_or(null_mut::<fd_set_t>() as usize, |except_fds| {
+        except_fds as *mut fd_set_t as usize
+    });
+    let timeout_ptr = timeout.map_or(null::<timespec_t>() as usize, |timeout| {
+        timeout as *const timespec_t as usize
+    });
+    let sigmask_ptr = sigmask.map_or(null::<sigset_t>() as usize, |sigmask| {
+        sigmask as *const sigset_t as usize
+    });
     syscall6(
         SYS_PSELECT6,
         nfds,
-        readfds_ptr,
-        writefds_ptr,
-        exceptfds_ptr,
+        read_fds_ptr,
+        write_fds_ptr,
+        except_fds_ptr,
         timeout_ptr,
         sigmask_ptr,
     )

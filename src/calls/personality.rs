5aef8702f12a03cff806030879ee359040be2fea
Xu Shaohua <shaohua@biofan.org>
2024-08-05 22:53:29 +0800

    calls: Update params in fanotify_mark()

--- src/calls/personality.rs
+++ src/calls/personality.rs
@@ -1,4 +1,6 @@
 /// Set the process execution domain.
+///
+/// Returns old execution domain.
 pub unsafe fn personality(persona: u32) -> Result<u32, Errno> {
     let persona = persona as usize;
     syscall1(SYS_PERSONALITY, persona).map(|ret| ret as u32)
--- src/platform/linux-mips/call.rs
+++ src/platform/linux-mips/call.rs
@@ -1561,21 +1561,23 @@ pub unsafe fn fanotify_mark<P: AsRef<Path>>(
     fanotify_fd: i32,
     flags: u32,
     mask: u64,
-    fd: i32,
-    filename: P,
+    dir_fd: i32,
+    filename: Option<P>,
 ) -> Result<(), Errno> {
     let fanotify_fd = fanotify_fd as usize;
     let flags = flags as usize;
     let mask = mask as usize;
-    let fd = fd as usize;
-    let filename = CString::new(filename.as_ref());
-    let filename_ptr = filename.as_ptr() as usize;
+    let dir_fd = dir_fd as usize;
+    let filename = filename.map(|filename| CString::new(filename.as_ref()));
+    let filename_ptr = filename.map_or(core::ptr::null::<u8>() as usize, |filename| {
+        filename.as_ptr() as usize
+    });
     syscall5(
         SYS_FANOTIFY_MARK,
         fanotify_fd,
         flags,
         mask,
-        fd,
+        dir_fd,
         filename_ptr,
     )
     .map(drop)
@@ -5702,6 +5704,8 @@ pub unsafe fn perf_event_open(
 }
 
 /// Set the process execution domain.
+///
+/// Returns old execution domain.
 pub unsafe fn personality(persona: u32) -> Result<u32, Errno> {
     let persona = persona as usize;
     syscall1(SYS_PERSONALITY, persona).map(|ret| ret as u32)

301031b4b45f97a0153d789679de9d2d4379642d
Xu Shaohua <shaohua@biofan.org>
2024-08-05 22:46:00 +0800

    calls: Update params in set_robust_list()

--- src/platform/linux-ppc64/call.rs
+++ src/platform/linux-ppc64/call.rs
@@ -8458,10 +8458,9 @@ pub unsafe fn set_mempolicy_home_node(
 /// Params:
 /// - `head`: pointer to the list-head
 /// - `len`: length of the list-head, as userspace expects
-pub unsafe fn set_robust_list(heads: &[robust_list_head_t]) -> Result<(), Errno> {
-    let heads_ptr = heads.as_ptr() as usize;
-    let len = heads.len();
-    syscall2(SYS_SET_ROBUST_LIST, heads_ptr, len).map(drop)
+pub unsafe fn set_robust_list(head: *mut robust_list_head_t, len: usize) -> Result<(), Errno> {
+    let head_ptr = head as usize;
+    syscall2(SYS_SET_ROBUST_LIST, head_ptr, len).map(drop)
 }
 
 /// Set pointer to thread ID.

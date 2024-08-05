dcc27b5109d44d2db615a4e2fc7a2367325184fd
Xu Shaohua <shaohua@biofan.org>
2024-08-05 23:28:14 +0800

    linux-types: Move to sub-folders

---
+++ src/platform/linux-types/x86_64/uapi/asm/mod.rs
@@ -0,0 +1,13 @@
+// Copyright (c) 2024 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+mod ldt;
+mod ptrace_abi;
+mod signal;
+mod stat;
+
+pub use ldt::*;
+pub use ptrace_abi::*;
+pub use signal::*;
+pub use stat::*;
--- src/platform/linux-types/x86_64/uapi/mod.rs
+++ src/platform/linux-types/x86_64/uapi/mod.rs
@@ -1,9 +1,7 @@
-// Copyright (c) 2020 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Copyright (c) 2024 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
 mod asm;
-mod uapi;
 
 pub use asm::*;
-pub use uapi::*;

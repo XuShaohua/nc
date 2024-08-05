40e25e22674549d3e19bdf6a8880fd82a26a53dc
Xu Shaohua <shaohua@biofan.org>
2024-08-05 23:41:45 +0800

    calls: Update params in acct()

--- src/platform/linux-aarch64/call.rs
+++ src/platform/linux-aarch64/call.rs
@@ -230,14 +230,16 @@ pub unsafe fn accept4(
 /// let fd = fd.unwrap();
 /// let ret = unsafe { nc::close(fd) };
 /// assert!(ret.is_ok());
-/// let ret = unsafe { nc::acct(path) };
+/// let ret = unsafe { nc::acct(Some(path)) };
 /// assert_eq!(ret, Err(nc::EPERM));
 /// let ret = unsafe { nc::unlinkat(nc::AT_FDCWD, path, 0) };
 /// assert!(ret.is_ok());
 /// ```
-pub unsafe fn acct<P: AsRef<Path>>(filename: P) -> Result<(), Errno> {
-    let filename = CString::new(filename.as_ref());
-    let filename_ptr = filename.as_ptr() as usize;
+pub unsafe fn acct<P: AsRef<Path>>(filename: Option<P>) -> Result<(), Errno> {
+    let filename = filename.map(|filename| CString::new(filename.as_ref()));
+    let filename_ptr = filename.map_or(core::ptr::null::<u8>() as usize, |filename| {
+        filename.as_ptr() as usize
+    });
     syscall1(SYS_ACCT, filename_ptr).map(drop)
 }
 

1ff7141684a0383484b6e7c97d7c1e5eb073ab17
Xu Shaohua <shaohua@biofan.org>
2024-08-05 22:32:25 +0800

    calls: Add quotactl_fd()

--- src/platform/linux-x86/call.rs
+++ src/platform/linux-x86/call.rs
@@ -6346,6 +6346,20 @@ pub unsafe fn quotactl<P: AsRef<Path>>(
     syscall4(SYS_QUOTACTL, cmd, special_ptr, id, addr).map(drop)
 }
 
+/// Manipulate disk quotes.
+pub unsafe fn quotactl_fd<P: AsRef<Path>>(
+    fd: i32,
+    cmd: u32,
+    id: qid_t,
+    addr: *const core::ffi::c_void,
+) -> Result<(), Errno> {
+    let fd = fd as usize;
+    let cmd = cmd as usize;
+    let id = id as usize;
+    let addr = addr as usize;
+    syscall4(SYS_QUOTACTL_FD, fd, cmd, id, addr).map(drop)
+}
+
 /// Read from a file descriptor.
 ///
 /// # Examples

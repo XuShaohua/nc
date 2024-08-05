740d9b2876d13bf1ac58328c6db4b3cef9067885
Xu Shaohua <shaohua@biofan.org>
2024-08-05 22:35:57 +0800

    calls: Update params in request_key()

--- src/platform/linux-arm/call.rs
+++ src/platform/linux-arm/call.rs
@@ -6905,15 +6905,17 @@ pub unsafe fn renameat2<P: AsRef<Path>>(
 pub unsafe fn request_key<P: AsRef<Path>>(
     type_: P,
     description: P,
-    callout_info: P,
+    callout_info: Option<P>,
     dest_keyring: key_serial_t,
 ) -> Result<key_serial_t, Errno> {
     let type_ = CString::new(type_.as_ref());
     let type_ptr = type_.as_ptr() as usize;
     let description = CString::new(description.as_ref());
     let description_ptr = description.as_ptr() as usize;
-    let callout_info = CString::new(callout_info.as_ref());
-    let callout_info_ptr = callout_info.as_ptr() as usize;
+    let callout_info = callout_info.map(|callout_info| CString::new(callout_info.as_ref()));
+    let callout_info_ptr = callout_info.map_or(core::ptr::null::<u8>() as usize, |callout_info| {
+        callout_info.as_ptr() as usize
+    });
     let dest_keyring = dest_keyring as usize;
     syscall4(
         SYS_REQUEST_KEY,

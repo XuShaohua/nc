567c64118c5c88a7d34d1c1dfc8bbd9fc8362651
Xu Shaohua <shaohua@biofan.org>
2024-08-06 15:51:58 +0800

    calls: Update params in clone3()

--- src/platform/linux-arm/call.rs
+++ src/platform/linux-arm/call.rs
@@ -715,11 +715,11 @@ pub unsafe fn clone(
 /// args.exit_signal = nc::SIGCHLD as u64;
 /// args.pidfd = &mut pid_fd as *mut i32 as usize as u64;
 /// args.flags = nc::CLONE_PIDFD as u64 | nc::CLONE_PARENT_SETTID as u64;
-/// let pid = unsafe { nc::clone3(&mut args) };
+/// let pid = unsafe { nc::clone3(&args) };
 /// assert!(pid.is_ok());
 /// ```
-pub unsafe fn clone3(cl_args: &mut clone_args_t) -> Result<pid_t, Errno> {
-    let cl_args_ptr = cl_args as *mut clone_args_t as usize;
+pub unsafe fn clone3(cl_args: &clone_args_t) -> Result<pid_t, Errno> {
+    let cl_args_ptr = cl_args as *const clone_args_t as usize;
     let size = core::mem::size_of::<clone_args_t>();
     syscall2(SYS_CLONE3, cl_args_ptr, size).map(|ret| ret as pid_t)
 }

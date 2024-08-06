d350bfbbbc31978f166fa04bb22a5fae5a543c2e
Xu Shaohua <shaohua@biofan.org>
2024-08-06 09:53:02 +0800

    calls: Update params in clone()

--- src/platform/linux-mips64/call.rs
+++ src/platform/linux-mips64/call.rs
@@ -705,20 +705,27 @@ pub unsafe fn clock_settime(which_clock: clockid_t, tp: &timespec_t) -> Result<(
 /// Create a child process.
 pub unsafe fn clone(
     clone_flags: usize,
-    newsp: usize,
-    parent_tid: &mut i32,
-    child_tid: &mut i32,
-    tls: usize,
+    new_sp: *const core::ffi::c_void,
+    parent_tid: Option<&mut pid_t>,
+    child_tid: Option<&mut pid_t>,
+    tls: Option<*const core::ffi::c_void>,
 ) -> Result<pid_t, Errno> {
-    let parent_tid_ptr = parent_tid as *mut i32 as usize;
-    let child_tid_ptr = child_tid as *mut i32 as usize;
+    use core::ptr::null_mut;
+    let new_sp = new_sp as usize;
+    let parent_tid_ptr = parent_tid.map_or(null_mut::<pid_t>() as usize, |parent_tid| {
+        parent_tid as *mut pid_t as usize
+    });
+    let child_tid_ptr = child_tid.map_or(null_mut::<pid_t>() as usize, |child_tid| {
+        child_tid as *mut pid_t as usize
+    });
+    let tls_ptr = tls.map_or(core::ptr::null::<u8>() as usize, |tls| tls as usize);
     syscall5(
         SYS_CLONE,
         clone_flags,
-        newsp,
+        new_sp,
         parent_tid_ptr,
         child_tid_ptr,
-        tls,
+        tls_ptr,
     )
     .map(|ret| ret as pid_t)
 }
